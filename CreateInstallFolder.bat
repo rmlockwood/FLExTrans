45c28e053d4d81567fbead793a710f191afc002d
R Lockwood <rmlockwood@users.noreply.github.com>
2015-08-01 13:17:01 +0200

    Version 1.1.1
    bug fixes

--- CreateInstallFolder.bat
+++ CreateInstallFolder.bat
@@ -13,7 +13,7 @@ mkdir Install\Modules\FLExTrans
 mkdir Install\Modules\FLExTrans\Lib
 for /f %%f in ('dir /ad /b') do copy %%f\*.py Install\Modules\FLExTrans
 copy readconfig.py Install\Modules\FLExTrans\Lib
-copy utils.py Install\Modules\FLExTrans\Lib
+copy Utils.py Install\Modules\FLExTrans\Lib
 copy subdirs.pth Install\Modules
 copy FlexTrans.config Install
 copy "FLExTrans Step 1.ini" Install\Collections
@@ -24,6 +24,7 @@ copy replace.dix Install
 copy stamp32.exe Install
 cd Install
 "C:\Program Files (x86)\7-Zip\7z" a FlexTrans.zip Modules Collections *.config *.ini *.t1x *.dix stamp32.exe
+copy /Y FlexTrans.zip ..
 cd ..
 rem pause
 
