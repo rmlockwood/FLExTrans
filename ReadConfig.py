8129e9b8312c85e5f8254e47639c6703aef2fa3d
rmlockwood <rlbooks@web.de>
2021-01-25 16:18:27 +0100

    linted

--- ReadConfig.py
+++ ReadConfig.py
@@ -12,7 +12,7 @@
 #    Bump the version number.
 #
 #   Version 1.1 - 3/7/18 - Ron Lockwood
-#    Give an error only if the report object is not None 
+#    Give an error only if the report object is not None
 #
 #   Functions for reading a configuration file
 
@@ -26,7 +26,7 @@ def readConfig(report):
         if report is not None:
             report.Error('Error reading the file: "' + CONFIG_FILE + '". Check that it is in the FlexTools folder.')
         return None
-    
+
     my_map = {}
     for line in f_handle:
         if len(line) < 2:
@@ -37,19 +37,19 @@ def readConfig(report):
         if line[0] == '#':
             continue
         # We expect lines in the form -- property=value
-        if not re.search('=',line):
+        if not re.search('=', line):
             if report is not None:
                 report.Error('Error reading the file: "' + CONFIG_FILE + '". A line without "=" was found.')
             return
         (prop, value) = line.split('=')
         value = value.rstrip()
         # if the value has commas, save it as a list
-        if re.search(',',value):
+        if re.search(',', value):
             my_list = value.split(',')
             my_map[prop] = my_list
         else:
             my_map[prop] = value
-    
+
     return my_map
 
 def getConfigVal(my_map, key, report):
@@ -60,11 +60,10 @@ def getConfigVal(my_map, key, report):
     else:
         return my_map[key]
 
-def configValIsList(my_map, key, report):    
-    if type(my_map[key]) is not list:
+def configValIsList(my_map, key, report):
+    if isinstance(my_map[key], list) is False:
         if report is not None:
             report.Error('Error in the file: "' + CONFIG_FILE + '". The value for "'+key+'" is supposed to be a comma separated list. For a single value, end it with a comma.')
         return False
     else:
         return True
-    
