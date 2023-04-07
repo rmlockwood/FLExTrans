4a6846b881cf075ab99e2e941772d9e933d74a8f
rmlockwood <rlbooks@web.de>
2023-04-07 18:05:35 +0400

    include FlexTools version

--- Version.py
+++ Version.py
@@ -3,8 +3,11 @@
 #   FlexTools window.
 #
 
+FTName = "FLExTools"
+FTVersion = "2.2.1"
+
 Name = "FLExTrans"
 
 Version = "3.8"
 
-Title = f"{Name} {Version}"
+Title = f"{FTName} {FTVersion} ({Name} {Version})"
