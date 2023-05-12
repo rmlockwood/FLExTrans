0f955fe9d3e3603864bb62e311f03c9de3e99c98
rmlockwood <rlbooks@web.de>
2023-05-12 16:33:20 +0200

    updated comment

--- FLExTrans.py
+++ FLExTrans.py
@@ -1,6 +1,9 @@
 #
 #   A shell module to launch FlexTools
 #
+#   Version 3.8.1 - 5/12/23 - Ron Lockwood
+#    Turn off dpi awareness.
+#
 #   Version 3.8 - 4/20/23 - Ron Lockwood
 #    Use Status Bar callback function
 #
