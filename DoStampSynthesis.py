8d444699b8da84b1a48443a7c2ed261dd76cb860
rmlockwood <rlbooks@web.de>
2023-07-19 12:16:15 +0200

    Update DoStampSynthesis.py

--- DoStampSynthesis.py
+++ DoStampSynthesis.py
@@ -239,8 +239,7 @@ This will default to something like 'target_text-syn.txt'.
 Before creating the synthesized text, this module extracts the target language lexicons files. One for
 roots, prefixes, suffixes and infixes. They are in the STAMP format for synthesis. The lexicon files 
 are put into the folder designated in the Settings as Target Lexicon Files Folder. By default it is the 'Build' folder.
-NOTE: Messages will say the SOURCE database
-is being used. Actually the target database is being used.
+NOTE: Messages will say the SOURCE database is being used. Actually the target database is being used.
 """
 docs = {FTM_Name       : "Synthesize Text with STAMP",
         FTM_Version    : "3.9.2",
