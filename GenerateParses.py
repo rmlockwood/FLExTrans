9dcf5774d7356431aae39e97c320365ed05d9f5f
R Lockwood <rlbooks@web.de>
2024-05-14 11:20:19 +0700

    Handle null LexemeForm by skipping it.

--- GenerateParses.py
+++ GenerateParses.py
@@ -14,6 +14,7 @@
 #   the correct homograph/sense number on root glosses
 #
 #
+#   14 May 2024 rl v3.10.1 Handle null LexemeForm by skipping it.
 #   18 Jan 2023 rl v3.10  Bumped to 3.10
 #   17 Aug 2023 rl v3.9.4 More changes to support FLEx 9.1.22 and FlexTools 2.2.3 for Pythonnet 3.0.
 #   12 Aug 2023 rl v3.9.3 Changes to support FLEx 9.1.22 and FlexTools 2.2.3 for Pythonnet 3.0.
@@ -58,7 +59,7 @@ import ReadConfig
 # Documentation that the user sees:
 
 docs = {FTM_Name       : "Generate All Parses",
-        FTM_Version    : "3.10",
+        FTM_Version    : "3.10.1",
         FTM_ModifiesDB : False,
         FTM_Synopsis   : "Creates all possible parses from a FLEx project, in Apertium format.",
         FTM_Help       :"",
@@ -443,6 +444,9 @@ def MainFunction(DB, report, modifyAllowed):
     
         report.ProgressUpdate(entryCount)
               
+        if e.LexemeFormOA is None:
+            continue
+
         morphGuidStr = e.LexemeFormOA.MorphTypeRA.Guid.ToString()
         morphType = Utils.morphTypeMap[morphGuidStr]
 
