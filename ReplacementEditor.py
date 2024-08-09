7e1c02f1c0544f3a5413de1484eed198df046423
Daniel Swanson <awesomeevildudes@gmail.com>
2024-08-09 10:55:06 -0400

    Replacement Editor: documentation

--- ReplacementEditor.py
+++ ReplacementEditor.py
@@ -7,25 +7,23 @@ from flextoolslib import (
     FTM_Synopsis, FTM_Help, FTM_Description,
 )
 
+import xml.etree.ElementTree as ET
+
 import Utils
 
 docs = {
     FTM_Name: "Edit Replacement Dictionary",
     FTM_Version: "3.12",
     FTM_ModifiesDB: False,
-    FTM_Synopsis: "Builds an Apertium-style bilingual lexicon.",
+    FTM_Synopsis: "Edit manual overrides for the bilingual dictionary.",
     FTM_Help: "",
     FTM_Description:
 """
-This module will build a bilingual
-lexicon for two projects. The
-database that FlexTools is set to is your source project. Set the Target Project
-in Settings to the name of your target project.
-This module builds the bilingual lexicon based on the links from source senses to target senses
-that are in your source project. Use the Sense Linker Module to create these link.
-
-You can make custom changes to the bilingual lexicon by using a replacement file. See the help
-document for more details.
+This module provides an interface for editing the replacement dictionary
+which allows you to override the links created by the Sense Linker Tool
+in the presence of particular affixes. For example, if you have a noun that
+is generally translated one way, but has a different translation in the
+vocative singular, you can specify that here.
 """
 }
 
@@ -59,17 +57,26 @@ class TableRow:
 
         self.arrow.setText('⇒')
 
-    def loadData(self, entry):
+    def loadData(self, entry: ET.Element):
+        '''Iterate through the descendants of `entry` and extract the values
+        for this row of the table'''
+
         llem = ''
         ltags = []
         rlem = ''
         rtags = []
         def read(node, side=None):
+            '''Recursively examine the children of `node`'''
+
             nonlocal llem, ltags, rlem, rtags
+
+            # old style lemmas (which have literal spaces)
             if node.tag == 'leftdata':
                 llem += (node.text or '')
             elif node.tag == 'rightdata':
                 rlem += (node.text or '')
+
+            # tags
             elif node.tag == 's':
                 tag = node.attrib.get('n')
                 if not tag:
@@ -78,6 +85,8 @@ class TableRow:
                     ltags.append(tag)
                 elif side == 'right':
                     rtags.append(tag)
+
+            # spaces
             elif node.tag == 'b':
                 if side == 'left':
                     llem += ' '
@@ -87,6 +96,8 @@ class TableRow:
                     rlem += ' '
                     if node.tail:
                         rlem += node.tail
+
+            # other containers
             else:
                 nextSide = side
                 if node.tag == 'l':
@@ -99,10 +110,15 @@ class TableRow:
                         rlem += node.text
                 for ch in node:
                     read(ch, nextSide)
+
+        # recurse through the entry
         read(entry)
+
         # get rid of excess whitespace
         llem = ' '.join(llem.strip().split())
         rlem = ' '.join(rlem.strip().split())
+
+        # put the lemmas and tags in the table cells
         self.sourceLemma.setText(llem)
         if len(ltags) > 0:
             self.sourcePOS.setText(ltags[0])
@@ -116,7 +132,9 @@ class TableRow:
             self.targetInfl.setText(infl)
             self.targetAffixes.setText(aff)
 
-    def splitTagList(self, tags, features):
+    def splitTagList(self, tags: list[str], features) -> tuple[str, str]:
+        '''Attempt to distinguish inflectional features from affixes'''
+
         infl, aff = [], []
         for tag in tags:
             if tag in features:
@@ -125,11 +143,16 @@ class TableRow:
                 aff.append(tag)
         return '.'.join(infl), '.'.join(aff)
 
-    def getSource(self):
+    def getSource(self) -> tuple[str, str, str, str]:
+        '''Return the source fields as a tuple'''
+
         return (self.sourceLemma.text(), self.sourcePOS.text(),
                 self.sourceInfl.text(), self.sourceAffixes.text())
 
-    def tagList(self, *widgets):
+    def tagList(self, *widgets) -> list[str]:
+        '''Given a collection of widgets, extract their values and return
+        the tags in them as a list'''
+
         ret = []
         for widget in widgets:
             if value := widget.text():
@@ -138,17 +161,22 @@ class TableRow:
                         ret.append(tag)
         return ret
 
-    def toXML(self):
-        import xml.etree.ElementTree as ET
+    def toXML(self) -> ET.Element:
+        '''Generate an XML entry from the cell values'''
+
+        # get the data
         llem = self.sourceLemma.text().split() or ['']
         ltags = self.tagList(self.sourcePOS, self.sourceInfl, self.sourceAffixes)
         rlem = self.targetLemma.text().split() or ['']
         rtags = self.tagList(self.targetPOS, self.targetInfl, self.targetAffixes)
+
+        # build the structure
         entry = ET.Element('e')
         p = ET.SubElement(entry, 'p')
         l = ET.SubElement(p, 'l')
         r = ET.SubElement(p, 'r')
 
+        # insert the left
         l.text = llem[0]
         for piece in llem[1:]:
             b = ET.SubElement(l, 'b')
@@ -156,6 +184,7 @@ class TableRow:
         for tag in ltags:
             ET.SubElement(l, 's', n=tag)
 
+        # insert the right
         r.text = rlem[0]
         for piece in rlem[1:]:
             b = ET.SubElement(r, 'b')
@@ -166,14 +195,20 @@ class TableRow:
         return entry
 
     def checkCellUpdate(self, column):
+        '''Check if a lemma has changed, and autofill the other columns
+        as needed'''
+
         if column == 0:
+            # source lemma
             lemma = self.sourceLemma.text()
             pos, infl = self.window.sourceLemmas.get(lemma, (None, None))
             if pos is not None:
                 self.sourcePOS.setText(pos)
             if infl is not None:
                 self.sourceInfl.setText(infl)
+
         elif column == 5:
+            # target lemma
             lemma = self.targetLemma.text()
             pos, infl = self.window.targetLemmas.get(lemma, (None, None))
             if pos is not None:
@@ -182,16 +217,27 @@ class TableRow:
                 self.targetInfl.setText(infl)
 
 class SegmentedCompleter(QCompleter):
-    def splitPath(self, path):
+    '''Override the Qt autocomplete class to each tag in a period-separated
+    list rather than the field value as a whole'''
+
+    def splitPath(self, path: str) -> list[str]:
+        '''Pretend the field value is solely the tag which currently
+        contains the cursor'''
+
         pos = self.parent().cursorPosition()
         left = path[:pos].split('.')[-1]
         right = path[pos:].split('.')[0]
         return [left+right]
 
-    def pathFromIndex(self, index):
+    def pathFromIndex(self, index) -> str:
+        '''A selection has been chosen, so generate the actual field value
+        from it'''
+
         pos = self.parent().cursorPosition()
         text = self.parent().text()
         oldMiddle = ''
+
+        # get the tags to the left of the cursor
         left = text[:pos]
         if '.' in left:
             splitPos = left.rfind('.') + 1
@@ -200,6 +246,8 @@ class SegmentedCompleter(QCompleter):
         else:
             oldMiddle += left
             left = ''
+
+        # get the tags to the right of the cursor
         right = text[pos:]
         if '.' in right:
             splitPos = right.find('.')
@@ -208,16 +256,30 @@ class SegmentedCompleter(QCompleter):
         else:
             oldMiddle += right
             right = ''
+
+        # get the autocompleted tag
         middle = super().pathFromIndex(index)
+
         # If we we're completing a tag in the middle and we select an option,
         # then we sometimes apply the completion twice and also replace the
         # last tag in the input, hence this check.
         if not middle.startswith(oldMiddle):
             return text
+
+        # I'm not quite sure where in the process to use this value,
+        # but ideally after completing we'd be able to set the user's cursor
+        # to be at the end of the tag they just completed.
+        # Unfortunately, for now I think we're stuck with the cursor jumping
+        # to the end of the input. -DGS 2024-08-09
         self.posShouldBe = len(left + middle)
+
+        # return the final value
         return left + middle + right
 
 class CompleterDelegate(QItemDelegate):
+    '''Intermediary between the table and the fields to ensure that when the
+    fields turn into input boxes they have autocompleters attached'''
+
     def __init__(self, values, use_segmented, composed):
         super().__init__()
         self.values = values
@@ -227,6 +289,8 @@ class CompleterDelegate(QItemDelegate):
         self.use_segmented = use_segmented
 
     def createEditor(self, *args, **kwargs):
+        '''Turn a label into an input box and attach an autocompleter'''
+
         ret = super().createEditor(*args, **kwargs)
         if self.use_segmented:
             comp = SegmentedCompleter(self.values, ret)
@@ -288,7 +352,6 @@ class Main(QMainWindow):
         super().setWindowIcon(QtGui.QIcon(os.path.join(FTPaths.TOOLS_DIR, 'FLExTransWindowIcon.ico')))
 
     def loadEntries(self):
-        from xml.etree import ElementTree as ET
 
         entries = []
         try:
@@ -396,7 +459,6 @@ class Main(QMainWindow):
 
     def save(self):
         self.checkTable()
-        from xml.etree import ElementTree as ET
         dix = ET.Element('repldictionary')
         section = ET.SubElement(dix, 'section', id='append', type='standard')
         for row in self.rows:
