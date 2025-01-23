2309a8fcffd992aee466155d633045963871ea3f
R Lockwood <rlbooks@web.de>
2025-01-23 11:00:21 +0100

    Support import of Glossary book (GLO).

--- Dev/Lib/Utils.py
+++ Dev/Lib/Utils.py
@@ -5,6 +5,9 @@
 #   SIL International
 #   7/23/2014
 #
+#   Version 3.12.11 - 1/23/25 - Ron Lockwood
+#    Support import of Glossary book (GLO).
+#
 #   Version 3.12.10 - 1/18/25 - Ron Lockwood
 #    Escape angle brackets.
 #
@@ -2223,8 +2226,8 @@ def insertParagraphs(DB, inputStr, m_stTxtParaFactory, stText):
     # Attributes are of the form |x=123 ... \s*
     # You can't have parens inside of the split expression since it is already in parens. It will mess up the output.
     #                                                                                                                                                                                                  eg \+xt
-    #                  attribs end mrk footnt  footnt ref+dash     footnt ref      cr ref note   cr ref  cr ref orig+dash    cr ref orig     verse+dash   verse    pub verse chap    ref+dash       ref        marker+ any marker
-    segs = re.split(r'(\|.+?\*|\\\w+\*|\\f \+ |\\fr \d+[:.]\d+-\d+|\\fr \d+[:.]\d+|\\xt .+?\\x\*|\\x \+ |\\xo \d+[:.]\d+-\d+|\\xo \d+[:.]\d+|\\v \d+-\d+ |\\v \d+ |\\vp \S+ |\\c \d+|\d+[:.]\d+-\d+|\d+[:.]\d+|\\\+\w+|\\\w+)', inputStr) 
+    #                  attribs end mrk footnt  footnt ref+dash     footnt ref      cr ref note   cr ref  cr ref orig+dash    cr ref orig     verse+dash   verse    pub verse chap    remark     ref+dash       ref        marker+ any marker
+    segs = re.split(r'(\|.+?\*|\\\w+\*|\\f \+ |\\fr \d+[:.]\d+-\d+|\\fr \d+[:.]\d+|\\xt .+?\\x\*|\\x \+ |\\xo \d+[:.]\d+-\d+|\\xo \d+[:.]\d+|\\v \d+-\d+ |\\v \d+ |\\vp \S+ |\\c \d+|\\rem.+?\n|\d+[:.]\d+-\d+|\d+[:.]\d+|\\\+\w+|\\\w+)', inputStr) 
 
     # Create 1st paragraph object
     stTxtPara = m_stTxtParaFactory.Create()
