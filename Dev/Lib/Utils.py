3382c0bbf65bce0358cdc05e20fa4b4d02ff9542
R Lockwood <rlbooks@web.de>
2025-03-10 17:14:10 +0100

    Fixes #928.
    Look for newlines between | and * when splitting what will be analysis WS text.

--- Dev/Lib/Utils.py
+++ Dev/Lib/Utils.py
@@ -5,6 +5,9 @@
 #   SIL International
 #   7/23/2014
 #
+#   Version 3.13.1 - 3/10/25 - Ron Lockwood
+#    Fixes #928. Look for newlines between | and * when splitting what will be analysis WS text.
+#
 #   Version 3.13 - 3/10/25 - Ron Lockwood
 #    Bumped to 3.13.
 #
@@ -2293,7 +2296,7 @@ def insertParagraphs(DB, inputStr, m_stTxtParaFactory, stText):
     # You can't have parens inside of the split expression since it is already in parens. It will mess up the output.
     #                                                                                                                                                                                                  eg \+xt
     #                  attribs end mrk footnt  footnt ref+dash     footnt ref      cr ref note   cr ref  cr ref orig+dash    cr ref orig     verse+dash   verse    pub verse chap    remark     ref+dash       ref        marker+ any marker
-    segs = re.split(r'(\n|\|.+?\*|\\\w+\*|\\f \+ |\\fr \d+[:.]\d+-\d+|\\fr \d+[:.]\d+|\\xt .+?\\x\*|\\x \+ |\\xo \d+[:.]\d+-\d+|\\xo \d+[:.]\d+|\\v \d+-\d+ |\\v \d+ |\\vp \S+ |\\c \d+|\\rem.+?\n|\d+[:.]\d+-\d+|\d+[:.]\d+|\\\+\w+|\\\w+)', inputStr) 
+    segs = re.split(r'(\n|\|(?:.|\n)+?\*|\\\w+\*|\\f \+ |\\fr \d+[:.]\d+-\d+|\\fr \d+[:.]\d+|\\xt .+?\\x\*|\\x \+ |\\xo \d+[:.]\d+-\d+|\\xo \d+[:.]\d+|\\v \d+-\d+ |\\v \d+ |\\vp \S+ |\\c \d+|\\rem.+?\n|\d+[:.]\d+-\d+|\d+[:.]\d+|\\\+\w+|\\\w+)', inputStr) 
 
     # Create 1st paragraph object
     stTxtPara = m_stTxtParaFactory.Create()
