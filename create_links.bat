c5b9a96e1c3114dea626ff18b8b671cd6a47efd9
bbryson <Beth-docs_Bryson@sil.org>
2025-03-26 19:03:52 -0500

    Add instructions
    Adding instructions about things that were not clear to me when I tried to use this script.

--- create_links.bat
+++ create_links.bat
@@ -1,6 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Set the variables current_dir and git_dev to the appropriate directories on your machine.
+
+REM current_dir points to a folder inside of where you have FLExTrans installed for testing your development work.
+REM More specifically, inside that installation, find the path FlexTools\Modules\FLExTrans
+REM After you install FLExTrans in this location, you will need to delete all the .py files that
+REM got installed in the folders: FlexTools, FlexTools\Modules\FLExTrans, and FlexTools\Modules\FLExTrans\Lib
+REM so that this script can create symbolic links in place of those real files.
+
+REM git_dev points to the Dev folder inside your FLExTrans github repo folder.
+REM This path must not have any spaces in it (in order for the symbolic linking to work).
+
+REM You will probably need to run this .bat file "as Administrator"
+
 set current_dir="C:\Data\FLExTrans\Dev\Test Projects\FlexTools\Modules\FLExTrans"
 set git_dev=c:\users\rlboo\GitHub\flextrans\Dev
 
