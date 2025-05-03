151fe7f37e333aa9d9d4a4ad6bc494061496ee23
R Lockwood <rlbooks@web.de>
2025-05-03 09:16:08 +0200

    kind of working, except tranl() within functions

--- Dev/TopLevel/FLExTransMenu.py
+++ Dev/TopLevel/FLExTransMenu.py
@@ -12,26 +12,40 @@
 #
 #   Version 3.8 - 4/20/23 - Ron Lockwood
 #    Settings are now launched from the menu
+import sys
 
-from System.Windows.Forms import ( # type: ignore
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QCoreApplication, QTranslator
+from System.Windows.Forms import (  # type: ignore
     Shortcut,
-    MessageBox, 
+    MessageBox,
     MessageBoxButtons,
-    )
-    
+)
+
 import os
 from subprocess import call
 
 import SettingsGUI
-from FTPaths import HELP_DIR
+from FTPaths import HELP_DIR, TRANSL_DIR
 import Version
 import ReadConfig
+
+app = QApplication(sys.argv)
+
+# Load translations
+translator = QTranslator()
+langCode = 'de'
+if translator.load(TRANSL_DIR+f"/FLExTransMenu_{langCode}.qm"):
+    QCoreApplication.installTranslator(translator)
+
+# Define _translate for convenience
+_translate = QCoreApplication.translate
 
 def RunSettings(sender, event):
     SettingsGUI.MainFunction(None, None)
 
+
 def RunEditTransferRules(sender, event):
-        
     configMap = ReadConfig.readConfig(None)
 
     if not configMap:
@@ -41,27 +55,44 @@ def RunEditTransferRules(sender, event):
     xferRulesFile = ReadConfig.getConfigVal(configMap, ReadConfig.TRANSFER_RULES_FILE, report=None, giveError=True)
 
     if not xferRulesFile or os.path.exists(xferRulesFile) == False:
-
-        MessageBox.Show(f'Transfer rule file: {xferRulesFile} does not exist.', 'Not Found Error', MessageBoxButtons.OK)
+        MessageBox.Show(
+            _translate("FLExTransMenu", "Transfer rule file: {xferRulesFile} does not exist.").format(
+                xferRulesFile=xferRulesFile
+            ),
+            _translate("FLExTransMenu", "Not Found Error"),
+            MessageBoxButtons.OK,
+        )
         return
 
-    progFilesFolder = os.environ['ProgramFiles(x86)']
-    xxe = progFilesFolder + '\\XMLmind_XML_Editor\\bin\\xxe.exe'
+    progFilesFolder = os.environ["ProgramFiles(x86)"]
+    xxe = progFilesFolder + "\\XMLmind_XML_Editor\\bin\\xxe.exe"
     call([xxe, xferRulesFile])
 
-def RunHelp(sender, event):
 
+def RunHelp(sender, event):
     HelpFile = os.path.join(HELP_DIR, "UserDoc.htm")
     os.startfile(HelpFile)
 
+
 def RunAbout(sender, event):
+    MessageBox.Show(
+        _translate("FLExTransMenu", "{name} version {version}\n\nsoftware.sil.org/flextrans").format(
+            name=Version.Name, version=Version.Version
+        ),
+        _translate("FLExTransMenu", "About FLExTrans"),
+        MessageBoxButtons.OK,
+    )
 
-    MessageBox.Show(f"{Version.Name} version {Version.Version}\n\nsoftware.sil.org/flextrans", "About FLExTrans", MessageBoxButtons.OK)
-          
-customMenu = ("FLExTrans",
-              [(RunHelp, "Help", Shortcut.CtrlH, None),
-               (RunSettings, "Settings", Shortcut.CtrlS, None),
-               (RunEditTransferRules, "Edit Transfer Rules", Shortcut.CtrlT, None),
-               (RunAbout, "About", None, "About FLExTrans"),
-               ])
 
+customMenu = (
+    "FLExTrans",
+    [
+        (RunHelp, _translate("FLExTransMenu", "Help"), Shortcut.CtrlH, None),
+        (RunSettings, _translate("FLExTransMenu", "Settings"), Shortcut.CtrlS, None),
+        (RunEditTransferRules, _translate("FLExTransMenu", "Edit Transfer Rules"), Shortcut.CtrlT, None),
+        (RunAbout, _translate("FLExTransMenu", "About"), None, _translate("FLExTransMenu", "About FLExTrans")),
+    ],
+)
+
+app.quit()
+del app
---
+++ Dev/TopLevel/translations/FLExTransMenu_de.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FLExTransMenu</name>
+    <message>
+        <location filename="../FLExTransMenu.py" line="49"/>
+        <source>Transfer rule file: {xferRulesFile} does not exist.</source>
+        <translation>Die Übertragungsregeldatei: {xferRulesFile} existiert nicht.</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="49"/>
+        <source>Not Found Error</source>
+        <translation>Nicht gefunden Fehler</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="69"/>
+        <source>{name} version {version}
+
+software.sil.org/flextrans</source>
+        <translation>{name} Version {version}
+
+software.sil.org/flextrans</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="78"/>
+        <source>About FLExTrans</source>
+        <translation>Über FLExTrans</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="78"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="78"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="78"/>
+        <source>Edit Transfer Rules</source>
+        <translation>Übertragungsregeln bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../FLExTransMenu.py" line="78"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+</context>
+</TS>
