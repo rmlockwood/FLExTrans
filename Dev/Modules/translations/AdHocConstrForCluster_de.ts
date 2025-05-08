dfb36f8480fd14098fd9ff63e537738b7f063e33
R Lockwood <rlbooks@web.de>
2025-05-08 21:41:56 +0200

    save this

---
+++ Dev/Modules/translations/AdHocConstrForCluster_de.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AdHocConstrForCluster</name>
+    <message>
+        <location filename="../AdHocConstrForCluster.py" line="117"/>
+        <source>This new group name will be created in all the selected cluster projects.</source>
+        <translation>Dieser neue Gruppenname wird in allen ausgewählten Cluster-Projekten erstellt.</translation>
+    </message>
+    <message>
+        <location filename="../AdHocConstrForCluster.py" line="120"/>
+        <source>Group Name:</source>
+        <translation>Gruppenname:</translation>
+    </message>
+    <message>
+        <location filename="../AdHocConstrForCluster.py" line="125"/>
+        <source>Group Description:</source>
+        <translation>Gruppenbeschreibung:</translation>
+    </message>
+</context>
+</TS>
