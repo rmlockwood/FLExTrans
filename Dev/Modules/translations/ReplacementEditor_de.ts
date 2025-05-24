f0b87dbb36469e37b3444d6827496216cfc2ca6e
R Lockwood <rlbooks@web.de>
2025-05-24 09:37:55 +0200

    translations

---
+++ Dev/Modules/translations/ReplacementEditor_de.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ReplacementEditor</name>
+    <message>
+        <location filename="../ReplacementEditor.py" line="79"/>
+        <source>Edit manual overrides for the bilingual dictionary.</source>
+        <translation>Manuelle Überschreibungen für das zweisprachige Wörterbuch bearbeiten.</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="81"/>
+        <source>This module provides an interface for editing the replacement dictionary
+which allows you to override the links created by the Sense Linker Tool
+in the presence of particular affixes. For example, if you have a noun that
+is generally translated one way, but has a different translation in the
+vocative singular, you can specify that here.</source>
+        <translation type="unfinished">Dieses Modul bietet eine Schnittstelle zum Bearbeiten des Ersetzungswörterbuchs,
+mit dem Sie die vom Sense Linker Tool erstellten Verknüpfungen
+bei bestimmten Affixen überschreiben können. Zum Beispiel, wenn Sie ein Substantiv haben,
+das normalerweise auf eine Weise übersetzt wird, aber im Vokativ Singular eine andere Übersetzung hat,
+können Sie dies hier angeben.</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="545"/>
+        <source>There are unsaved changes.</source>
+        <translation>Es gibt ungespeicherte Änderungen.</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="560"/>
+        <source>The following sets of rows are identical on the source side and only the first one will have any effect:
+</source>
+        <translation type="unfinished">Die folgenden Zeilensätze sind auf der Quellseite identisch, und nur der erste wird eine Wirkung haben:
+</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="562"/>
+        <source>The following rows have no affixes and thus are redundant with the links created by Sense Linker Tool: </source>
+        <translation>Die folgenden Zeilen haben keine Affixe und sind daher redundant mit den vom Sense Linker Tool erstellten Verknüpfungen: </translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="565"/>
+        <source>Useless Lines:</source>
+        <translation>Nutzlose Zeilen:</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="577"/>
+        <source>Replacement dictionary file saved.</source>
+        <translation>Ersetzungswörterbuchdatei gespeichert.</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="583"/>
+        <source>Unsaved Changes</source>
+        <translation>Ungespeicherte Änderungen</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="583"/>
+        <source>Save changes before exiting?</source>
+        <translation>Änderungen vor dem Beenden speichern?</translation>
+    </message>
+    <message>
+        <location filename="../ReplacementEditor.py" line="604"/>
+        <source>A value for {configValue} not found in the configuration file.</source>
+        <translation>Ein Wert für {configValue} wurde in der Konfigurationsdatei nicht gefunden.</translation>
+    </message>
+</context>
+</TS>
