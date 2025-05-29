4cf0d58609a5b8a1753a5ce891bd88e38f0a186b
R Lockwood <rlbooks@web.de>
2025-05-29 20:51:50 +0200

    reference .py file instead of lui

--- Dev/Lib/Windows/translations/ClusterAdHoc_es.ts
+++ Dev/Lib/Windows/translations/ClusterAdHoc_es.ts
@@ -4,52 +4,52 @@
 <context>
     <name>AdHocMainWindow</name>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="33"/>
+        <location filename="../ClusterAdHoc.py" line="123"/>
         <source>Source Project</source>
         <translation>Proyecto fuente</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="59"/>
+        <location filename="../ClusterAdHoc.py" line="124"/>
         <source>Add</source>
         <translation>Agregar</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="72"/>
+        <location filename="../ClusterAdHoc.py" line="125"/>
         <source>Close</source>
         <translation>Cerrar</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="95"/>
+        <location filename="../ClusterAdHoc.py" line="126"/>
         <source>Ad Hoc Group</source>
         <translation>Grupo ad hoc</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="121"/>
+        <location filename="../ClusterAdHoc.py" line="127"/>
         <source>Ad Hoc Type</source>
         <translation>Tipo ad hoc</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="147"/>
+        <location filename="../ClusterAdHoc.py" line="128"/>
         <source>Key Morpheme/Allomorph</source>
         <translation>Morfema clave/Alomorfo</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="176"/>
+        <location filename="../ClusterAdHoc.py" line="129"/>
         <source>Other Morphemes/Allomorphs</source>
         <translation>Otros morfemas/Alomorfos</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="228"/>
+        <location filename="../ClusterAdHoc.py" line="130"/>
         <source>Cannot Occur</source>
         <translation>No puede ocurrir</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="270"/>
+        <location filename="../ClusterAdHoc.py" line="131"/>
         <source>Cluster Projects to Apply to</source>
         <translation>Proyectos de clúster a los que aplicar</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.ui" line="296"/>
+        <location filename="../ClusterAdHoc.py" line="132"/>
         <source>New Group</source>
         <translation>Nuevo grupo</translation>
     </message>
