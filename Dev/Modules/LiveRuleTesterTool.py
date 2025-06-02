ca384da75131b34265d545aea52725f3721bad96
R Lockwood <rlbooks@web.de>
2025-06-02 11:46:17 +0200

    Improved exception handling for HermitCrab DLL.

--- Dev/Modules/LiveRuleTesterTool.py
+++ Dev/Modules/LiveRuleTesterTool.py
@@ -5,6 +5,9 @@
 #   SIL International
 #   7/2/16
 #
+#   Version 3.13.3 - 6/2/25 - Ron Lockwood
+#    Improved exception handling around use of the HermitCrab DLL.
+# 
 #   Version 3.13.2 - 4/9/25 - Ron Lockwood
 #    Delete non-sentence-ending punctuation from the synthesis result before adding it to the testbed.
 #    Also, apply Text Out rules to the sentence-ending punctuation if necessary.
@@ -237,7 +240,7 @@ import FTPaths
 # Documentation that the user sees:
 
 docs = {FTM_Name       : "Live Rule Tester Tool",
-        FTM_Version    : "3.13.2",
+        FTM_Version    : "3.13.3",
         FTM_ModifiesDB : False,
         FTM_Synopsis   : "Test transfer rules and synthesis live against specific words.",
         FTM_Help   : "",
@@ -1193,7 +1196,15 @@ class Main(QMainWindow):
 
                 # Change to the Fieldworks folder for doing the dll operations
                 fieldworksDir = os.getenv(ENVIR_VAR_FIELDWORKSDIR)
-                os.chdir(fieldworksDir)
+
+                try:
+                    os.chdir(fieldworksDir)
+
+                except OSError as e:
+
+                    QMessageBox.warning(self, 'Directory Error', 'Could not change to the Fieldworks directory: {fieldworksDir}. Error: {e}'.format)
+                    self.unsetCursor()
+                    return
 
                 # Import the clr module from pythonnet
                 import clr 
@@ -1203,7 +1214,14 @@ class Main(QMainWindow):
                 from SIL.HCSynthByGloss2 import HCSynthByGlossDll # type: ignore
 
                 # Initialize the object with the output file name
-                self.HCdllObj = HCSynthByGlossDll(self.surfaceFormsFile)
+                try:
+                    self.HCdllObj = HCSynthByGlossDll(self.surfaceFormsFile)
+
+                except Exception as e:
+
+                    QMessageBox.warning(self, 'DLL Error', 'An exception occurred. Could not initialize the HermitCrab synthesis DLL. Error: {e}'.format(e=e))
+                    self.unsetCursor()
+                    return
 
         ## CATALOG
         # Catalog all the target affixes
