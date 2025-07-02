4931613b9c6a3ee724163ddd81d090877acfe5c5
R Lockwood <rlbooks@web.de>
2025-07-02 10:53:54 +0200

    some tweaks to German

--- Dev/Lib/translations/TextInOutUtils_de.ts
+++ Dev/Lib/translations/TextInOutUtils_de.ts
@@ -4,34 +4,34 @@
 <context>
     <name>TextInOutUtils</name>
     <message>
-        <location filename="../TextInOutUtils.py" line="237"/>
-        <source>Test stopped on failure of rule: {ruleString}</source>
-        <translation>Test wegen Regelfehlers abgebrochen: {ruleString}</translation>
+      <location filename="../TextInOutUtils.py" line="237"/>
+      <source>Test stopped on failure of rule: {ruleString}</source>
+      <translation>Test wegen Regelfehlers abgebrochen: {ruleString}</translation>
     </message>
     <message>
-        <location filename="../TextInOutUtils.py" line="329"/>
-        <source>Run the {wildebeest} cleanup tool</source>
-        <translation>Führen Sie das {wildebeest}-Bereinigungstool aus</translation>
+      <location filename="../TextInOutUtils.py" line="329"/>
+      <source>Run the {wildebeest} cleanup tool</source>
+      <translation>Führen Sie das {wildebeest}-Bereinigungstool aus</translation>
     </message>
     <message>
-        <location filename="../TextInOutUtils.py" line="330"/>
-        <source>help</source>
-        <translation>Hilfe</translation>
+      <location filename="../TextInOutUtils.py" line="330"/>
+      <source>help</source>
+      <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="../TextInOutUtils.py" line="528"/>
-        <source>Test stopped on failure of rule {ruleNumber}: {ruleString}</source>
-        <translation>Test wegen Nichterfüllung der Regel {ruleNumber} abgebrochen: {ruleString}</translation>
+      <location filename="../TextInOutUtils.py" line="528"/>
+      <source>Test stopped on failure of rule {ruleNumber}: {ruleString}</source>
+      <translation>Test wegen Nichterfüllung der Regel {ruleNumber} abgebrochen: {ruleString}</translation>
     </message>
     <message>
-        <location filename="../TextInOutUtils.py" line="697"/>
-        <source>Invalid File</source>
-        <translation>Ungültige Datei</translation>
+      <location filename="../TextInOutUtils.py" line="697"/>
+      <source>Invalid File</source>
+      <translation>Ungültige Datei</translation>
     </message>
     <message>
-        <location filename="../TextInOutUtils.py" line="715"/>
-        <source>Invalid Rules File</source>
-        <translation>Ungültige Regeldatei</translation>
+      <location filename="../TextInOutUtils.py" line="715"/>
+      <source>Invalid Rules File</source>
+      <translation>Ungültige Regeldatei</translation>
     </message>
-</context>
+  </context>
 </TS>
