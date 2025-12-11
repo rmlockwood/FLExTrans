8ccc816bfdd943a302ae2589dd3f2a66e56d8467
R Lockwood <rlbooks@web.de>
2025-12-11 14:44:30 +0100

    bump version & comment

--- Dev/Modules/LiveRuleTesterTool.py
+++ Dev/Modules/LiveRuleTesterTool.py
@@ -5,6 +5,10 @@
 #   SIL International
 #   7/2/16
 #
+#   Version 3.14.15 - 12/11/25 - Ron Lockwood
+#    Add a stylesheet to pad all but a few buttons with space left and right.
+#    This makes the buttons look less cramped when the window is resized.
+#
 #   Version 3.14.14 - 11/28/25 - Ron Lockwood
 #    Use a text edit for warnings so multiple lines can be shown with a scroll bar.
 #
@@ -302,7 +306,7 @@ librariesToTranslate = ['ReadConfig', 'Utils', 'Mixpanel', 'LiveRuleTester', 'Te
 #----------------------------------------------------------------
 # Documentation that the user sees:
 docs = {FTM_Name       : _translate("LiveRuleTesterTool", "Live Rule Tester Tool"),
-        FTM_Version    : "3.14.14",
+        FTM_Version    : "3.14.15",
         FTM_ModifiesDB : False,
         FTM_Synopsis   : _translate("LiveRuleTesterTool", "Test transfer rules and synthesis live against specific words."),
         FTM_Help       : "", 
