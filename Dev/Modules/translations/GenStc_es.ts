a5d93e128ba129f7e37f7cfc340dc97cf36d0c33
R Lockwood <rlbooks@web.de>
2026-01-26 08:28:23 +0100

    Made GenStc localizable

---
+++ Dev/Modules/translations/GenStc_es.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>GenStc</name>
+    <message>
+        <location filename="../GenStc.py" line="68"/>
+        <source>Generate Sentences from Model</source>
+        <translation>Generar frases a partir de un modelo</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="73"/>
+        <source>Iterate over certain grammatical categories in a model sentence to produce variations.</source>
+        <translation>Iterar sobre ciertas categorías gramaticales en una frase modelo para producir variaciones.</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="170"/>
+        <source>Problem with output file path: {filePath}. Please check configuration.</source>
+        <translation>Problema con la ruta del archivo de salida: {filePath}. Por favor revise la configuración.</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="186"/>
+        <source>Text not found: {sourceTextName}</source>
+        <translation>Texto no encontrado: {sourceTextName}</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="643"/>
+        <source>Found {stcCount} sentences in the text</source>
+        <translation>Se encontraron {stcCount} frases en el texto</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="272"/>
+        <source>valid pos: {valid_pos}</source>
+        <translation>pos válida: {valid_pos}</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="563"/>
+        <source>custom field: {customField}</source>
+        <translation>campo personalizado: {customField}</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="570"/>
+        <source>Translation file check: {translationFile}</source>
+        <translation>Verificación del archivo de traducción: {translationFile}</translation>
+    </message>
+    <message>
+        <location filename="../GenStc.py" line="729"/>
+        <source>Export of {sourceName} complete.</source>
+        <translation>Exportación de {sourceName} completada.</translation>
+    </message>
+</context>
+</TS>
