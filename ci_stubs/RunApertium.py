0b0e9048689a657eddbf21d56c67f4909a6ef43d
R Lockwood <rlbooks@web.de>
2026-02-06 17:49:24 +0100

    more CI changes

---
+++ ci_stubs/RunApertium.py
@@ -0,0 +1,13 @@
+# Stub for RunApertium module
+# Provides the docs dictionary needed by dependent modules
+
+from flextoolslib import FTM_Name, FTM_Version, FTM_ModifiesDB, FTM_Synopsis, FTM_Help, FTM_Description
+
+docs = {
+    FTM_Name: "Run Apertium",
+    FTM_Version: "3.15",
+    FTM_ModifiesDB: False,
+    FTM_Synopsis: "Run Apertium transfer rules",
+    FTM_Help: "",
+    FTM_Description: "Run Apertium tool",
+}
