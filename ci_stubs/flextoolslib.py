62f4f541aec55faf70ef4687c074bfa7c5ae83b5
R Lockwood <rlbooks@web.de>
2026-02-06 17:29:45 +0100

    Update flextoolslib.py

--- ci_stubs/flextoolslib.py
+++ ci_stubs/flextoolslib.py
@@ -9,8 +9,19 @@ class FTConfig:
 
 class FlexToolsModuleClass:
     """Mock FlexToolsModuleClass - base class for FlexTools modules"""
-    def __init__(self):
-        pass
+    def __init__(self, runFunction=None, docs=None):
+        self.runFunction = runFunction
+        self.docs = docs or {}
+    
+    def Help(self):
+        """Mock Help method - prints documentation"""
+        if self.docs:
+            print(self.docs)
 
-# Export for convenience
-FTM_Name = 'FTM'
+# Export constants for convenience
+FTM_Name = 'FTM_Name'
+FTM_Version = 'FTM_Version'
+FTM_ModifiesDB = 'FTM_ModifiesDB'
+FTM_Synopsis = 'FTM_Synopsis'
+FTM_Help = 'FTM_Help'
+FTM_Description = 'FTM_Description'
