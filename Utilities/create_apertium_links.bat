48488100f1e3987df009bcf32dd5a038d586f969
R Lockwood <rlbooks@web.de>
2026-02-11 11:23:42 +0100

    Update create_apertium_links.bat

--- Utilities/create_apertium_links.bat
+++ Utilities/create_apertium_links.bat
@@ -1,30 +1,30 @@
 cd "C:\Data\FLExTrans\Dev\Active Projects\FlexTools\Tools"
-mklink libwinpthread-1.dll                 c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libwinpthread-1.dll
-mklink libxml2-2.dll                       c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libxml2-2.dll
-mklink libstdc++-6.dll                     c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libstdc++-6.dll
-mklink liblzma-5.dll                       c:\users\rlboo\GitHub\flextrans\Apertium4Windows\liblzma-5.dll
-mklink liblttoolbox.dll                    c:\users\rlboo\GitHub\flextrans\Apertium4Windows\liblttoolbox.dll
-mklink icudt66.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icudt66.dll
-mklink icuin66.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuin66.dll
-mklink icuio66.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuio66.dll
-mklink icuuc66.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuuc66.dll
-mklink icudt74.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icudt74.dll
-mklink icuin74.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuin74.dll
-mklink icuio74.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuio74.dll
-mklink icuuc74.dll                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\icuuc74.dll
-mklink libgcc_s_seh-1.dll                  c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libgcc_s_seh-1.dll
-mklink libgcc_s_sjlj-1.dll                 c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libgcc_s_sjlj-1.dll
-mklink libiconv-2.dll                      c:\users\rlboo\GitHub\flextrans\Apertium4Windows\libiconv-2.dll
-mklink zlib1.dll                           c:\users\rlboo\GitHub\flextrans\Apertium4Windows\zlib1.dll
-mklink apertium-transfer.exe               c:\users\rlboo\GitHub\flextrans\Apertium4Windows\apertium-transfer.exe
-mklink apertium-preprocess-transfer.exe    c:\users\rlboo\GitHub\flextrans\Apertium4Windows\apertium-preprocess-transfer.exe
-mklink apertium-interchunk.exe             c:\users\rlboo\GitHub\flextrans\Apertium4Windows\apertium-interchunk.exe
-mklink lt-comp.exe                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\lt-comp.exe
-mklink lt-proc.exe                         c:\users\rlboo\GitHub\flextrans\Apertium4Windows\lt-proc.exe
-mklink apertium-postchunk.exe              c:\users\rlboo\GitHub\flextrans\Apertium4Windows\apertium-postchunk.exe
-       
-mklink make.exe                            c:\users\rlboo\GitHub\flextrans\Make4Windows\make.exe
-mklink libiconv2.dll                       c:\users\rlboo\GitHub\flextrans\Make4Windows\libiconv2.dll
-mklink libintl3.dll                        c:\users\rlboo\GitHub\flextrans\Make4Windows\libintl3.dll
+mklink libwinpthread-1.dll                 c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libwinpthread-1.dll
+mklink libxml2-2.dll                       c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libxml2-2.dll
+mklink libstdc++-6.dll                     c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libstdc++-6.dll
+mklink liblzma-5.dll                       c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\liblzma-5.dll
+mklink liblttoolbox.dll                    c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\liblttoolbox.dll
+mklink icudt66.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icudt66.dll
+mklink icuin66.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuin66.dll
+mklink icuio66.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuio66.dll
+mklink icuuc66.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuuc66.dll
+mklink icudt74.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icudt74.dll
+mklink icuin74.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuin74.dll
+mklink icuio74.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuio74.dll
+mklink icuuc74.dll                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\icuuc74.dll
+mklink libgcc_s_seh-1.dll                  c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libgcc_s_seh-1.dll
+mklink libgcc_s_sjlj-1.dll                 c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libgcc_s_sjlj-1.dll
+mklink libiconv-2.dll                      c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\libiconv-2.dll
+mklink zlib1.dll                           c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\zlib1.dll
+mklink apertium-transfer.exe               c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\apertium-transfer.exe
+mklink apertium-preprocess-transfer.exe    c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\apertium-preprocess-transfer.exe
+mklink apertium-interchunk.exe             c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\apertium-interchunk.exe
+mklink lt-comp.exe                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\lt-comp.exe
+mklink lt-proc.exe                         c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\lt-proc.exe
+mklink apertium-postchunk.exe              c:\users\rlboo\GitHub\flextrans\InstallerResources\Apertium4Windows\apertium-postchunk.exe
+																		   
+mklink make.exe                            c:\users\rlboo\GitHub\flextrans\InstallerResources\Make4Windows\make.exe
+mklink libiconv2.dll                       c:\users\rlboo\GitHub\flextrans\InstallerResources\Make4Windows\libiconv2.dll
+mklink libintl3.dll                        c:\users\rlboo\GitHub\flextrans\InstallerResources\Make4Windows\libintl3.dll
 
 pause
