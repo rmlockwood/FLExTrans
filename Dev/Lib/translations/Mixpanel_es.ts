cf650bd737d52c22d9889b512eefcd2e64b1c933
R Lockwood <rlbooks@web.de>
2026-02-17 09:29:07 +0100

    translate dialog title

--- Dev/Lib/translations/Mixpanel_es.ts
+++ Dev/Lib/translations/Mixpanel_es.ts
@@ -8,5 +8,10 @@
         <source>FLExTrans would like to send usage statistics to FLExTrans developers. No personally identifiable information is sent. These anonymous statistics will help with future development. Do you want to opt out of sending usage statistics?</source>
         <translation>FLExTrans desea enviar estadísticas de uso a los desarrolladores de FLExTrans. No se envía información personal identificable. Estas estadísticas anónimas ayudarán con el desarrollo futuro. ¿Desea optar por no enviar estadísticas de uso?</translation>
     </message>
+    <message>
+        <location filename="../Mixpanel.py" line="54"/>
+        <source>FLExTrans Usage</source>
+        <translation>Uso de FLExTrans</translation>
+    </message>
 </context>
 </TS>
--- Dev/Lib/translations/Mixpanel_fr.ts
+++ Dev/Lib/translations/Mixpanel_fr.ts
@@ -8,5 +8,10 @@
         <source>FLExTrans would like to send usage statistics to FLExTrans developers. No personally identifiable information is sent. These anonymous statistics will help with future development. Do you want to opt out of sending usage statistics?</source>
         <translation>FLExTrans souhaiterait envoyer des statistiques d'utilisation aux développeurs de FLExTrans. Aucune information personnellement identifiable n'est envoyée. Ces statistiques anonymes aideront au développement futur. Voulez-vous refuser l'envoi de statistiques d'utilisation ?</translation>
     </message>
+    <message>
+        <location filename="../Mixpanel.py" line="54"/>
+        <source>FLExTrans Usage</source>
+        <translation>Utilisation de FLExTrans</translation>
+    </message>
 </context>
 </TS>
