a40a29adc660884cd471f6e0c4073138e789184f
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:46 +0200

    New translations textinrules.ts (Spanish)

--- Dev/Modules/translations/TextInRules_es.ts
+++ Dev/Modules/translations/TextInRules_es.ts
@@ -6,19 +6,19 @@
     <message>
       <location filename="../TextInRules.py" line="67"/>
       <source>Text In Rules</source>
-      <translation type="unfinished">Text In Rules</translation>
+      <translation>Reglas de entrada de texto</translation>
     </message>
     <message>
       <location filename="../TextInRules.py" line="70"/>
       <source>Define and test a set of Paratext-import search and replace operations.</source>
-      <translation type="unfinished">Define and test a set of Paratext-import search and replace operations.</translation>
+      <translation>Definir y probar un conjunto de operaciones de búsqueda y reemplazo para la importación de Paratext.</translation>
     </message>
     <message>
       <location filename="../TextInRules.py" line="72"/>
       <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 Paratext. Regular expressions and Wildebeest normalization can be used if desired.</source>
-      <translation type="unfinished">This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
-Paratext. Regular expressions and Wildebeest normalization can be used if desired.</translation>
+      <translation>Este módulo se utiliza para definir y probar un conjunto de operaciones de búsqueda y reemplazo que se utilizarán para corregir el texto que proviene de 
+Paratext. Se pueden usar expresiones regulares y la normalización de Wildebeest si se desea.</translation>
     </message>
   </context>
 </TS>
