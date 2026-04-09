98fe5784862755a4689e8d6d004e488a56f3627a
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:17:59 +0200

    New translations endtestbed.ts (French)

--- Dev/Modules/translations/EndTestbed_fr.ts
+++ Dev/Modules/translations/EndTestbed_fr.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
     <name>EndTestbed</name>
     <message>
-        <location filename="../EndTestbed.py" line="73"/>
-        <source>End Testbed</source>
-        <translation>End banc d'essai</translation>
+      <location filename="../EndTestbed.py" line="73"/>
+      <source>End Testbed</source>
+      <translation type="unfinished">End Testbed</translation>
     </message>
     <message>
-        <location filename="../EndTestbed.py" line="78"/>
-        <source>Conclude a testbed log result.</source>
-        <translation>Conclure un résultat de journal du banc d'essai.</translation>
+      <location filename="../EndTestbed.py" line="78"/>
+      <source>Conclude a testbed log result.</source>
+      <translation type="unfinished">Conclude a testbed log result.</translation>
     </message>
     <message>
-        <location filename="../EndTestbed.py" line="114"/>
-        <source>There is a problem with the Synthesis Output File path: {outFileVal}. Please check the configuration file setting.</source>
-        <translation>Il y a un problème avec le chemin du fichier de sortie de synthèse : {outFileVal}. Veuillez vérifier le paramètre du fichier de configuration.</translation>
+      <location filename="../EndTestbed.py" line="114"/>
+      <source>There is a problem with the Synthesis Output File path: {outFileVal}. Please check the configuration file setting.</source>
+      <translation type="unfinished">There is a problem with the Synthesis Output File path: {outFileVal}. Please check the configuration file setting.</translation>
     </message>
     <message>
-        <location filename="../EndTestbed.py" line="132"/>
-        <source>{count} results extracted.</source>
-        <translation>{count} résultats extraits.</translation>
+      <location filename="../EndTestbed.py" line="132"/>
+      <source>{count} results extracted.</source>
+      <translation type="unfinished">{count} results extracted.</translation>
     </message>
-</context>
+  </context>
 </TS>
