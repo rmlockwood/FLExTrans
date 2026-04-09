bb061ecb57d438b15dd2d2913316cb5bf9493f0a
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:49 +0200

    New translations testbedlogviewer.ts (German)

--- Dev/Modules/translations/TestbedLogViewer_de.ts
+++ Dev/Modules/translations/TestbedLogViewer_de.ts
@@ -6,67 +6,67 @@
     <message>
       <location filename="../TestbedLogViewer.py" line="91"/>
       <source>Testbed Log Viewer</source>
-      <translation type="unfinished">Testbed Log Viewer</translation>
+      <translation>Testbed-Log-Viewer</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="94"/>
       <source>View testbed run results.</source>
-      <translation type="unfinished">View testbed run results.</translation>
+      <translation>Testbett-Ausführungsergebnisse anzeigen.</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="96"/>
       <source>View testbed run results. The number of results to display is set by default to 25. Change MAX_RESULTS_TO_DISPLAY to a different value as needed.</source>
-      <translation type="unfinished">View testbed run results. The number of results to display is set by default to 25. Change MAX_RESULTS_TO_DISPLAY to a different value as needed.</translation>
+      <translation>Testbett-Ausführungsergebnisse anzeigen. Die Anzahl der anzuzeigenden Ergebnisse ist standardmäßig auf 25 gesetzt. Ändern Sie MAX_RESULTS_TO_DISPLAY bei Bedarf auf einen anderen Wert.</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="136"/>
       <source>{total} Tests, {failed} Failed, {invalid} Invalid</source>
-      <translation type="unfinished">{total} Tests, {failed} Failed, {invalid} Invalid</translation>
+      <translation>{total} Tests, {failed} Fehlgeschlagen, {invalid} Ungültig</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="169"/>
       <source>Column Count Not Specified!!</source>
-      <translation type="unfinished">Column Count Not Specified!!</translation>
+      <translation>Spaltenanzahl nicht angegeben!!</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="172"/>
       <source>Data gather method not implemented!</source>
-      <translation type="unfinished">Data gather method not implemented!</translation>
+      <translation>Datensammlungsmethode nicht implementiert!</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="195"/>
       <source>Source Lexical Unit(s)</source>
-      <translation type="unfinished">Source Lexical Unit(s)</translation>
+      <translation>Quell-Lexikalische Einheit(en)</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="197"/>
       <source>Expected Result</source>
-      <translation type="unfinished">Expected Result</translation>
+      <translation>Erwartetes Ergebnis</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="199"/>
       <source>Actual Result</source>
-      <translation type="unfinished">Actual Result</translation>
+      <translation>Tatsächliches Ergebnis</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="221"/>
       <source>Date format error.</source>
-      <translation type="unfinished">Date format error.</translation>
+      <translation>Datumsformat-Fehler.</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="240"/>
       <source>Test completed in 1 minute and {seconds} seconds</source>
-      <translation type="unfinished">Test completed in 1 minute and {seconds} seconds</translation>
+      <translation>Test abgeschlossen in 1 Minute und {seconds} Sekunden</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="242"/>
       <source>Test completed in {minutes} minute and {seconds} seconds</source>
-      <translation type="unfinished">Test completed in {minutes} minute and {seconds} seconds</translation>
+      <translation>Test abgeschlossen in {minutes} Minuten und {seconds} Sekunden</translation>
     </message>
     <message>
       <location filename="../TestbedLogViewer.py" line="644"/>
       <source>Testbed file: {testbedPath} does not exist. Please add tests to the testbed.</source>
-      <translation type="unfinished">Testbed file: {testbedPath} does not exist. Please add tests to the testbed.</translation>
+      <translation>Testbett-Datei: {testbedPath} existiert nicht. Bitte fügen Sie Tests zum Testbett hinzu.</translation>
     </message>
   </context>
 </TS>
