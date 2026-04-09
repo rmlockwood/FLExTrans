4e7d9e4321a3f9c6111cb4416bf82f4089962ddf
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:23 +0200

    New translations dosynthesis.ts (German)

--- Dev/Modules/translations/DoSynthesis_de.ts
+++ Dev/Modules/translations/DoSynthesis_de.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../DoSynthesis.py" line="70"/>
       <source>Synthesize Text</source>
-      <translation type="unfinished">Synthesize Text</translation>
+      <translation>Text synthetisieren</translation>
     </message>
     <message>
       <location filename="../DoSynthesis.py" line="73"/>
       <source>Synthesizes the target text with either STAMP or HermitCrab.</source>
-      <translation type="unfinished">Synthesizes the target text with either STAMP or HermitCrab.</translation>
+      <translation>Synthetisiert den Zieltext entweder mit STAMP oder HermitCrab.</translation>
     </message>
     <message>
       <location filename="../DoSynthesis.py" line="75"/>
@@ -21,27 +21,27 @@ then the following information from the {hermitCrabModule} module applies: {herm
 
 If in the settings you select 'No' for 'Use HermitCrab synthesis?',
 then the following information from the {stampModule} module applies: {stampDescription}</source>
-      <translation type="unfinished">This module synthesizes the target text. If in the settings you select 'Yes' for 'Use HermitCrab synthesis?',
-then the following information from the {hermitCrabModule} module applies: {hermitCrabDescription}
+      <translation>Dieses Modul synthetisiert den Zieltext. Wenn Sie in den Einstellungen 'Ja' für 'HermitCrab-Synthese verwenden?' auswählen,
+dann gilt die folgende Information aus dem Modul {hermitCrabModule}: {hermitCrabDescription}
 
 
-If in the settings you select 'No' for 'Use HermitCrab synthesis?',
-then the following information from the {stampModule} module applies: {stampDescription}</translation>
+Wenn Sie in den Einstellungen 'Nein' für 'HermitCrab-Synthese verwenden?' auswählen,
+dann gilt die folgende Information aus dem Modul {stampModule}: {stampDescription}</translation>
     </message>
     <message>
       <location filename="../DoSynthesis.py" line="102"/>
       <source>Failed to read the configuration file.</source>
-      <translation type="unfinished">Failed to read the configuration file.</translation>
+      <translation>Fehler beim Lesen der Konfigurationsdatei.</translation>
     </message>
     <message>
       <location filename="../DoSynthesis.py" line="112"/>
       <source>Using HermitCrab for synthesis.</source>
-      <translation type="unfinished">Using HermitCrab for synthesis.</translation>
+      <translation>HermitCrab wird für die Synthese verwendet.</translation>
     </message>
     <message>
       <location filename="../DoSynthesis.py" line="118"/>
       <source>Using STAMP for synthesis.</source>
-      <translation type="unfinished">Using STAMP for synthesis.</translation>
+      <translation>STAMP wird für die Synthese verwendet.</translation>
     </message>
   </context>
 </TS>
