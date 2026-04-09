453234874d5058096dbc93b45fa4acf503e20e4f
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:19:57 +0200

    New translations dosynthesis.ts (German)

--- Dev/Modules/translations/DoSynthesis_de.ts
+++ Dev/Modules/translations/DoSynthesis_de.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
     <name>DoSynthesis</name>
     <message>
-      <location filename="../DoSynthesis.py" line="118"/>
+      <location filename="../DoSynthesis.py" line="70"/>
       <source>Synthesize Text</source>
-      <translation>Text synthetisieren</translation>
+      <translation type="unfinished">Synthesize Text</translation>
     </message>
     <message>
-        <location filename="../DoSynthesis.py" line="64"/>
-        <source>Synthesizes the target text with either STAMP or HermitCrab.</source>
-        <translation>Synthetisiert den Zieltext entweder mit STAMP oder HermitCrab.</translation>
+      <location filename="../DoSynthesis.py" line="73"/>
+      <source>Synthesizes the target text with either STAMP or HermitCrab.</source>
+      <translation type="unfinished">Synthesizes the target text with either STAMP or HermitCrab.</translation>
     </message>
     <message>
-        <location filename="../DoSynthesis.py" line="66"/>
-        <source>This module synthesizes the target text. If in the settings you select 'Yes' for 'Use HermitCrab synthesis?',
+      <location filename="../DoSynthesis.py" line="75"/>
+      <source>This module synthesizes the target text. If in the settings you select 'Yes' for 'Use HermitCrab synthesis?',
 then the following information from the {hermitCrabModule} module applies: {hermitCrabDescription}
 
 
 If in the settings you select 'No' for 'Use HermitCrab synthesis?',
 then the following information from the {stampModule} module applies: {stampDescription}</source>
-        <translation>Dieses Modul synthetisiert den Zieltext. Wenn Sie in den Einstellungen 'Ja' für 'HermitCrab-Synthese verwenden?' auswählen,
-dann gilt die folgende Information aus dem Modul {hermitCrabModule}: {hermitCrabDescription}
+      <translation type="unfinished">This module synthesizes the target text. If in the settings you select 'Yes' for 'Use HermitCrab synthesis?',
+then the following information from the {hermitCrabModule} module applies: {hermitCrabDescription}
 
 
-Wenn Sie in den Einstellungen 'Nein' für 'HermitCrab-Synthese verwenden?' auswählen,
-dann gilt die folgende Information aus dem Modul {stampModule}: {stampDescription}</translation>
+If in the settings you select 'No' for 'Use HermitCrab synthesis?',
+then the following information from the {stampModule} module applies: {stampDescription}</translation>
     </message>
     <message>
-        <location filename="../DoSynthesis.py" line="89"/>
-        <source>Failed to read the configuration file.</source>
-        <translation>Fehler beim Lesen der Konfigurationsdatei.</translation>
+      <location filename="../DoSynthesis.py" line="102"/>
+      <source>Failed to read the configuration file.</source>
+      <translation type="unfinished">Failed to read the configuration file.</translation>
     </message>
     <message>
-        <location filename="../DoSynthesis.py" line="99"/>
-        <source>Using HermitCrab for synthesis.</source>
-        <translation>HermitCrab wird für die Synthese verwendet.</translation>
+      <location filename="../DoSynthesis.py" line="112"/>
+      <source>Using HermitCrab for synthesis.</source>
+      <translation type="unfinished">Using HermitCrab for synthesis.</translation>
     </message>
     <message>
-        <location filename="../DoSynthesis.py" line="105"/>
-        <source>Using STAMP for synthesis.</source>
-        <translation>STAMP wird für die Synthese verwendet.</translation>
+      <location filename="../DoSynthesis.py" line="118"/>
+      <source>Using STAMP for synthesis.</source>
+      <translation type="unfinished">Using STAMP for synthesis.</translation>
     </message>
-</context>
+  </context>
 </TS>
