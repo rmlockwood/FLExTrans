9523a8ced8343795fa225e6b3178b8c768c45940
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:04:18 +0200

    New translations exporttoparatext.ts (French)

--- Dev/Modules/translations/ExportToParatext_fr.ts
+++ Dev/Modules/translations/ExportToParatext_fr.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../ExportToParatext.py" line="116"/>
       <source>Export FLExTrans Draft to Paratext</source>
-      <translation type="unfinished">Export FLExTrans Draft to Paratext</translation>
+      <translation>Exporter le brouillon FLExTrans vers Paratext</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="119"/>
       <source>Export the draft that has been translated with FLExTrans to Paratext.</source>
-      <translation type="unfinished">Export the draft that has been translated with FLExTrans to Paratext.</translation>
+      <translation>Exporter le brouillon qui a été traduit avec FLExTrans vers Paratext.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="121"/>
@@ -19,35 +19,35 @@
 After chapters have been synthesized with the {synthText} module, the draft resides in the file specified
 by the setting 'Target Output Synthesis File' (typically called 'target_text-syn.txt'). This module
 takes the draft in this file and copies the chapters into Paratext to the project specified.</source>
-      <translation type="unfinished">
-After chapters have been synthesized with the {synthText} module, the draft resides in the file specified
-by the setting 'Target Output Synthesis File' (typically called 'target_text-syn.txt'). This module
-takes the draft in this file and copies the chapters into Paratext to the project specified.</translation>
+      <translation>
+Après que les chapitres ont été synthétisés avec le module {synthText}, le brouillon se trouve dans le fichier spécifié
+par le paramètre « Fichier de synthèse de la sortie cible » (généralement appelé « target_text-syn.txt »). Ce module
+prend le brouillon dans ce fichier et copie les chapitres dans Paratext vers le projet spécifié.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="144"/>
       <source>Export Chapters to Paratext</source>
-      <translation type="unfinished">Export Chapters to Paratext</translation>
+      <translation>Exporter les chapitres vers Paratext</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="173"/>
       <source>The text name &quot;{sourceText}&quot; is invalid it should be of the form GEN 01 or Genesis 23-38</source>
-      <translation type="unfinished">The text name &quot;{sourceText}&quot; is invalid it should be of the form GEN 01 or Genesis 23-38</translation>
+      <translation>Le nom du texte « {sourceText} » est invalide, il devrait être de la forme GEN 01 ou Genèse 23-38</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="196"/>
       <source>The book name or abbreviation {book} is invalid. It should match a Paratext book.</source>
-      <translation type="unfinished">The book name or abbreviation {book} is invalid. It should match a Paratext book.</translation>
+      <translation>Le nom ou l'abréviation du livre {book} est invalide. Il devrait correspondre à un livre Paratext.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="275"/>
       <source>Could not find the synthesis file. Have you run the Synthesize Text module? Missing file: {synFile}.</source>
-      <translation type="unfinished">Could not find the synthesis file. Have you run the Synthesize Text module? Missing file: {synFile}.</translation>
+      <translation>Impossible de trouver le fichier de synthèse. Avez-vous exécuté le module Synthétiser le texte ? Fichier manquant : {synFile}.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="291"/>
       <source>Export cancelled.</source>
-      <translation type="unfinished">Export cancelled.</translation>
+      <translation>Exportation annulée.</translation>
     </message>
   </context>
 </TS>
