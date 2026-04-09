8d232a06b53c48390fe51b8f7bb6feab4f4a8b3f
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:04:23 +0200

    New translations fixupsynthtext.ts (French)

--- Dev/Modules/translations/FixUpSynthText_fr.ts
+++ Dev/Modules/translations/FixUpSynthText_fr.ts
@@ -6,41 +6,41 @@
     <message>
       <location filename="../FixUpSynthText.py" line="82"/>
       <source>Fix Up Synthesis Text</source>
-      <translation type="unfinished">Fix Up Synthesis Text</translation>
+      <translation>Corriger le texte de synthèse</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="85"/>
       <source>Run a set of post-synthesis search and replace operations.</source>
-      <translation type="unfinished">Run a set of post-synthesis search and replace operations.</translation>
+      <translation>Exécuter un ensemble d'opérations de recherche et remplacement post-synthèse.</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="87"/>
       <source>This module will run a set of search and replace operations to fix up the text that comes out of
 synthesis. The operations are defined with the {textOutRulesModule} module. The rules are stored in the
 Fix Up Synthesis Text Rules File as specified in the Settings.</source>
-      <translation type="unfinished">This module will run a set of search and replace operations to fix up the text that comes out of
-synthesis. The operations are defined with the {textOutRulesModule} module. The rules are stored in the
-Fix Up Synthesis Text Rules File as specified in the Settings.</translation>
+      <translation>Ce module exécutera un ensemble d'opérations de recherche et remplacement pour corriger le texte qui sort de
+la synthèse. Les opérations sont définies avec le module {textOutRulesModule}. Les règles sont stockées dans le
+fichier de règles de correction du texte de synthèse tel que spécifié dans les paramètres.</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="125"/>
       <source>The rules file: {textOutRulesFile} could not be found. Use the Text Out Rules module to define the rules.</source>
-      <translation type="unfinished">The rules file: {textOutRulesFile} could not be found. Use the Text Out Rules module to define the rules.</translation>
+      <translation>Le fichier de règles : {textOutRulesFile} est introuvable. Utilisez le module Règles du texte en sortie pour définir les règles.</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="138"/>
       <source>The rules file: {textOutRulesFile} has invalid XML data.</source>
-      <translation type="unfinished">The rules file: {textOutRulesFile} has invalid XML data.</translation>
+      <translation>Le fichier de règles : {textOutRulesFile} contient des données XML invalides.</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="146"/>
       <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: '{synthFile}'.</source>
-      <translation type="unfinished">The Synthesize Text module must be run before this one. Could not open the synthesis file: '{synthFile}'.</translation>
+      <translation>Le module Synthétiser le texte doit être exécuté avant celui-ci. Impossible d'ouvrir le fichier de synthèse : « {synthFile} ».</translation>
     </message>
     <message>
       <location filename="../FixUpSynthText.py" line="170"/>
       <source>The synthesis file was fixed using {numRules} 'Text Out' rules.</source>
-      <translation type="unfinished">The synthesis file was fixed using {numRules} 'Text Out' rules.</translation>
+      <translation>Le fichier de synthèse a été corrigé en utilisant {numRules} règles « Texte en sortie ».</translation>
     </message>
   </context>
 </TS>
