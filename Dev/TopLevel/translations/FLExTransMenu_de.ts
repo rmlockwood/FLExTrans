e0897cea41bb50a0b36e702ca9eb7fee53b936d3
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:20:28 +0200

    New translations flextransmenu.ts (German)

--- Dev/TopLevel/translations/FLExTransMenu_de.ts
+++ Dev/TopLevel/translations/FLExTransMenu_de.ts
@@ -1,51 +1,54 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
     <name>FLExTransMenu</name>
     <message>
-        <location filename="../FLExTransMenu.py" line="70"/>
-        <source>Transfer rule file: {xferRulesFile} does not exist.</source>
-        <translation>Die Übertragungsregeldatei: {xferRulesFile} existiert nicht.</translation>
+      <location filename="../FLExTransMenu.py" line="95"/>
+      <source>Transfer rule file: {xferRulesFile} does not exist.</source>
+      <translation>Die Übertragungsregeldatei: {xferRulesFile} existiert nicht.</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="70"/>
-        <source>Not Found Error</source>
-        <translation>Nicht gefunden Fehler</translation>
+      <location filename="../FLExTransMenu.py" line="95"/>
+      <source>Not Found Error</source>
+      <translation>Nicht gefunden Fehler</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="93"/>
-        <source>{name} version {version}
+      <location filename="../FLExTransMenu.py" line="120"/>
+      <source>{name} version {version}
+
+Build {build}, {build_date}
 
 software.sil.org/flextrans</source>
-        <translation>{name} Version {version}
+      <translation type="unfinished">{name} version {version}
+
+Build {build}, {build_date}
 
 software.sil.org/flextrans</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="100"/>
-        <source>About FLExTrans</source>
-        <translation>Über FLExTrans</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>About FLExTrans</source>
+      <translation>Über FLExTrans</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="100"/>
-        <source>Help</source>
-        <translation>Hilfe</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Help</source>
+      <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="100"/>
-        <source>Settings</source>
-        <translation>Einstellungen</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Settings</source>
+      <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="100"/>
-        <source>Edit Transfer Rules</source>
-        <translation>Übertragungsregeln bearbeiten</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Edit Transfer Rules</source>
+      <translation>Übertragungsregeln bearbeiten</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="100"/>
-        <source>About</source>
-        <translation>Über</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>About</source>
+      <translation>Über</translation>
     </message>
-</context>
+  </context>
 </TS>
