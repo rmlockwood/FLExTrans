b6acf78c7ac0c8887fc5116a64c527be6f0af387
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:50 +0200

    New translations textinrules.ts (German)

--- Dev/Modules/translations/TextInRules_de.ts
+++ Dev/Modules/translations/TextInRules_de.ts
@@ -6,19 +6,18 @@
     <message>
       <location filename="../TextInRules.py" line="67"/>
       <source>Text In Rules</source>
-      <translation type="unfinished">Text In Rules</translation>
+      <translation>Text-In-Regeln</translation>
     </message>
     <message>
       <location filename="../TextInRules.py" line="70"/>
       <source>Define and test a set of Paratext-import search and replace operations.</source>
-      <translation type="unfinished">Define and test a set of Paratext-import search and replace operations.</translation>
+      <translation>Definieren und testen Sie eine Reihe von Such- und Ersetzungsoperationen für den Paratext-Import.</translation>
     </message>
     <message>
       <location filename="../TextInRules.py" line="72"/>
       <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 Paratext. Regular expressions and Wildebeest normalization can be used if desired.</source>
-      <translation type="unfinished">This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
-Paratext. Regular expressions and Wildebeest normalization can be used if desired.</translation>
+      <translation>Dieses Modul wird verwendet, um eine Reihe von Such- und Ersetzungsoperationen zu definieren und zu testen, die verwendet werden, um den Text zu korrigieren, der aus Paratext stammt. Reguläre Ausdrücke und Wildebeest-Normalisierung können bei Bedarf verwendet werden.</translation>
     </message>
   </context>
 </TS>
