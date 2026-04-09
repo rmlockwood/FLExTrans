75726321c0b6d8d95fdfc1377b0b3fbfbc56db9c
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:04:02 +0200

    New translations replacementeditorwindow.ts (French)

--- Dev/Lib/Windows/translations/ReplacementEditorWindow_fr.ts
+++ Dev/Lib/Windows/translations/ReplacementEditorWindow_fr.ts
@@ -6,22 +6,22 @@
     <message>
       <location filename="../ReplacementEditorWindow.py" line="108"/>
       <source>Replacement Dictionary Editor</source>
-      <translation type="unfinished">Replacement Dictionary Editor</translation>
+      <translation>Éditeur de dictionnaire de remplacement</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="110"/>
       <source>Source Lemma</source>
-      <translation type="unfinished">Source Lemma</translation>
+      <translation>Lemme source</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="120"/>
       <source>Gramm. Cat.</source>
-      <translation type="unfinished">Gramm. Cat.</translation>
+      <translation>Cat. gramm.</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="122"/>
       <source>Features/Classes</source>
-      <translation type="unfinished">Features/Classes</translation>
+      <translation>Traits/Classes</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="124"/>
@@ -31,37 +31,37 @@
     <message>
       <location filename="../ReplacementEditorWindow.py" line="118"/>
       <source>Target Lemma</source>
-      <translation type="unfinished">Target Lemma</translation>
+      <translation>Lemme cible</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="126"/>
       <source>Comment</source>
-      <translation type="unfinished">Comment</translation>
+      <translation>Commentaire</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="127"/>
       <source>Separate multiple features/classes and/or affixes with a period. For example f.sg for femine singular.</source>
-      <translation type="unfinished">Separate multiple features/classes and/or affixes with a period. For example f.sg for femine singular.</translation>
+      <translation>Séparez plusieurs traits/classes et/ou affixes par un point. Par exemple f.sg pour féminin singulier.</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="128"/>
       <source>Add Row</source>
-      <translation type="unfinished">Add Row</translation>
+      <translation>Ajouter une ligne</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="129"/>
       <source>Delete Selected Rows</source>
-      <translation type="unfinished">Delete Selected Rows</translation>
+      <translation>Supprimer les lignes sélectionnées</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="130"/>
       <source>Save</source>
-      <translation type="unfinished">Save</translation>
+      <translation>Enregistrer</translation>
     </message>
     <message>
       <location filename="../ReplacementEditorWindow.py" line="131"/>
       <source>Close</source>
-      <translation type="unfinished">Close</translation>
+      <translation>Fermer</translation>
     </message>
   </context>
 </TS>
