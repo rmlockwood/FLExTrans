808bc3786bd9df22167bf20765e4fcdea6df6fb8
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 08:25:16 +0200

    New translations testbedvalidator.ts (German)

--- Dev/Lib/translations/TestbedValidator_de.ts
+++ Dev/Lib/translations/TestbedValidator_de.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
     <name>TestbedValidator</name>
     <message>
       <location filename="../TestbedValidator.py" line="108"/>
       <source>Word Sense: {wordSense} not found.</source>
-      <translation>Wortbedeutung: {wordSense} nicht gefunden.</translation>
+      <translation type="unfinished">Word Sense: {wordSense} not found.</translation>
     </message>
     <message>
       <location filename="../TestbedValidator.py" line="114"/>
       <source>Grammatical Category: {gramCat} not found.</source>
-      <translation>Grammatische Kategorie: {gramCat} nicht gefunden.</translation>
+      <translation type="unfinished">Grammatical Category: {gramCat} not found.</translation>
     </message>
     <message>
       <location filename="../TestbedValidator.py" line="120"/>
       <source>Tag: {tag} not found.</source>
-      <translation>Tag: {tag} nicht gefunden.</translation>
+      <translation type="unfinished">Tag: {tag} not found.</translation>
     </message>
     <message>
       <location filename="../TestbedValidator.py" line="232"/>
       <source>Configuration File Problem. Morphnames not found.</source>
-      <translation>Problem mit der Konfigurationsdatei. Morphnamen nicht gefunden.</translation>
+      <translation type="unfinished">Configuration File Problem. Morphnames not found.</translation>
     </message>
   </context>
 </TS>
