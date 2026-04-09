f52445b0f5217a84c63bdd2539b16e773cabd869
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:18:34 +0200

    New translations createapertiumrules.ts (Spanish)

--- Dev/Lib/translations/CreateApertiumRules_es.ts
+++ Dev/Lib/translations/CreateApertiumRules_es.ts
@@ -11,197 +11,197 @@
     <message>
       <location filename="../CreateApertiumRules.py" line="446"/>
       <source>Could not find any tags for feature {label} of part-of-speech {category}.</source>
-      <translation>No se pudieron encontrar etiquetas para la característica {label} de la categoría gramatical {category}.</translation>
+      <translation type="unfinished">Could not find any tags for feature {label} of part-of-speech {category}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="582"/>
       <source>Other possible values: {values}</source>
-      <translation>Otros valores posibles: {values}</translation>
+      <translation type="unfinished">Other possible values: {values}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="638"/>
       <source>m_{srcSpec.xmlLabel}-to-{trgSpec.xmlLabel}</source>
-      <translation>m_{srcSpec.xmlLabel}-a-{trgSpec.xmlLabel}</translation>
+      <translation type="unfinished">m_{srcSpec.xmlLabel}-to-{trgSpec.xmlLabel}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="829"/>
       <source>Used by macro {macid}</source>
-      <translation>Usado por macro {macid}</translation>
+      <translation type="unfinished">Used by macro {macid}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="848"/>
       <source>Clear the variable to be sure we don't accidentally retain a prior value</source>
-      <translation>Limpie la variable para asegurarse de que no se retenga accidentalmente un valor anterior</translation>
+      <translation type="unfinished">Clear the variable to be sure we don't accidentally retain a prior value</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="650"/>
       <source>No target affixes found for feature '{trgSpec.label}' on part-of-speech {trgSpec.category}.</source>
-      <translation>No se encontraron afijos de destino para la característica '{trgSpec.label}' de la categoría gramatical {trgSpec.category}.</translation>
+      <translation type="unfinished">No target affixes found for feature '{trgSpec.label}' on part-of-speech {trgSpec.category}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="651"/>
       <source>There are no target affixes, so there's nothing further to do here.</source>
-      <translation>No hay afijos de destino, por lo que no hay nada más que hacer aquí.</translation>
+      <translation type="unfinished">There are no target affixes, so there's nothing further to do here.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="655"/>
       <source>Determine the appropriate noun class</source>
-      <translation>Determine la clase de sustantivo adecuada</translation>
+      <translation type="unfinished">Determine the appropriate noun class</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="660"/>
       <source>All relevant target affixes have the default value</source>
-      <translation>Todos los afijos de destino relevantes tienen el valor predeterminado</translation>
+      <translation type="unfinished">All relevant target affixes have the default value</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="761"/>
       <source>We should check for the appropriate singular noun class if one of the following is true:</source>
-      <translation>Deberíamos verificar la clase de sustantivo singular adecuada si se cumple una de las siguientes condiciones:</translation>
+      <translation type="unfinished">We should check for the appropriate singular noun class if one of the following is true:</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="763"/>
       <source>The source noun doesn't have a plural affix attached. (Likely it has a singular affix.)</source>
-      <translation>El sustantivo fuente no tiene un afijo plural adjunto. (Probablemente tenga un afijo singular.)</translation>
+      <translation type="unfinished">The source noun doesn't have a plural affix attached. (Likely it has a singular affix.)</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="767"/>
       <source>The target noun doesn't take plural agreement (marked as such).</source>
-      <translation>El sustantivo objetivo no toma concordancia plural (marcado como tal).</translation>
+      <translation type="unfinished">The target noun doesn't take plural agreement (marked as such).</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="771"/>
       <source>But if the target noun doesn't take singular agreement (marked as such), then we will have a plural noun class.</source>
-      <translation>Pero si el sustantivo objetivo no toma concordancia singular (marcado como tal), entonces tendremos una clase de sustantivo plural.</translation>
+      <translation type="unfinished">But if the target noun doesn't take singular agreement (marked as such), then we will have a plural noun class.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="778"/>
       <source>Check for the appropriate plural noun class.</source>
-      <translation>Verifique la clase de sustantivo plural adecuada.</translation>
+      <translation type="unfinished">Check for the appropriate plural noun class.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="823"/>
       <source>lemma</source>
-      <translation>lema</translation>
+      <translation type="unfinished">lemma</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="823"/>
       <source>affix</source>
-      <translation>afijo</translation>
+      <translation type="unfinished">affix</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="824"/>
       <source>from</source>
-      <translation>de</translation>
+      <translation type="unfinished">from</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="836"/>
       <source>Item {n} is part-of-speech {cat}.</source>
-      <translation>El elemento {n} es la categoría gramatical {cat}.</translation>
+      <translation type="unfinished">Item {n} is part-of-speech {cat}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="966"/>
       <source> given {specs}</source>
-      <translation> dado {specs}</translation>
+      <translation type="unfinished"> given {specs}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="972"/>
       <source>There is only one possible value for {source.label} here: {source.default}.</source>
-      <translation>Solo hay un valor posible para {source.label} aquí: {source.default}.</translation>
+      <translation type="unfinished">There is only one possible value for {source.label} here: {source.default}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="974"/>
       <source>Narrow the set of possible values based on {source.label} ({tags}){given}.</source>
-      <translation>Reduzca el conjunto de valores posibles en función de {source.label} ({tags}){given}.</translation>
+      <translation type="unfinished">Narrow the set of possible values based on {source.label} ({tags}){given}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="982"/>
       <source>Set {varid} based on {source.label} = {feature}.</source>
-      <translation>Establezca {varid} en función de {source.label} = {feature}.</translation>
+      <translation type="unfinished">Set {varid} based on {source.label} = {feature}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1004"/>
       <source>Set {varid} based on {source.label} = {source.default}.</source>
-      <translation>Establezca {varid} en función de {source.label} = {source.default}.</translation>
+      <translation type="unfinished">Set {varid} based on {source.label} = {source.default}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1024"/>
       <source>Possible values with this combination of tags: {lemmas}</source>
-      <translation>Valores posibles con esta combinación de etiquetas: {lemmas}</translation>
+      <translation type="unfinished">Possible values with this combination of tags: {lemmas}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1065"/>
       <source>Multiple source words have ID {wid} in rule &quot;{ruleName}&quot;.</source>
-      <translation>Múltiples palabras fuente tienen el ID {wid} en la regla &quot;{ruleName}&quot;.</translation>
+      <translation type="unfinished">Multiple source words have ID {wid} in rule &quot;{ruleName}&quot;.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1070"/>
       <source>Source word {wid} in rule &quot;{ruleName}&quot; has no category.</source>
-      <translation>La palabra fuente {wid} en la regla &quot;{ruleName}&quot; no tiene categoría.</translation>
+      <translation type="unfinished">Source word {wid} in rule &quot;{ruleName}&quot; has no category.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1083"/>
       <source>No target word has been set as head in rule &quot;{ruleName}&quot;.</source>
-      <translation>No se ha establecido ninguna palabra objetivo como cabeza en la regla &quot;{ruleName}&quot;.</translation>
+      <translation type="unfinished">No target word has been set as head in rule &quot;{ruleName}&quot;.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1106"/>
       <source>Rule name &quot;{ruleName}&quot; already exists in the rule file. Renaming added rule to &quot;{altName}&quot;.</source>
-      <translation>El nombre de la regla &quot;{ruleName}&quot; ya existe en el archivo de reglas. Renombrando la regla añadida a &quot;{altName}&quot;.</translation>
+      <translation type="unfinished">Rule name &quot;{ruleName}&quot; already exists in the rule file. Renaming added rule to &quot;{altName}&quot;.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1115"/>
       <source>Rule Assistant Description: {desc}</source>
-      <translation>Descripción del Asistente de Reglas: {desc}</translation>
+      <translation type="unfinished">Rule Assistant Description: {desc}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1183"/>
       <source>Unable to determine source for {match} {label} in {ruleName}.</source>
-      <translation>No se pudo determinar la fuente para {match} {label} en {ruleName}.</translation>
+      <translation type="unfinished">Unable to determine source for {match} {label} in {ruleName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1210"/>
       <source>Missing category for inserted word {wid} in rule {ruleName}.</source>
-      <translation>Falta la categoría para la palabra insertada {wid} en la regla {ruleName}.</translation>
+      <translation type="unfinished">Missing category for inserted word {wid} in rule {ruleName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1216"/>
       <source>Generate and output {cat}</source>
-      <translation>Generar y emitir {cat}</translation>
+      <translation type="unfinished">Generate and output {cat}</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1240"/>
       <source>Missing source for feature {label} on inserted word {wid} in rule {ruleName}.</source>
-      <translation>Falta la fuente para la característica {label} en la palabra insertada {wid} en la regla {ruleName}.</translation>
+      <translation type="unfinished">Missing source for feature {label} on inserted word {wid} in rule {ruleName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1252"/>
       <source>Unable to generate lemma for inserted word {wid} in rule {ruleName}.</source>
-      <translation>No se pudo generar el lema para la palabra insertada {wid} en la regla {ruleName}.</translation>
+      <translation type="unfinished">Unable to generate lemma for inserted word {wid} in rule {ruleName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1272"/>
       <source>Determine the appropriate lemma for {cat} and store it in a variable named {spec.varid}.</source>
-      <translation>Determine el lema apropiado para {cat} y guárdelo en una variable llamada {spec.varid}.</translation>
+      <translation type="unfinished">Determine the appropriate lemma for {cat} and store it in a variable named {spec.varid}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1376"/>
       <source>Unable to find source for feature {label} on word {wid} in rule {ruleName}.</source>
-      <translation>No se pudo encontrar la fuente para la característica {label} en la palabra {wid} en la regla {ruleName}.</translation>
+      <translation type="unfinished">Unable to find source for feature {label} on word {wid} in rule {ruleName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1343"/>
       <source>Determine the appropriate affix for {cat} and store it in a variable named {spec.varid}.</source>
-      <translation>Determine el afijo apropiado para {cat} y guárdelo en una variable llamada {spec.varid}.</translation>
+      <translation type="unfinished">Determine the appropriate affix for {cat} and store it in a variable named {spec.varid}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1367"/>
       <source>No tag found for value {value} of feature {label} in rule &quot;{ruleName}&quot;.</source>
-      <translation>No se encontró etiqueta para el valor {value} de la característica {label} en la regla &quot;{ruleName}&quot;.</translation>
+      <translation type="unfinished">No tag found for value {value} of feature {label} in rule &quot;{ruleName}&quot;.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1393"/>
       <source>Determine the appropriate {label} tag for {cat} and store it in a variable named {varid}.</source>
-      <translation>Determine la etiqueta {label} apropiada para {cat} y guárdela en una variable llamada {varid}.</translation>
+      <translation type="unfinished">Determine the appropriate {label} tag for {cat} and store it in a variable named {varid}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1487"/>
@@ -221,17 +221,17 @@
     <message>
       <location filename="../CreateApertiumRules.py" line="1557"/>
       <source>Added {ruleCount} rule(s) from {fileName}.</source>
-      <translation>Se añadieron {ruleCount} regla(s) desde {fileName}.</translation>
+      <translation type="unfinished">Added {ruleCount} rule(s) from {fileName}.</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1585"/>
       <source>No Rule Assistant file found, please run the Set Up Transfer Rule Categories and Attributes tool</source>
-      <translation>No se encontró el archivo del Asistente de Reglas, por favor ejecute la herramienta &quot;Configurar categorías y atributos de reglas de transferencia&quot;</translation>
+      <translation type="unfinished">No Rule Assistant file found, please run the Set Up Transfer Rule Categories and Attributes tool</translation>
     </message>
     <message>
       <location filename="../CreateApertiumRules.py" line="1594"/>
       <source>Copying prior version of transfer rules to {backupPath}.</source>
-      <translation>Copiando la versión anterior de las reglas de transferencia a {backupPath}.</translation>
+      <translation type="unfinished">Copying prior version of transfer rules to {backupPath}.</translation>
     </message>
   </context>
 </TS>
