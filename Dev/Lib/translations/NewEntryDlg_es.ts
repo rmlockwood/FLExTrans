3ce9dd47eca8302432d8c3e0a491053ad3d05bef
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 08:25:05 +0200

    New translations newentrydlg.ts (Spanish)

--- Dev/Lib/translations/NewEntryDlg_es.ts
+++ Dev/Lib/translations/NewEntryDlg_es.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="es-ES" sourcelanguage="en">
+  <context>
     <name>NewEntryDlg</name>
     <message>
-        <location filename="../NewEntryDlg.py" line="89"/>
-        <source>Target FLEx project name</source>
-        <translation>Nombre del proyecto FLEx de destino</translation>
+      <location filename="../NewEntryDlg.py" line="92"/>
+      <source>Target FLEx project name</source>
+      <translation type="unfinished">Target FLEx project name</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="90"/>
-        <source>Lexeme Form</source>
-        <translation>Forma del Lexema</translation>
+      <location filename="../NewEntryDlg.py" line="93"/>
+      <source>Lexeme Form</source>
+      <translation type="unfinished">Lexeme Form</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="271"/>
-        <source>Error Check</source>
-        <translation>Verificación de Errores</translation>
+      <location filename="../NewEntryDlg.py" line="292"/>
+      <source>Error Check</source>
+      <translation type="unfinished">Error Check</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="196"/>
-        <source>You must enter a Gloss.</source>
-        <translation>Debe ingresar una Glosa.</translation>
+      <location filename="../NewEntryDlg.py" line="217"/>
+      <source>You must enter a Gloss.</source>
+      <translation type="unfinished">You must enter a Gloss.</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="217"/>
-        <source>Cluster Project Selection Error</source>
-        <translation>Error de Selección de Proyecto en Clúster</translation>
+      <location filename="../NewEntryDlg.py" line="238"/>
+      <source>Cluster Project Selection Error</source>
+      <translation type="unfinished">Cluster Project Selection Error</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="217"/>
-        <source>You must at least select the default target project, {proj}, among your cluster projects.</source>
-        <translation>Debe seleccionar al menos el proyecto de destino predeterminado, {proj}, entre sus proyectos en clúster.</translation>
+      <location filename="../NewEntryDlg.py" line="238"/>
+      <source>You must at least select the default target project, {proj}, among your cluster projects.</source>
+      <translation type="unfinished">You must at least select the default target project, {proj}, among your cluster projects.</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="230"/>
-        <source>You must enter all the Lexeme Forms.</source>
-        <translation>Debe ingresar todas las Formas de Lexema.</translation>
+      <location filename="../NewEntryDlg.py" line="251"/>
+      <source>You must enter all the Lexeme Forms.</source>
+      <translation type="unfinished">You must enter all the Lexeme Forms.</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="250"/>
-        <source>Not Found Error</source>
-        <translation>Error No Encontrado</translation>
+      <location filename="../NewEntryDlg.py" line="271"/>
+      <source>Not Found Error</source>
+      <translation type="unfinished">Not Found Error</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="250"/>
-        <source>Failed to open the project: {proj}</source>
-        <translation>Error al abrir el proyecto: {proj}</translation>
+      <location filename="../NewEntryDlg.py" line="271"/>
+      <source>Failed to open the project: {proj}</source>
+      <translation type="unfinished">Failed to open the project: {proj}</translation>
     </message>
     <message>
-        <location filename="../NewEntryDlg.py" line="271"/>
-        <source>You must enter a Lexeme Form.</source>
-        <translation>Debe ingresar una Forma de Lexema.</translation>
+      <location filename="../NewEntryDlg.py" line="292"/>
+      <source>You must enter a Lexeme Form.</source>
+      <translation type="unfinished">You must enter a Lexeme Form.</translation>
     </message>
-</context>
+  </context>
 </TS>
