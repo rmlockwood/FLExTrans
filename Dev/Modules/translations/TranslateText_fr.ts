a0b3fe172f1ea30bc8840c2453f6c4dc676a0906
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:18:27 +0200

    New translations translatetext.ts (French)

--- Dev/Modules/translations/TranslateText_fr.ts
+++ Dev/Modules/translations/TranslateText_fr.ts
@@ -1,72 +1,72 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
     <name>TranslateText</name>
     <message>
-        <location filename="../TranslateText.py" line="67"/>
-        <source>Translate Text</source>
-        <translation>Traduire le texte</translation>
+      <location filename="../TranslateText.py" line="67"/>
+      <source>Translate Text</source>
+      <translation type="unfinished">Translate Text</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="72"/>
-        <source>Translate the current source text.</source>
-        <translation>Traduire le texte source actuel.</translation>
+      <location filename="../TranslateText.py" line="72"/>
+      <source>Translate the current source text.</source>
+      <translation type="unfinished">Translate the current source text.</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="98"/>
-        <source>Exporting source text...</source>
-        <translation>Exportation du texte source...</translation>
+      <location filename="../TranslateText.py" line="98"/>
+      <source>Exporting source text...</source>
+      <translation type="unfinished">Exporting source text...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="105"/>
-        <source>Building the bilingual lexicon...</source>
-        <translation>Construction du lexique bilingue...</translation>
+      <location filename="../TranslateText.py" line="105"/>
+      <source>Building the bilingual lexicon...</source>
+      <translation type="unfinished">Building the bilingual lexicon...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="114"/>
-        <source>Running the Apertium transfer engine...</source>
-        <translation>Exécution du moteur de transfert Apertium...</translation>
+      <location filename="../TranslateText.py" line="114"/>
+      <source>Running the Apertium transfer engine...</source>
+      <translation type="unfinished">Running the Apertium transfer engine...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="128"/>
-        <source>Cataloging target affixes...</source>
-        <translation>Catalogage des affixes cibles...</translation>
+      <location filename="../TranslateText.py" line="128"/>
+      <source>Cataloging target affixes...</source>
+      <translation type="unfinished">Cataloging target affixes...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="137"/>
-        <source>Converting target words to synthesizer format...</source>
-        <translation>Conversion des mots cibles au format du synthétiseur...</translation>
+      <location filename="../TranslateText.py" line="137"/>
+      <source>Converting target words to synthesizer format...</source>
+      <translation type="unfinished">Converting target words to synthesizer format...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="146"/>
-        <source>Synthesizing target text...</source>
-        <translation>Synthèse du texte cible...</translation>
+      <location filename="../TranslateText.py" line="146"/>
+      <source>Synthesizing target text...</source>
+      <translation type="unfinished">Synthesizing target text...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="150"/>
-        <source>Using HermitCrab for synthesis.</source>
-        <translation>Utilisation de HermitCrab pour la synthèse.</translation>
+      <location filename="../TranslateText.py" line="150"/>
+      <source>Using HermitCrab for synthesis.</source>
+      <translation type="unfinished">Using HermitCrab for synthesis.</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="154"/>
-        <source>Using STAMP for synthesis.</source>
-        <translation>Utilisation de STAMP pour la synthèse.</translation>
+      <location filename="../TranslateText.py" line="154"/>
+      <source>Using STAMP for synthesis.</source>
+      <translation type="unfinished">Using STAMP for synthesis.</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="164"/>
-        <source>Exporting to Paratext...</source>
-        <translation>Exportation vers Paratext...</translation>
+      <location filename="../TranslateText.py" line="164"/>
+      <source>Exporting to Paratext...</source>
+      <translation type="unfinished">Exporting to Paratext...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="171"/>
-        <source>Inserting text into the target project...</source>
-        <translation>Insertion du texte dans le projet cible...</translation>
+      <location filename="../TranslateText.py" line="171"/>
+      <source>Inserting text into the target project...</source>
+      <translation type="unfinished">Inserting text into the target project...</translation>
     </message>
     <message>
-        <location filename="../TranslateText.py" line="177"/>
-        <source>Translation complete.</source>
-        <translation>Traduction terminée.</translation>
+      <location filename="../TranslateText.py" line="177"/>
+      <source>Translation complete.</source>
+      <translation type="unfinished">Translation complete.</translation>
     </message>
-</context>
+  </context>
 </TS>
