a224fa9b6513188d64230fa2c892da118f5e0929
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:19:18 +0200

    New translations ruleassistant.ts (Spanish)

--- Dev/Modules/translations/RuleAssistant_es.ts
+++ Dev/Modules/translations/RuleAssistant_es.ts
@@ -1,69 +1,59 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="es-ES" sourcelanguage="en">
+  <context>
     <name>RuleAssistant</name>
     <message>
-        <location filename="../RuleAssistant.py" line="111"/>
-        <source>Rule Assistant</source>
-        <translation>Asistente de reglas</translation>
+      <location filename="../RuleAssistant.py" line="102"/>
+      <source>This module runs a tool which let's you create transfer rules.</source>
+      <translation type="unfinished">This module runs a tool which let's you create transfer rules.</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="110"/>
-        <source>This module runs a tool which let's you create transfer rules.</source>
-        <translation>Este módulo ejecuta una herramienta que le permite crear reglas de transferencia.</translation>
+      <location filename="../RuleAssistant.py" line="103"/>
+      <source>Rule Assistant</source>
+      <translation type="unfinished">Rule Assistant</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="114"/>
-        <source>Runs a tool for creating transfer rules.</source>
-        <translation>Ejecuta una herramienta para crear reglas de transferencia.</translation>
+      <location filename="../RuleAssistant.py" line="106"/>
+      <source>Runs a tool for creating transfer rules.</source>
+      <translation type="unfinished">Runs a tool for creating transfer rules.</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="333"/>
-        <source>Compiled bilingual dictionary not found. Run the &quot;Run Apertium&quot; module to display test data in the Rule Assistant.</source>
-        <translation type="obsolete">No se encontró el diccionario bilingüe compilado. Ejecute el módulo &quot;Run Apertium&quot; para mostrar datos de prueba en el Rule Assistant.</translation>
+      <location filename="../RuleAssistant.py" line="347"/>
+      <source>Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</source>
+      <translation type="unfinished">Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="367"/>
-        <source>The text named '%s' was not found.</source>
-        <translation>No se encontró el texto llamado '%s'.</translation>
+      <location filename="../RuleAssistant.py" line="359"/>
+      <source>The text named '%s' was not found.</source>
+      <translation type="unfinished">The text named '%s' was not found.</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="442"/>
-        <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
+      <location filename="../RuleAssistant.py" line="379"/>
+      <source>Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</source>
+      <translation type="unfinished">Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</translation>
+    </message>
+    <message>
+      <location filename="../RuleAssistant.py" line="395"/>
+      <source>&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </source>
+      <translation type="unfinished">&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </translation>
+    </message>
+    <message>
+      <location filename="../RuleAssistant.py" line="434"/>
+      <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
 </source>
-        <translation>&lt;html&gt;&lt;body&gt;&lt;p&gt;No hay datos de prueba disponibles.&lt;/body&gt;&lt;/html&gt;
+      <translation type="unfinished">&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
 </translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="443"/>
-        <source>An error happened when running the Rule Assistant tool: {e.output.decode(&quot;utf-8&quot;)}</source>
-        <translation type="obsolete">Ocurrió un error al ejecutar la herramienta Rule Assistant: {e.output.decode(&quot;utf-8&quot;)}</translation>
+      <location filename="../RuleAssistant.py" line="469"/>
+      <source>An error happened when running the {ruleAssistant} tool: {error}</source>
+      <translation type="unfinished">An error happened when running the {ruleAssistant} tool: {error}</translation>
     </message>
     <message>
-        <location filename="../RuleAssistant.py" line="535"/>
-        <source>No rules created.</source>
-        <translation>No se crearon reglas.</translation>
+      <location filename="../RuleAssistant.py" line="527"/>
+      <source>No rules created.</source>
+      <translation type="unfinished">No rules created.</translation>
     </message>
-    <message>
-        <location filename="../RuleAssistant.py" line="403"/>
-        <source>&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </source>
-        <translation>&lt;p&gt;&lt;b&gt;Texto fuente:&lt;/b&gt; </translation>
-    </message>
-    <message>
-        <location filename="../RuleAssistant.py" line="355"/>
-        <source>Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</source>
-        <translation>No se encontró el diccionario bilingüe. Compile el diccionario bilingüe para ver datos de prueba en el {ruleAssistant}.</translation>
-    </message>
-    <message>
-        <location filename="../RuleAssistant.py" line="387"/>
-        <source>Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</source>
-        <translation>No se encontró el diccionario bilingüe compilado. Hubo un error al compilar el diccionario bilingüe.</translation>
-    </message>
-    <message>
-        <location filename="../RuleAssistant.py" line="477"/>
-        <source>An error happened when running the {ruleAssistant} tool: {error}</source>
-        <translation>Ocurrió un error al ejecutar la herramienta {ruleAssistant}: {error}</translation>
-    </message>
-</context>
+  </context>
 </TS>
