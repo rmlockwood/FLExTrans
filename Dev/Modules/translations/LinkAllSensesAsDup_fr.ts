7b06aa36533566c08f3544a58dec88d2a0387876
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:04:28 +0200

    New translations linkallsensesasdup.ts (French)

--- Dev/Modules/translations/LinkAllSensesAsDup_fr.ts
+++ Dev/Modules/translations/LinkAllSensesAsDup_fr.ts
@@ -5,12 +5,12 @@
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="74"/>
       <source>Link All Senses As Duplicate</source>
-      <translation type="unfinished">Link All Senses As Duplicate</translation>
+      <translation>Lier tous les sens comme dupliqué</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="77"/>
       <source>Link all senses to the same ID in the target.</source>
-      <translation type="unfinished">Link all senses to the same ID in the target.</translation>
+      <translation>Lier tous les sens au même ID dans la cible.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="79"/>
@@ -18,40 +18,40 @@
 overwrite all senses in the source project!
 This assumes the target project was copied from the source and all the senses have the same
 unique identifier (guid).</source>
-      <translation type="unfinished">This module will link all senses to the same ID in the target. CAUTION: This will 
-overwrite all senses in the source project!
-This assumes the target project was copied from the source and all the senses have the same
-unique identifier (guid).</translation>
+      <translation>Ce module liera tous les sens au même ID dans la cible. ATTENTION : Cela va
+écraser tous les sens dans le projet source !
+Cela suppose que le projet cible a été copié depuis la source et que tous les sens ont le même
+identifiant unique (guid).</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="100"/>
       <source>You need to run this module in "modify mode."</source>
-      <translation type="unfinished">You need to run this module in "modify mode."</translation>
+      <translation>Vous devez exécuter ce module en "mode modification".</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="120"/>
       <source>No Source Text Name has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Text Name has been set. Please go to Settings and fix this.</translation>
+      <translation>Aucun nom de texte source n'a été défini. Veuillez aller dans Paramètres et corriger cela.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="125"/>
       <source>No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</translation>
+      <translation>Aucun champ personnalisé source pour le lien d'entrée n'a été défini. Veuillez aller dans Paramètres et corriger cela.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="133"/>
       <source>No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
+      <translation>Aucun type de morphème source compté comme racine n'a été sélectionné. Veuillez aller dans Paramètres et corriger cela.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="171"/>
       <source>Skipped this guid that was not found: {guidSubStr}.</source>
-      <translation type="unfinished">Skipped this guid that was not found: {guidSubStr}.</translation>
+      <translation>Ignoré ce guid qui n'a pas été trouvé : {guidSubStr}.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="177"/>
       <source>{entryIndex} entries processed.</source>
-      <translation type="unfinished">{entryIndex} entries processed.</translation>
+      <translation>{entryIndex} entrées traitées.</translation>
     </message>
   </context>
 </TS>
