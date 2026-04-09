31a31d5e7056149dd7abd18046edf141d3e70c5d
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:31 +0200

    New translations linkallsensesasdup.ts (Spanish)

--- Dev/Modules/translations/LinkAllSensesAsDup_es.ts
+++ Dev/Modules/translations/LinkAllSensesAsDup_es.ts
@@ -5,12 +5,12 @@
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="74"/>
       <source>Link All Senses As Duplicate</source>
-      <translation type="unfinished">Link All Senses As Duplicate</translation>
+      <translation>Enlazar todos los sentidos como duplicados</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="77"/>
       <source>Link all senses to the same ID in the target.</source>
-      <translation type="unfinished">Link all senses to the same ID in the target.</translation>
+      <translation>Vincular todos los sentidos al mismo ID en el destino.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="79"/>
@@ -18,10 +18,10 @@
 overwrite all senses in the source project!
 This assumes the target project was copied from the source and all the senses have the same
 unique identifier (guid).</source>
-      <translation type="unfinished">This module will link all senses to the same ID in the target. CAUTION: This will 
-overwrite all senses in the source project!
-This assumes the target project was copied from the source and all the senses have the same
-unique identifier (guid).</translation>
+      <translation>Este módulo vinculará todos los sentidos al mismo ID en el destino. PRECAUCIÓN: ¡Esto 
+sobrescribirá todos los sentidos en el proyecto origen!
+Esto asume que el proyecto destino fue copiado del origen y todos los sentidos tienen el mismo
+identificador único (guid).</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="100"/>
@@ -31,27 +31,27 @@ unique identifier (guid).</translation>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="120"/>
       <source>No Source Text Name has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Text Name has been set. Please go to Settings and fix this.</translation>
+      <translation>No se ha establecido ningún Nombre de Texto Origen. Por favor vaya a Configuración y corrija esto.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="125"/>
       <source>No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</translation>
+      <translation>No se ha establecido ningún Campo Personalizado Origen para Enlace de Entrada. Por favor vaya a Configuración y corrija esto.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="133"/>
       <source>No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
+      <translation>No se han seleccionado Tipos de Morfemas Origen Contados Como Raíces. Por favor vaya a Configuración y corrija esto.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="171"/>
       <source>Skipped this guid that was not found: {guidSubStr}.</source>
-      <translation type="unfinished">Skipped this guid that was not found: {guidSubStr}.</translation>
+      <translation>Se omitió esta guía que no se encontró: {guidSubStr}.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="177"/>
       <source>{entryIndex} entries processed.</source>
-      <translation type="unfinished">{entryIndex} entries processed.</translation>
+      <translation>{entryIndex} entradas procesadas.</translation>
     </message>
   </context>
 </TS>
