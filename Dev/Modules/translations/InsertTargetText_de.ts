d57d7c01de2e1d6ae72329d289bace328ebe631d
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:36 +0200

    New translations inserttargettext.ts (German)

--- Dev/Modules/translations/InsertTargetText_de.ts
+++ Dev/Modules/translations/InsertTargetText_de.ts
@@ -5,12 +5,12 @@
     <message>
       <location filename="../InsertTargetText.py" line="107"/>
       <source>Insert Target Text</source>
-      <translation type="unfinished">Insert Target Text</translation>
+      <translation>Zieltext einfügen</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="110"/>
       <source>Insert a translated text into the target FLEx project.</source>
-      <translation type="unfinished">Insert a translated text into the target FLEx project.</translation>
+      <translation>Fügen Sie einen übersetzten Text in das Ziel-FLEx-Projekt ein.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="112"/>
@@ -19,21 +19,18 @@ the results of the {doSynthModule} module
 and insert the text into the target FLEx project. The Source Text Name setting
 will be used for the text name in the target project. An existing text of the 
 same name will not be overwritten. A copy will be created.</source>
-      <translation type="unfinished">The target project specified in the settings will be used. This module will take
-the results of the {doSynthModule} module
-and insert the text into the target FLEx project. The Source Text Name setting
-will be used for the text name in the target project. An existing text of the 
-same name will not be overwritten. A copy will be created.</translation>
+      <translation>Das im Konfigurationsfile festgelegte Zielprojekt wird verwendet. Dieses Modul übernimmt die Ergebnisse des {doSynthModule}-Moduls
+und fügt den Text in das Ziel-FLEx-Projekt ein. Die Eigenschaft „SourceTextName“ in der FlexTrans.config-Datei wird als Textname im Zielprojekt verwendet. Ein bestehender Text mit demselben Namen wird nicht überschrieben. Es wird eine Kopie erstellt.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="137"/>
       <source>Failed to open the target project.</source>
-      <translation type="unfinished">Failed to open the target project.</translation>
+      <translation>Das Zielprojekt konnte nicht geöffnet werden.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="140"/>
       <source>Using: {targetProj} as the target project.</source>
-      <translation type="unfinished">Using: {targetProj} as the target project.</translation>
+      <translation>Verwende {targetProj} als Zielprojekt.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="158"/>
