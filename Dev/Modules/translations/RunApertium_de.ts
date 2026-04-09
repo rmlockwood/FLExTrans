539ccb5f535fb6e91401f2b5da91e9a395cdf6c7
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:20:18 +0200

    New translations runapertium.ts (German)

--- Dev/Modules/translations/RunApertium_de.ts
+++ Dev/Modules/translations/RunApertium_de.ts
@@ -1,76 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
     <name>RunApertium</name>
     <message>
-      <location filename="../RunApertium.py" line="118"/>
-      <source>Run Apertium</source>
-      <translation>Apertium ausführen</translation>
-    </message>
-    <message>
-        <location filename="../RunApertium.py" line="87"/>
-        <source>This module executes lexical transfer based on links from source to target sense 
+      <location filename="../RunApertium.py" line="95"/>
+      <source>This module executes lexical transfer based on links from source to target sense 
 you have established and then executes structural transfer which
 runs the transfer rules you have made to transform source morphemes into target morphemes.
 The results of this module are found in the file you specified in the Target Transfer Results File.
 This is typically called target_text-aper.txt and is usually in the Build folder.</source>
-        <translation>Dieses Modul führt den lexikalischen Transfer basierend auf den von Ihnen erstellten Verknüpfungen vom Quell- zum Zielbedeutung durch und anschließend den strukturellen Transfer, der die von Ihnen erstellten Transferregeln ausführt, um Quellmorpheme in Zielmorpheme umzuwandeln. Die Ergebnisse dieses Moduls finden Sie in der Datei, die Sie als Ziel-Transfer-Ergebnisdatei angegeben haben. Diese heißt typischerweise target_text-aper.txt und befindet sich normalerweise im Build-Ordner.</translation>
+      <translation type="unfinished">This module executes lexical transfer based on links from source to target sense 
+you have established and then executes structural transfer which
+runs the transfer rules you have made to transform source morphemes into target morphemes.
+The results of this module are found in the file you specified in the Target Transfer Results File.
+This is typically called target_text-aper.txt and is usually in the Build folder.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="96"/>
-        <source>Run the Apertium transfer engine.</source>
-        <translation>Führen Sie die Apertium-Transfer-Engine aus.</translation>
+      <location filename="../RunApertium.py" line="101"/>
+      <source>Run Apertium</source>
+      <translation type="unfinished">Run Apertium</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="218"/>
-        <source>Error in opening the file: &quot;{file}&quot;, check that it exists and that it is valid.</source>
-        <translation>Fehler beim Öffnen der Datei: &quot;{file}&quot;. Bitte überprüfen Sie, ob sie existiert und gültig ist.</translation>
+      <location filename="../RunApertium.py" line="104"/>
+      <source>Run the Apertium transfer engine.</source>
+      <translation type="unfinished">Run the Apertium transfer engine.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="257"/>
-        <source>Invalid File</source>
-        <translation>Ungültige Datei</translation>
+      <location filename="../RunApertium.py" line="226"/>
+      <source>Error in opening the file: &quot;{file}&quot;, check that it exists and that it is valid.</source>
+      <translation type="unfinished">Error in opening the file: &quot;{file}&quot;, check that it exists and that it is valid.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="257"/>
-        <source>The transfer file: {file} is invalid.</source>
-        <translation>Die Transferdatei: {file} ist ungültig.</translation>
+      <location filename="../RunApertium.py" line="265"/>
+      <source>Invalid File</source>
+      <translation type="unfinished">Invalid File</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="307"/>
-        <source>The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; is the same as a gramm. cat. Your rules may not work as expected.</source>
-        <translation>Das Attribut: &quot;{attribStr}&quot; in &quot;{attrName}&quot; ist identisch mit einer grammatischen Kategorie. Ihre Regeln funktionieren möglicherweise nicht wie erwartet.</translation>
+      <location filename="../RunApertium.py" line="265"/>
+      <source>The transfer file: {file} is invalid.</source>
+      <translation type="unfinished">The transfer file: {file} is invalid.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="312"/>
-        <source>The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; has a period in it. It needs to be an underscore. Your rules may not work as expected.</source>
-        <translation>Das Attribut: &quot;{attribStr}&quot; in &quot;{attrName}&quot; enthält einen Punkt. Es muss ein Unterstrich sein. Ihre Regeln funktionieren möglicherweise nicht wie erwartet.</translation>
+      <location filename="../RunApertium.py" line="315"/>
+      <source>The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; is the same as a gramm. cat. Your rules may not work as expected.</source>
+      <translation type="unfinished">The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; is the same as a gramm. cat. Your rules may not work as expected.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="411"/>
-        <source>The bilingual dictionary file does not exist. You may need to run the Build Bilingual Lexicon module. The file should be: {file}</source>
-        <translation>Die zweisprachige Wörterbuchdatei existiert nicht. Sie müssen möglicherweise das Modul &quot;Bilinguales Lexikon erstellen&quot; ausführen. Die Datei sollte sein: {file}</translation>
+      <location filename="../RunApertium.py" line="320"/>
+      <source>The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; has a period in it. It needs to be an underscore. Your rules may not work as expected.</source>
+      <translation type="unfinished">The attribute: &quot;{attribStr}&quot; in &quot;{attrName}&quot; has a period in it. It needs to be an underscore. Your rules may not work as expected.</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="421"/>
-        <source>The analyzed text file does not exist. You may need to run the Extract Source Text module. The file should be: {file}</source>
-        <translation>Die analysierte Textdatei existiert nicht. Sie müssen möglicherweise das Modul &quot;Quelltext extrahieren&quot; ausführen. Die Datei sollte sein: {file}</translation>
+      <location filename="../RunApertium.py" line="423"/>
+      <source>The bilingual dictionary file does not exist. You may need to run the {buildLex} module. The file should be: {file}</source>
+      <translation type="unfinished">The bilingual dictionary file does not exist. You may need to run the {buildLex} module. The file should be: {file}</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="478"/>
-        <source>An error happened when running the Apertium tools. The contents of apertium_error.txt is:</source>
-        <translation>Beim Ausführen der Apertium-Tools ist ein Fehler aufgetreten. Der Inhalt von apertium_error.txt ist:</translation>
+      <location filename="../RunApertium.py" line="433"/>
+      <source>The analyzed text file does not exist. You may need to run the {extrSource} module. The file should be: {file}</source>
+      <translation type="unfinished">The analyzed text file does not exist. You may need to run the {extrSource} module. The file should be: {file}</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="489"/>
-        <source>Transferred text put in the file: {file}.</source>
-        <translation>Übertragener Text wurde in die Datei {file} geschrieben.</translation>
+      <location filename="../RunApertium.py" line="448"/>
+      <source>The transfer rules file does not exist. The file should be at: {file}</source>
+      <translation type="unfinished">The transfer rules file does not exist. The file should be at: {file}</translation>
     </message>
     <message>
-        <location filename="../RunApertium.py" line="490"/>
-        <source>Apertium transfer complete.</source>
-        <translation>Apertium-Transfer abgeschlossen.</translation>
+      <location filename="../RunApertium.py" line="495"/>
+      <source>An error happened when running the Apertium tools. The contents of apertium_error.txt is:</source>
+      <translation type="unfinished">An error happened when running the Apertium tools. The contents of apertium_error.txt is:</translation>
     </message>
-</context>
+    <message>
+      <location filename="../RunApertium.py" line="506"/>
+      <source>Transferred text put in the file: {file}.</source>
+      <translation type="unfinished">Transferred text put in the file: {file}.</translation>
+    </message>
+    <message>
+      <location filename="../RunApertium.py" line="507"/>
+      <source>Apertium transfer complete.</source>
+      <translation type="unfinished">Apertium transfer complete.</translation>
+    </message>
+  </context>
 </TS>
