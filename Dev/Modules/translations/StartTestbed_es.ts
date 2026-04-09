b97682cc8ede19abcac16a001a02207c7ce2b830
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:44 +0200

    New translations starttestbed.ts (Spanish)

--- Dev/Modules/translations/StartTestbed_es.ts
+++ Dev/Modules/translations/StartTestbed_es.ts
@@ -6,36 +6,36 @@
     <message>
       <location filename="../StartTestbed.py" line="54"/>
       <source>Start Testbed</source>
-      <translation type="unfinished">Start Testbed</translation>
+      <translation>Iniciar banco de pruebas</translation>
     </message>
     <message>
       <location filename="../StartTestbed.py" line="57"/>
       <source>Initialize the testbed log and create source text from the testbed.</source>
-      <translation type="unfinished">Initialize the testbed log and create source text from the testbed.</translation>
+      <translation>Inicialice el registro del testbed y cree texto fuente desde el testbed.</translation>
     </message>
     <message>
       <location filename="../StartTestbed.py" line="59"/>
       <source>
         Initialize the testbed log and create source text from the testbed.
         </source>
-      <translation type="unfinished">
-        Initialize the testbed log and create source text from the testbed.
+      <translation>
+        Inicialice el registro del testbed y cree texto fuente desde el testbed.
         </translation>
     </message>
     <message>
       <location filename="../StartTestbed.py" line="78"/>
       <source>Testbed does not exist. Please add tests to the testbed.</source>
-      <translation type="unfinished">Testbed does not exist. Please add tests to the testbed.</translation>
+      <translation>El testbed no existe. Por favor, agregue pruebas al testbed.</translation>
     </message>
     <message>
       <location filename="../StartTestbed.py" line="128"/>
       <source>There is a problem with the Analyzed Text Output File path: {outFileVal}. Please check the configuration file setting.</source>
-      <translation type="unfinished">There is a problem with the Analyzed Text Output File path: {outFileVal}. Please check the configuration file setting.</translation>
+      <translation>Hay un problema con la ruta del archivo de salida del texto analizado: {outFileVal}. Por favor, revise la configuración del archivo de configuración.</translation>
     </message>
     <message>
       <location filename="../StartTestbed.py" line="146"/>
       <source>{count} tests prepared for testing.</source>
-      <translation type="unfinished">{count} tests prepared for testing.</translation>
+      <translation>{count} pruebas preparadas para el testeo.</translation>
     </message>
   </context>
 </TS>
