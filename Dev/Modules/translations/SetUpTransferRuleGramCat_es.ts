78a88a747c60bd7c75b1a827513e13f8fb11431f
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:43 +0200

    New translations setuptransferrulegramcat.ts (Spanish)

--- Dev/Modules/translations/SetUpTransferRuleGramCat_es.ts
+++ Dev/Modules/translations/SetUpTransferRuleGramCat_es.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="99"/>
       <source>Set Up Transfer Rule Categories and Attributes</source>
-      <translation type="unfinished">Set Up Transfer Rule Categories and Attributes</translation>
+      <translation>Configurar categorías y atributos de reglas de transferencia</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="102"/>
       <source>Set up the transfer rule file with categories and attributes from source and target FLEx projects.</source>
-      <translation type="unfinished">Set up the transfer rule file with categories and attributes from source and target FLEx projects.</translation>
+      <translation>Configurar el archivo de reglas de transferencia con categorías y atributos de los proyectos FLEx origen y destino.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="104"/>
@@ -25,51 +25,43 @@ grammatical categories from the source FLEx project. This module will also creat
 attributes in the transfer rule file from FLEx inflection features, inflection classes
 and template slots. You can decide which of these are used and whether existing attributes
 should be overwritten.</source>
-      <translation type="unfinished">This module first goes through both the source and target FLEx projects and extracts
-the grammatical category lists. It will replace what is currently listed for the
-tags of the a_gram_cat attribute with the lists extracted. Duplicate categories
-will be discarded. Also naming conventions will be followed like in the bilingual
-lexicon. I.e. spaces are converted to underscores, periods and slashes are removed.
-This module will also populate the categories section of the transfer rule file with
-grammatical categories from the source FLEx project. This module will also create
-attributes in the transfer rule file from FLEx inflection features, inflection classes
-and template slots. You can decide which of these are used and whether existing attributes
-should be overwritten.</translation>
+      <translation>Este módulo primero recorre tanto el proyecto FLEx de origen como el de destino y extrae las listas de categorías gramaticales. Sustituirá lo que actualmente está listado en las etiquetas del atributo a_gram_cat con las listas extraídas. Las categorías duplicadas serán descartadas. Además, se seguirán las convenciones de nomenclatura como en el léxico bilingüe. Es decir, los espacios se convertirán en guiones bajos, y se eliminarán los puntos y las barras.
+Este módulo también rellenará la sección de categorías del archivo de reglas de transferencia con las categorías gramaticales del proyecto FLEx de origen. Asimismo, creará atributos en dicho archivo a partir de características de inflexión de FLEx, clases de inflexión y espacios de plantillas. Puedes decidir cuáles de estos elementos se utilizarán y si se deben sobrescribir los atributos existentes.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="532"/>
       <source>There was a problem finding the transfer rules file. Check your configuration.</source>
-      <translation type="unfinished">There was a problem finding the transfer rules file. Check your configuration.</translation>
+      <translation>Hubo un problema encontrando el archivo de reglas de transferencia. Verifique su configuración.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="544"/>
       <source>The transfer rules file is malformed or not valid XML.</source>
-      <translation type="unfinished">The transfer rules file is malformed or not valid XML.</translation>
+      <translation>El archivo de reglas de transferencia está mal formado o no es un XML válido.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="554"/>
       <source>The transfer rules file is missing required sections.</source>
-      <translation type="unfinished">The transfer rules file is missing required sections.</translation>
+      <translation>Al archivo de reglas de transferencia le faltan secciones requeridas.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="589"/>
       <source>There was a problem writing the transfer rules file: {error}</source>
-      <translation type="unfinished">There was a problem writing the transfer rules file: {error}</translation>
+      <translation>Hubo un problema al escribir el archivo de reglas de transferencia: {error}</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="592"/>
       <source>{attrCount} attributes added to the attributes section.</source>
-      <translation type="unfinished">{attrCount} attributes added to the attributes section.</translation>
+      <translation>{attrCount} atributos agregados a la sección de atributos.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="593"/>
       <source>{num} categories created for the a_gram_cat attribute.</source>
-      <translation type="unfinished">{num} categories created for the a_gram_cat attribute.</translation>
+      <translation>{num} categorías creadas para el atributo a_gram_cat.</translation>
     </message>
     <message>
       <location filename="../SetUpTransferRuleGramCat.py" line="594"/>
       <source>{catCount} categories added to the categories section.</source>
-      <translation type="unfinished">{catCount} categories added to the categories section.</translation>
+      <translation>{catCount} categorías agregadas a la sección de categorías.</translation>
     </message>
   </context>
 </TS>
