927d981ad05019c681b7f8a49eecfae5fcd14036
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 10:48:49 +0200

    New translations interlindata.ts (French)

--- Dev/Lib/translations/InterlinData_fr.ts
+++ Dev/Lib/translations/InterlinData_fr.ts
@@ -6,62 +6,62 @@
     <message>
       <location filename="../InterlinData.py" line="163"/>
       <source>No analyses found.</source>
-      <translation type="unfinished">No analyses found.</translation>
+      <translation>Aucune analyse trouvée.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="174"/>
       <source>The Tree Tran Words to Insert File has invalid XML content.</source>
-      <translation type="unfinished">The Tree Tran Words to Insert File has invalid XML content.</translation>
+      <translation>The Tree Tran Words to Insert File has invalid XML content.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="220"/>
       <source>The Tree Tran Result File has invalid XML content.</source>
-      <translation type="unfinished">The Tree Tran Result File has invalid XML content.</translation>
+      <translation>The Tree Tran Result File has invalid XML content.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="265"/>
       <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</source>
-      <translation type="unfinished">Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</translation>
+      <translation>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="265"/>
       <source>. Exiting.</source>
-      <translation type="unfinished">. Exiting.</translation>
+      <translation>. Exiting.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="378"/>
       <source>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</source>
-      <translation type="unfinished">Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</translation>
+      <translation>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="624"/>
       <source>No sense found for some part of the source word: </source>
-      <translation type="unfinished">No sense found for some part of the source word: </translation>
+      <translation>No sense found for some part of the source word: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="629"/>
       <source>No morphosyntactic analysis found for some part of the source word: </source>
-      <translation type="unfinished">No morphosyntactic analysis found for some part of the source word: </translation>
+      <translation>No morphosyntactic analysis found for some part of the source word: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="661"/>
       <source>No grammatical category found for the source word: </source>
-      <translation type="unfinished">No grammatical category found for the source word: </translation>
+      <translation>No grammatical category found for the source word: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="678"/>
       <source>Couldn't find the sense for source headword: </source>
-      <translation type="unfinished">Couldn't find the sense for source headword: </translation>
+      <translation>Couldn't find the sense for source headword: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="691"/>
       <source>No root or stem found for source word: </source>
-      <translation type="unfinished">No root or stem found for source word: </translation>
+      <translation>No root or stem found for source word: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="701"/>
       <source>One or more unknown words occurred multiple times.</source>
-      <translation type="unfinished">One or more unknown words occurred multiple times.</translation>
+      <translation>One or more unknown words occurred multiple times.</translation>
     </message>
   </context>
 </TS>
