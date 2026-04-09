8018b720adee389ffc4f7c4a7878e3bf95d49055
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:33 +0200

    New translations linksensetool.ts (Spanish)

--- Dev/Modules/translations/LinkSenseTool_es.ts
+++ Dev/Modules/translations/LinkSenseTool_es.ts
@@ -5,12 +5,12 @@
     <message>
       <location filename="../LinkSenseTool.py" line="198"/>
       <source>Sense Linker Tool</source>
-      <translation type="unfinished">Sense Linker Tool</translation>
+      <translation>Herramienta de enlace de sentidos</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="201"/>
       <source>Link source and target senses.</source>
-      <translation type="unfinished">Link source and target senses.</translation>
+      <translation>Vincular sentidos de origen y destino.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="203"/>
@@ -32,49 +32,49 @@ This module requires
 a sense-level custom field in your source project. It should be simple text field.
 The purpose of the custom field is to hold the link to a sense in the target project.
 Set which custom field is used for linking in the settings.</source>
-      <translation type="unfinished">This module will create links 
-in the source project to senses in the target project. It will show a window
-with a list of all the senses in the text. White background rows indicate links that
-already exist. blue background rows indicate suggested links based on an exact match
-on gloss, light blue background rows indicate suggested links based on a close match on
-gloss (currently 75% similar), red background rows
-have no link yet established. Double-click on the Target Head Word column for a row to copy
-the currently selected target sense in the upper combo box into that row. Click the checkbox
-to create a link for that row. I.e. the source sense will be linked to the target sense.
-Unchecking a checkbox for white row will unlink the specified sense from its target sense.
-Close matches are only attempted for words with five letters or longer.
-For suggested sense pairs where
-there is a mismatch in the grammatical category, both categories are colored red. This
-is to indicate you may not want to link the two sense even though the glosses match. 
-This module requires
-a sense-level custom field in your source project. It should be simple text field.
-The purpose of the custom field is to hold the link to a sense in the target project.
-Set which custom field is used for linking in the settings.</translation>
+      <translation>Este módulo creará enlaces 
+en el proyecto de origen a los sentidos en el proyecto de destino. Mostrará una ventana
+con una lista de todos los sentidos en el texto. Las filas con fondo blanco indican enlaces que
+ya existen. Las filas con fondo azul indican enlaces sugeridos basados en una coincidencia exacta
+en la glosa, las filas con fondo azul claro indican enlaces sugeridos basados en una coincidencia cercana
+en la glosa (actualmente 75% similar), las filas con fondo rojo
+aún no tienen un enlace establecido. Haga doble clic en la columna &quot;Palabra principal del destino&quot; para una fila para copiar
+el sentido de destino seleccionado actualmente en el cuadro combinado superior a esa fila. Marque la casilla
+para crear un enlace para esa fila. Es decir, el sentido de origen se vinculará al sentido de destino.
+Desmarcar una casilla para una fila blanca desvinculará el sentido especificado de su sentido de destino.
+Las coincidencias cercanas solo se intentan para palabras con cinco letras o más.
+Para pares de sentidos sugeridos donde
+hay una discrepancia en la categoría gramatical, ambas categorías se colorean en rojo. Esto
+es para indicar que puede que no desee vincular los dos sentidos aunque las glosas coincidan.
+Este módulo requiere
+un campo personalizado a nivel de sentido en su proyecto de origen. Debe ser un campo de texto simple.
+El propósito del campo personalizado es contener el enlace a un sentido en el proyecto de destino.
+Configure qué campo personalizado se utiliza para vincular en la configuración.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1144"/>
       <source>Save Changes</source>
-      <translation type="unfinished">Save Changes</translation>
+      <translation>Guardar cambios</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1144"/>
       <source>Do you want to save your changes?</source>
-      <translation type="unfinished">Do you want to save your changes?</translation>
+      <translation>¿Desea guardar los cambios?</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1265"/>
       <source>Empty grammatical category found for the target word: </source>
-      <translation type="unfinished">Empty grammatical category found for the target word: </translation>
+      <translation>Se encontró una categoría gramatical vacía para la palabra de destino: </translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1289"/>
       <source>Empty gloss found for the target word: </source>
-      <translation type="unfinished">Empty gloss found for the target word: </translation>
+      <translation>Se encontró una glosa vacía para la palabra de destino: </translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1294"/>
       <source>More than {num_warnings} empty glosses found. Suppressing further warnings for empty target glosses.</source>
-      <translation type="unfinished">More than {num_warnings} empty glosses found. Suppressing further warnings for empty target glosses.</translation>
+      <translation>Se encontraron más de {num_warnings} glosas vacías. Se suprimirán más advertencias para glosas de destino vacías.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1512"/>
@@ -84,17 +84,17 @@ Set which custom field is used for linking in the settings.</translation>
     <message>
       <location filename="../LinkSenseTool.py" line="1619"/>
       <source>1 link created.</source>
-      <translation type="unfinished">1 link created.</translation>
+      <translation>1 enlace creado.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1621"/>
       <source>{num} links created.</source>
-      <translation type="unfinished">{num} links created.</translation>
+      <translation>{num} enlaces creados.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1625"/>
       <source>1 link removed</source>
-      <translation type="unfinished">1 link removed</translation>
+      <translation>1 enlace eliminado.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1629"/>
@@ -104,92 +104,92 @@ Set which custom field is used for linking in the settings.</translation>
     <message>
       <location filename="../LinkSenseTool.py" line="1697"/>
       <source>Gloss</source>
-      <translation type="unfinished">Gloss</translation>
+      <translation>Glosa</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1698"/>
       <source>Category</source>
-      <translation type="unfinished">Category</translation>
+      <translation>Categoría</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1700"/>
       <source>Comment</source>
-      <translation type="unfinished">Comment</translation>
+      <translation>Comentario</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1799"/>
       <source>Permission error writing {htmlFileName}. Perhaps the file is in use in another program?</source>
-      <translation type="unfinished">Permission error writing {htmlFileName}. Perhaps the file is in use in another program?</translation>
+      <translation>Error de permisos al escribir {htmlFileName}. ¿Quizás el archivo está en uso en otro programa?</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1803"/>
       <source>Error writing {htmlFileName}.</source>
-      <translation type="unfinished">Error writing {htmlFileName}.</translation>
+      <translation>Error al escribir {htmlFileName}.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1807"/>
       <source>{cnt} words written to the file: {htmlFileName}. You'll find it in the Output folder.</source>
-      <translation type="unfinished">{cnt} words written to the file: {htmlFileName}. You'll find it in the Output folder.</translation>
+      <translation>{cnt} palabras escritas en el archivo: {htmlFileName}. Lo encontrará en la carpeta de salida.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1810"/>
       <source>No unlinked words. Nothing exported.</source>
-      <translation type="unfinished">No unlinked words. Nothing exported.</translation>
+      <translation>No hay palabras no vinculadas. Nada exportado.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1825"/>
       <source>No Source Text Name has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Text Name has been set. Please go to Settings and fix this.</translation>
+      <translation>No se ha establecido un nombre de texto de origen. Por favor, vaya a Configuración y solucione esto.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1830"/>
       <source>No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</translation>
+      <translation>No se ha establecido un campo personalizado de origen para el enlace de entrada. Por favor, vaya a Configuración y solucione esto.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1836"/>
       <source>No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
+      <translation>No se han seleccionado tipos de morfemas de origen contados como raíces. Por favor, vaya a Configuración y solucione esto.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1841"/>
       <source>No Target Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Target Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
+      <translation>No se han seleccionado tipos de morfemas de destino contados como raíces. Por favor, vaya a Configuración y solucione esto.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1855"/>
       <source>The text named: {sourceTextName} not found.</source>
-      <translation type="unfinished">The text named: {sourceTextName} not found.</translation>
+      <translation>No se encontró el texto llamado: {sourceTextName}.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1872"/>
       <source>{linkField} field doesn't exist. Please read the instructions.</source>
-      <translation type="unfinished">{linkField} field doesn't exist. Please read the instructions.</translation>
+      <translation>El campo {linkField} no existe. Por favor, lea las instrucciones.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1886"/>
       <source>The target project does not exist. Please check the configuration file.</source>
-      <translation type="unfinished">The target project does not exist. Please check the configuration file.</translation>
+      <translation>El proyecto de destino no existe. Por favor, revise el archivo de configuración.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1889"/>
       <source>Opening: {targetProj} as the target project.</source>
-      <translation type="unfinished">Opening: {targetProj} as the target project.</translation>
+      <translation>Abriendo: {targetProj} como el proyecto de destino.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1896"/>
       <source>Failed to open the target project.</source>
-      <translation type="unfinished">Failed to open the target project.</translation>
+      <translation>No se pudo abrir el proyecto de destino.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1899"/>
       <source>Starting {moduleName} for text: {sourceTextName}.</source>
-      <translation type="unfinished">Starting {moduleName} for text: {sourceTextName}.</translation>
+      <translation>Iniciando {moduleName} para el texto: {sourceTextName}.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1928"/>
       <source>There were no senses found for linking. Please check your text and approve some words.</source>
-      <translation type="unfinished">There were no senses found for linking. Please check your text and approve some words.</translation>
+      <translation>No se encontraron sentidos para vincular. Por favor, revise su texto y apruebe algunas palabras.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1944"/>
@@ -199,42 +199,42 @@ Set which custom field is used for linking in the settings.</translation>
     <message>
       <location filename="../LinkSenseTool.py" line="1946"/>
       <source>Link it!</source>
-      <translation type="unfinished">Link it!</translation>
+      <translation>¡Vincúlalo!</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1948"/>
       <source>Source Head Word</source>
-      <translation type="unfinished">Source Head Word</translation>
+      <translation>Palabra principal de origen</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1949"/>
       <source>Source Category</source>
-      <translation type="unfinished">Source Category</translation>
+      <translation>Categoría de origen</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1950"/>
       <source>Source Gloss</source>
-      <translation type="unfinished">Source Gloss</translation>
+      <translation>Glosa de origen</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1951"/>
       <source>Target Head Word</source>
-      <translation type="unfinished">Target Head Word</translation>
+      <translation>Palabra principal de destino</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1952"/>
       <source>Target Category</source>
-      <translation type="unfinished">Target Category</translation>
+      <translation>Categoría de destino</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1953"/>
       <source>Target Gloss</source>
-      <translation type="unfinished">Target Gloss</translation>
+      <translation>Glosa de destino</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="2009"/>
       <source>You need to run this module in &quot;modify mode.&quot;</source>
-      <translation type="unfinished">You need to run this module in &quot;modify mode.&quot;</translation>
+      <translation>Debe ejecutar este módulo en &quot;modo de modificación&quot;.</translation>
     </message>
   </context>
 </TS>
