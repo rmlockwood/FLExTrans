885946eef64615b1e02cb20da1937ce46c42c940
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:20:05 +0200

    New translations fixupsynthtext.ts (German)

--- Dev/Modules/translations/FixUpSynthText_de.ts
+++ Dev/Modules/translations/FixUpSynthText_de.ts
@@ -1,51 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
     <name>FixUpSynthText</name>
     <message>
-        <location filename="../FixUpSynthText.py" line="76"/>
-        <source>Fix Up Synthesis Text</source>
-        <translation>Synthese-Text korrigieren</translation>
+      <location filename="../FixUpSynthText.py" line="82"/>
+      <source>Fix Up Synthesis Text</source>
+      <translation type="unfinished">Fix Up Synthesis Text</translation>
     </message>
     <message>
-        <location filename="../FixUpSynthText.py" line="79"/>
-        <source>Run a set of post-synthesis search and replace operations.</source>
-        <translation>Führen Sie eine Reihe von Such- und Ersetzungsoperationen nach der Synthese durch.</translation>
+      <location filename="../FixUpSynthText.py" line="85"/>
+      <source>Run a set of post-synthesis search and replace operations.</source>
+      <translation type="unfinished">Run a set of post-synthesis search and replace operations.</translation>
     </message>
     <message>
-        <location filename="../FixUpSynthText.py" line="72"/>
-        <source>This module will run a set of search and replace operations to fix up the text that comes out of 
-synthesis. The operations are defined with the Text Out Rules module. The rules are stored in the
-Fix Up Synthesis Text Rules File as specified in the Settings.</source>
-        <translation type="obsolete">Dieses Modul führt eine Reihe von Such- und Ersetzungsoperationen durch, um den Text zu korrigieren, der aus der Synthese stammt. Die Operationen werden mit dem Modul &quot;Text Out Rules&quot; definiert. Die Regeln werden in der Datei &quot;Fix Up Synthesis Text Rules&quot; gespeichert, wie in den Einstellungen angegeben.</translation>
-    </message>
-    <message>
-        <location filename="../FixUpSynthText.py" line="119"/>
-        <source>The rules file: {textOutRulesFile} could not be found. Use the Text Out Rules module to define the rules.</source>
-        <translation>Die Regeldatei: {textOutRulesFile} konnte nicht gefunden werden. Verwenden Sie das Modul &quot;Text Out Rules&quot;, um die Regeln zu definieren.</translation>
-    </message>
-    <message>
-        <location filename="../FixUpSynthText.py" line="132"/>
-        <source>The rules file: {textOutRulesFile} has invalid XML data.</source>
-        <translation>Die Regeldatei: {textOutRulesFile} enthält ungültige XML-Daten.</translation>
-    </message>
-    <message>
-        <location filename="../FixUpSynthText.py" line="140"/>
-        <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: '{synthFile}'.</source>
-        <translation>Das Modul &quot;Synthesize Text&quot; muss vor diesem ausgeführt werden. Die Synthesedatei konnte nicht geöffnet werden: '{synthFile}'.</translation>
-    </message>
-    <message>
-        <location filename="../FixUpSynthText.py" line="164"/>
-        <source>The synthesis file was fixed using {numRules} 'Text Out' rules.</source>
-        <translation>Die Synthesedatei wurde mit {numRules} 'Text Out'-Regeln korrigiert.</translation>
-    </message>
-    <message>
-        <location filename="../FixUpSynthText.py" line="81"/>
-        <source>This module will run a set of search and replace operations to fix up the text that comes out of 
+      <location filename="../FixUpSynthText.py" line="87"/>
+      <source>This module will run a set of search and replace operations to fix up the text that comes out of
 synthesis. The operations are defined with the {textOutRulesModule} module. The rules are stored in the
 Fix Up Synthesis Text Rules File as specified in the Settings.</source>
-        <translation>Dieses Modul führt eine Reihe von Such- und Ersetzungsoperationen durch, um den Text zu korrigieren, der aus der Synthese stammt. Die Operationen werden mit dem {textOutRulesModule}-Modul definiert. Die Regeln werden in der Datei &quot;Fix Up Synthesis Text Rules&quot; gespeichert, wie in den Einstellungen angegeben.</translation>
+      <translation type="unfinished">This module will run a set of search and replace operations to fix up the text that comes out of
+synthesis. The operations are defined with the {textOutRulesModule} module. The rules are stored in the
+Fix Up Synthesis Text Rules File as specified in the Settings.</translation>
     </message>
-</context>
+    <message>
+      <location filename="../FixUpSynthText.py" line="125"/>
+      <source>The rules file: {textOutRulesFile} could not be found. Use the Text Out Rules module to define the rules.</source>
+      <translation type="unfinished">The rules file: {textOutRulesFile} could not be found. Use the Text Out Rules module to define the rules.</translation>
+    </message>
+    <message>
+      <location filename="../FixUpSynthText.py" line="138"/>
+      <source>The rules file: {textOutRulesFile} has invalid XML data.</source>
+      <translation type="unfinished">The rules file: {textOutRulesFile} has invalid XML data.</translation>
+    </message>
+    <message>
+      <location filename="../FixUpSynthText.py" line="146"/>
+      <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: '{synthFile}'.</source>
+      <translation type="unfinished">The Synthesize Text module must be run before this one. Could not open the synthesis file: '{synthFile}'.</translation>
+    </message>
+    <message>
+      <location filename="../FixUpSynthText.py" line="170"/>
+      <source>The synthesis file was fixed using {numRules} 'Text Out' rules.</source>
+      <translation type="unfinished">The synthesis file was fixed using {numRules} 'Text Out' rules.</translation>
+    </message>
+  </context>
 </TS>
