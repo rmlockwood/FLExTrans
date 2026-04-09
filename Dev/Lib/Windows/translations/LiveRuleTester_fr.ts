223a340d411871227485baae61ddcad55f697ded
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:17:42 +0200

    New translations liveruletester.ts (French)

--- Dev/Lib/Windows/translations/LiveRuleTester_fr.ts
+++ Dev/Lib/Windows/translations/LiveRuleTester_fr.ts
@@ -1,169 +1,169 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
     <name>LRTWindow</name>
     <message>
-        <location filename="../LiveRuleTester.py" line="598"/>
-        <source>Live Rule Tester</source>
-        <translation>Testeur de règles en direct</translation>
+      <location filename="../LiveRuleTester.py" line="598"/>
+      <source>Live Rule Tester</source>
+      <translation type="unfinished">Live Rule Tester</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="599"/>
-        <source>Source Text</source>
-        <translation>Texte source</translation>
+      <location filename="../LiveRuleTester.py" line="599"/>
+      <source>Source Text</source>
+      <translation type="unfinished">Source Text</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="600"/>
-        <source>Refresh Source Lexicon</source>
-        <translation>Actualiser le lexique source</translation>
+      <location filename="../LiveRuleTester.py" line="600"/>
+      <source>Refresh Source Lexicon</source>
+      <translation type="unfinished">Refresh Source Lexicon</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="601"/>
-        <source>Rebuild &amp;Bilingual Lexicon</source>
-        <translation>Reconstruire le &amp;lexique bilingue</translation>
+      <location filename="../LiveRuleTester.py" line="601"/>
+      <source>Rebuild &amp;Bilingual Lexicon</source>
+      <translation type="unfinished">Rebuild &amp;Bilingual Lexicon</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="602"/>
-        <source>&amp;View Bilingual Lexicon</source>
-        <translation>&amp;Afficher le lexique bilingue</translation>
+      <location filename="../LiveRuleTester.py" line="602"/>
+      <source>&amp;View Bilingual Lexicon</source>
+      <translation type="unfinished">&amp;View Bilingual Lexicon</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="603"/>
-        <source>Replacement Dictionary Editor</source>
-        <translation>Éditeur de dictionnaire de remplacement</translation>
+      <location filename="../LiveRuleTester.py" line="603"/>
+      <source>Replacement Dictionary Editor</source>
+      <translation type="unfinished">Replacement Dictionary Editor</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="604"/>
-        <source>Hover over a word to see its bilingual dictionary entry.</source>
-        <translation>Survolez un mot pour voir son entrée dans le dictionnaire bilingue.</translation>
+      <location filename="../LiveRuleTester.py" line="604"/>
+      <source>Hover over a word to see its bilingual dictionary entry.</source>
+      <translation type="unfinished">Hover over a word to see its bilingual dictionary entry.</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="605"/>
-        <source>Select Words</source>
-        <translation>Sélectionner des mots</translation>
+      <location filename="../LiveRuleTester.py" line="605"/>
+      <source>Select Words</source>
+      <translation type="unfinished">Select Words</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="606"/>
-        <source>Select Sentences</source>
-        <translation>Sélectionner des phrases</translation>
+      <location filename="../LiveRuleTester.py" line="606"/>
+      <source>Select Sentences</source>
+      <translation type="unfinished">Select Sentences</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="607"/>
-        <source>Manual Entry</source>
-        <translation>Saisie manuelle</translation>
+      <location filename="../LiveRuleTester.py" line="607"/>
+      <source>Manual Entry</source>
+      <translation type="unfinished">Manual Entry</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="608"/>
-        <source>Select transfer rules to test</source>
-        <translation>Sélectionner les règles de transfert à tester</translation>
+      <location filename="../LiveRuleTester.py" line="608"/>
+      <source>Select transfer rules to test</source>
+      <translation type="unfinished">Select transfer rules to test</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="609"/>
-        <source>&amp;Refresh Rules</source>
-        <translation>&amp;Actualiser les règles</translation>
+      <location filename="../LiveRuleTester.py" line="609"/>
+      <source>&amp;Refresh Rules</source>
+      <translation type="unfinished">&amp;Refresh Rules</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="610"/>
-        <source>E&amp;dit Transfer Rules</source>
-        <translation>Mo&amp;difier les règles de transfert</translation>
+      <location filename="../LiveRuleTester.py" line="610"/>
+      <source>E&amp;dit Transfer Rules</source>
+      <translation type="unfinished">E&amp;dit Transfer Rules</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="611"/>
-        <source>Rule Execution Information</source>
-        <translation>Informations sur l'exécution des règles</translation>
+      <location filename="../LiveRuleTester.py" line="611"/>
+      <source>Rule Execution Information</source>
+      <translation type="unfinished">Rule Execution Information</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="615"/>
-        <source>Transfer</source>
-        <translation>Transfert</translation>
+      <location filename="../LiveRuleTester.py" line="615"/>
+      <source>Transfer</source>
+      <translation type="unfinished">Transfer</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="613"/>
-        <source>Target Text</source>
-        <translation>Texte cible</translation>
+      <location filename="../LiveRuleTester.py" line="613"/>
+      <source>Target Text</source>
+      <translation type="unfinished">Target Text</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="614"/>
-        <source>Advanced options</source>
-        <translation>Options avancées</translation>
+      <location filename="../LiveRuleTester.py" line="614"/>
+      <source>Advanced options</source>
+      <translation type="unfinished">Advanced options</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="616"/>
-        <source>Open Rule Assistant</source>
-        <translation>Ouvrir l'assistant de règles</translation>
+      <location filename="../LiveRuleTester.py" line="616"/>
+      <source>Open Rule Assistant</source>
+      <translation type="unfinished">Open Rule Assistant</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="617"/>
-        <source>Synthesized Text</source>
-        <translation>Texte synthétisé</translation>
+      <location filename="../LiveRuleTester.py" line="617"/>
+      <source>Synthesized Text</source>
+      <translation type="unfinished">Synthesized Text</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="618"/>
-        <source>Only the last word will be traced.</source>
-        <translation>Seul le dernier mot sera tracé.</translation>
+      <location filename="../LiveRuleTester.py" line="618"/>
+      <source>Only the last word will be traced.</source>
+      <translation type="unfinished">Only the last word will be traced.</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="619"/>
-        <source>Trace HermitCrab synthesis</source>
-        <translation>Tracer la synthèse HermitCrab</translation>
+      <location filename="../LiveRuleTester.py" line="619"/>
+      <source>Trace HermitCrab synthesis</source>
+      <translation type="unfinished">Trace HermitCrab synthesis</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="620"/>
-        <source>&amp;Synthesize</source>
-        <translation>&amp;Synthétiser</translation>
+      <location filename="../LiveRuleTester.py" line="620"/>
+      <source>&amp;Synthesize</source>
+      <translation type="unfinished">&amp;Synthesize</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="621"/>
-        <source>Refresh &amp;Target Lexicon</source>
-        <translation>Actualiser le &amp;lexique cible</translation>
+      <location filename="../LiveRuleTester.py" line="621"/>
+      <source>Refresh &amp;Target Lexicon</source>
+      <translation type="unfinished">Refresh &amp;Target Lexicon</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="622"/>
-        <source>Apply the search/replace rules defined in the Text Out Rules module.</source>
-        <translation>Appliquer les règles de recherche/remplacement définies dans le module Règles de sortie de texte.</translation>
+      <location filename="../LiveRuleTester.py" line="622"/>
+      <source>Apply the search/replace rules defined in the Text Out Rules module.</source>
+      <translation type="unfinished">Apply the search/replace rules defined in the Text Out Rules module.</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="623"/>
-        <source>Apply Text Out rules</source>
-        <translation>Appliquer les règles de sortie de texte</translation>
+      <location filename="../LiveRuleTester.py" line="623"/>
+      <source>Apply Text Out rules</source>
+      <translation type="unfinished">Apply Text Out rules</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="624"/>
-        <source>If you have checked the FLExTrans setting to clean up unknown words,
+      <location filename="../LiveRuleTester.py" line="624"/>
+      <source>If you have checked the FLExTrans setting to clean up unknown words,
 this keeps the clean up from happening so you can more easily see errors.</source>
-        <translation>Si vous avez coché le paramètre FLExTrans pour nettoyer les mots inconnus,
-cela empêche le nettoyage de se produire afin que vous puissiez voir plus facilement les erreurs.</translation>
+      <translation type="unfinished">If you have checked the FLExTrans setting to clean up unknown words,
+this keeps the clean up from happening so you can more easily see errors.</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="626"/>
-        <source>Do not clean up unknown words</source>
-        <translation>Ne pas nettoyer les mots inconnus</translation>
+      <location filename="../LiveRuleTester.py" line="626"/>
+      <source>Do not clean up unknown words</source>
+      <translation type="unfinished">Do not clean up unknown words</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="627"/>
-        <source>&amp;Add to Testbed</source>
-        <translation>&amp;Ajouter au banc d'essai</translation>
+      <location filename="../LiveRuleTester.py" line="627"/>
+      <source>&amp;Add to Testbed</source>
+      <translation type="unfinished">&amp;Add to Testbed</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="628"/>
-        <source>Add multiple words to the testbed word by word</source>
-        <translation>Ajouter plusieurs mots au banc d'essai mot par mot</translation>
+      <location filename="../LiveRuleTester.py" line="628"/>
+      <source>Add multiple words to the testbed word by word</source>
+      <translation type="unfinished">Add multiple words to the testbed word by word</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="629"/>
-        <source>&amp;Edit Testbed</source>
-        <translation>&amp;Modifier le banc d'essai</translation>
+      <location filename="../LiveRuleTester.py" line="629"/>
+      <source>&amp;Edit Testbed</source>
+      <translation type="unfinished">&amp;Edit Testbed</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="630"/>
-        <source>&amp;View Testbed Log</source>
-        <translation>&amp;Afficher le journal du banc d'essai</translation>
+      <location filename="../LiveRuleTester.py" line="630"/>
+      <source>&amp;View Testbed Log</source>
+      <translation type="unfinished">&amp;View Testbed Log</translation>
     </message>
     <message>
-        <location filename="../LiveRuleTester.py" line="632"/>
-        <source>Zoom:</source>
-        <translation>Zoom :</translation>
+      <location filename="../LiveRuleTester.py" line="632"/>
+      <source>Zoom:</source>
+      <translation type="unfinished">Zoom:</translation>
     </message>
-</context>
+  </context>
 </TS>
