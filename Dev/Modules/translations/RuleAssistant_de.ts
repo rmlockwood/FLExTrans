5d21babba63d6fc7fa3c8403e3af5112ec3eaaee
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:44 +0200

    New translations ruleassistant.ts (German)

--- Dev/Modules/translations/RuleAssistant_de.ts
+++ Dev/Modules/translations/RuleAssistant_de.ts
@@ -6,54 +6,54 @@
     <message>
       <location filename="../RuleAssistant.py" line="102"/>
       <source>This module runs a tool which let's you create transfer rules.</source>
-      <translation type="unfinished">This module runs a tool which let's you create transfer rules.</translation>
+      <translation>Dieses Modul führt ein Tool aus, mit dem Sie Übertragungsregeln erstellen können.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="103"/>
       <source>Rule Assistant</source>
-      <translation type="unfinished">Rule Assistant</translation>
+      <translation>Regelassistent</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="106"/>
       <source>Runs a tool for creating transfer rules.</source>
-      <translation type="unfinished">Runs a tool for creating transfer rules.</translation>
+      <translation>Führt ein Tool aus, mit dem Sie Übertragungsregeln erstellen können</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="347"/>
       <source>Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</source>
-      <translation type="unfinished">Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</translation>
+      <translation>Zweisprachiges Wörterbuch nicht gefunden. Erstellen Sie das zweisprachige Wörterbuch, um Testdaten im {ruleAssistant} anzuzeigen.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="359"/>
       <source>The text named '%s' was not found.</source>
-      <translation type="unfinished">The text named '%s' was not found.</translation>
+      <translation>Der Text mit dem Namen '%s' wurde nicht gefunden.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="379"/>
       <source>Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</source>
-      <translation type="unfinished">Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</translation>
+      <translation>Kompiliertes zweisprachiges Wörterbuch nicht gefunden. Beim Kompilieren des zweisprachigen Wörterbuchs ist ein Fehler aufgetreten.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="395"/>
       <source>&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </source>
-      <translation type="unfinished">&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </translation>
+      <translation>&lt;p&gt;&lt;b&gt;Quelltext:&lt;/b&gt; </translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="434"/>
       <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
 </source>
-      <translation type="unfinished">&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
+      <translation>&lt;html&gt;&lt;body&gt;&lt;p&gt;Keine Testdaten verfügbar.&lt;/body&gt;&lt;/html&gt;
 </translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="469"/>
       <source>An error happened when running the {ruleAssistant} tool: {error}</source>
-      <translation type="unfinished">An error happened when running the {ruleAssistant} tool: {error}</translation>
+      <translation>Beim Ausführen des {ruleAssistant}-Tools ist ein Fehler aufgetreten: {error}</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="527"/>
       <source>No rules created.</source>
-      <translation type="unfinished">No rules created.</translation>
+      <translation>Keine Regeln erstellt.</translation>
     </message>
   </context>
 </TS>
