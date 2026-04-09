0f9ea8f43c43f11c1c1586574c2f7d06a9d5a70e
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:18:11 +0200

    New translations inserttargettext.ts (French)

--- Dev/Modules/translations/InsertTargetText_fr.ts
+++ Dev/Modules/translations/InsertTargetText_fr.ts
@@ -1,54 +1,54 @@
-<?xml version='1.0' encoding='utf-8'?>
-<TS version="2.1">
-<context>
+<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
     <name>InsertTargetText</name>
     <message>
-        <location filename="../InsertTargetText.py" line="107"/>
-        <source>Insert Target Text</source>
-        <translation>Insérer le texte cible</translation>
+      <location filename="../InsertTargetText.py" line="107"/>
+      <source>Insert Target Text</source>
+      <translation type="unfinished">Insert Target Text</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="110"/>
-        <source>Insert a translated text into the target FLEx project.</source>
-        <translation>Insérer un texte traduit dans le projet FLEx cible.</translation>
+      <location filename="../InsertTargetText.py" line="110"/>
+      <source>Insert a translated text into the target FLEx project.</source>
+      <translation type="unfinished">Insert a translated text into the target FLEx project.</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="112"/>
-        <source>The target project specified in the settings will be used. This module will take
+      <location filename="../InsertTargetText.py" line="112"/>
+      <source>The target project specified in the settings will be used. This module will take
 the results of the {doSynthModule} module
 and insert the text into the target FLEx project. The Source Text Name setting
 will be used for the text name in the target project. An existing text of the 
 same name will not be overwritten. A copy will be created.</source>
-        <translation>Le projet cible spécifié dans les paramètres sera utilisé. Ce module prendra
-les résultats du module {doSynthModule}
-et insérera le texte dans le projet FLEx cible. Le paramètre Nom du texte source
-sera utilisé pour le nom du texte dans le projet cible. Un texte existant du
-même nom ne sera pas écrasé. Une copie sera créée.</translation>
+      <translation type="unfinished">The target project specified in the settings will be used. This module will take
+the results of the {doSynthModule} module
+and insert the text into the target FLEx project. The Source Text Name setting
+will be used for the text name in the target project. An existing text of the 
+same name will not be overwritten. A copy will be created.</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="137"/>
-        <source>Failed to open the target project.</source>
-        <translation>Échec de l'ouverture du projet cible.</translation>
+      <location filename="../InsertTargetText.py" line="137"/>
+      <source>Failed to open the target project.</source>
+      <translation type="unfinished">Failed to open the target project.</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="140"/>
-        <source>Using: {targetProj} as the target project.</source>
-        <translation>Utilisation : {targetProj} comme projet cible.</translation>
+      <location filename="../InsertTargetText.py" line="140"/>
+      <source>Using: {targetProj} as the target project.</source>
+      <translation type="unfinished">Using: {targetProj} as the target project.</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="158"/>
-        <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: "</source>
-        <translation>Le module Synthétiser le texte doit être exécuté avant celui-ci. Impossible d'ouvrir le fichier de synthèse : "</translation>
+      <location filename="../InsertTargetText.py" line="158"/>
+      <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: "</source>
+      <translation type="unfinished">The Synthesize Text module must be run before this one. Could not open the synthesis file: "</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="193"/>
-        <source>Text: "{sourceTextName}" created in the {targetProj} project.</source>
-        <translation>Texte : "{sourceTextName}" créé dans le projet {targetProj}.</translation>
+      <location filename="../InsertTargetText.py" line="193"/>
+      <source>Text: "{sourceTextName}" created in the {targetProj} project.</source>
+      <translation type="unfinished">Text: "{sourceTextName}" created in the {targetProj} project.</translation>
     </message>
     <message>
-        <location filename="../InsertTargetText.py" line="202"/>
-        <source>You need to run this module in "modify mode."</source>
-        <translation>Vous devez exécuter ce module en "mode modification".</translation>
+      <location filename="../InsertTargetText.py" line="202"/>
+      <source>You need to run this module in "modify mode."</source>
+      <translation type="unfinished">You need to run this module in "modify mode."</translation>
     </message>
-</context>
+  </context>
 </TS>
