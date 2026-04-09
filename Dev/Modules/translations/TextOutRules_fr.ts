4efd389a23fa1faa524b7b377dc2dc92601c6ce3
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:18:26 +0200

    New translations textoutrules.ts (French)

--- Dev/Modules/translations/TextOutRules_fr.ts
+++ Dev/Modules/translations/TextOutRules_fr.ts
@@ -1,24 +1,24 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
     <name>TextOutRules</name>
     <message>
-        <location filename="../TextOutRules.py" line="81"/>
-        <source>Text Out Rules</source>
-        <translation>Règles du texte en sortie</translation>
+      <location filename="../TextOutRules.py" line="81"/>
+      <source>Text Out Rules</source>
+      <translation type="unfinished">Text Out Rules</translation>
     </message>
     <message>
-        <location filename="../TextOutRules.py" line="84"/>
-        <source>Define and test a set of post-synthesis search and replace operations.</source>
-        <translation>Définir et tester un ensemble d'opérations de recherche et remplacement post-synthèse.</translation>
+      <location filename="../TextOutRules.py" line="84"/>
+      <source>Define and test a set of post-synthesis search and replace operations.</source>
+      <translation type="unfinished">Define and test a set of post-synthesis search and replace operations.</translation>
     </message>
     <message>
-        <location filename="../TextOutRules.py" line="86"/>
-        <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+      <location filename="../TextOutRules.py" line="86"/>
+      <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 synthesis. Regular expressions can be used if desired.</source>
-        <translation>Ce module est utilisé pour définir et tester un ensemble d'opérations de recherche et remplacement à utiliser pour corriger le texte qui sort de
-la synthèse. Les expressions régulières peuvent être utilisées si désiré.</translation>
+      <translation type="unfinished">This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+synthesis. Regular expressions can be used if desired.</translation>
     </message>
-</context>
+  </context>
 </TS>
