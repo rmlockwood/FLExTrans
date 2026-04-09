00682a2970b12c47f3fdca6a58fac47c3a243ae0
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 20:19:15 +0200

    New translations openflexprojects.ts (Spanish)

--- Dev/Modules/translations/OpenFLExProjects_es.ts
+++ Dev/Modules/translations/OpenFLExProjects_es.ts
@@ -1,52 +1,51 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
-<context>
+<TS version="2.1" language="es-ES" sourcelanguage="en">
+  <context>
     <name>OpenFLExProjects</name>
     <message>
-      <location filename="../OpenFLExProjects.py" line="118"/>
-      <source>Open Multiple FLEx Projects</source>
-      <translation>Abrir múltiples proyectos FLEx</translation>
-    </message>
-    <message>
-      <location filename="../OpenFLExProjects.py" line="58"/>
-      <source>Select one or more FLEx project and automatically open them one by one.</source>
-      <translation>Seleccione uno o más proyectos FLEx y ábralos automáticamente uno por uno.</translation>
-    </message>
-    <message>
-      <location filename="../OpenFLExProjects.py" line="60"/>
-      <source>Select one or more FLEx project and automatically open them one by one. The tool waits until one project is open before opening the next.</source>
-      <translation>Seleccione uno o más proyectos FLEx y ábralos automáticamente uno por uno. La herramienta espera hasta que un proyecto esté abierto antes de abrir el siguiente.</translation>
-    </message>
-    <message>
       <location filename="../OpenFLExProjects.py" line="85"/>
+      <source>Open Multiple FLEx Projects</source>
+      <translation type="unfinished">Open Multiple FLEx Projects</translation>
+    </message>
+    <message>
+      <location filename="../OpenFLExProjects.py" line="67"/>
+      <source>Select one or more FLEx project and automatically open them one by one.</source>
+      <translation type="unfinished">Select one or more FLEx project and automatically open them one by one.</translation>
+    </message>
+    <message>
+      <location filename="../OpenFLExProjects.py" line="69"/>
+      <source>Select one or more FLEx project and automatically open them one by one. The tool waits until one project is open before opening the next.</source>
+      <translation type="unfinished">Select one or more FLEx project and automatically open them one by one. The tool waits until one project is open before opening the next.</translation>
+    </message>
+    <message>
+      <location filename="../OpenFLExProjects.py" line="94"/>
       <source>FLEx project names (multi-select):</source>
-      <translation>Nombres de proyectos FLEx (selección múltiple):</translation>
+      <translation type="unfinished">FLEx project names (multi-select):</translation>
     </message>
     <message>
-      <location filename="../OpenFLExProjects.py" line="95"/>
+      <location filename="../OpenFLExProjects.py" line="104"/>
       <source>OK</source>
-      <translation>OK</translation>
+      <translation type="unfinished">OK</translation>
     </message>
     <message>
-      <location filename="../OpenFLExProjects.py" line="96"/>
+      <location filename="../OpenFLExProjects.py" line="105"/>
       <source>Cancel</source>
-      <translation>Cancelar</translation>
+      <translation type="unfinished">Cancel</translation>
     </message>
     <message>
-      <location filename="../OpenFLExProjects.py" line="152"/>
+      <location filename="../OpenFLExProjects.py" line="165"/>
       <source>The {proj} project is already open. Skipping.</source>
-      <translation>El proyecto {proj} ya está abierto. Omitiendo.</translation>
+      <translation type="unfinished">The {proj} project is already open. Skipping.</translation>
     </message>
     <message>
-      <location filename="../OpenFLExProjects.py" line="164"/>
+      <location filename="../OpenFLExProjects.py" line="177"/>
       <source>Couldn't detect the {proj} project as open after {secs} seconds. Skipping.</source>
-      <translation>No se pudo detectar el proyecto {proj} como abierto después de {secs} segundos. Omitiendo.</translation>
+      <translation type="unfinished">Couldn't detect the {proj} project as open after {secs} seconds. Skipping.</translation>
     </message>
     <message>
-      <location filename="../OpenFLExProjects.py" line="168"/>
+      <location filename="../OpenFLExProjects.py" line="181"/>
       <source>The {proj} project took about {secs} seconds to open.</source>
-      <translation>El proyecto {proj} tardó aproximadamente {secs} segundos en abrirse.</translation>
+      <translation type="unfinished">The {proj} project took about {secs} seconds to open.</translation>
     </message>
   </context>
 </TS>
