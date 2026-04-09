ce59bbc5e5d6e1ef26f2f5132b47469416a0d503
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:27 +0200

    New translations generateparses.ts (Spanish)

--- Dev/Modules/translations/GenerateParses_es.ts
+++ Dev/Modules/translations/GenerateParses_es.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../GenerateParses.py" line="100"/>
       <source>Generate All Parses</source>
-      <translation type="unfinished">Generate All Parses</translation>
+      <translation>Generar todos los análisis</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="103"/>
       <source>Creates all possible parses from a FLEx project, in Apertium format.</source>
-      <translation type="unfinished">Creates all possible parses from a FLEx project, in Apertium format.</translation>
+      <translation>Crea todos los análisis posibles de un proyecto FLEx en formato Apertium.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="105"/>
@@ -23,146 +23,146 @@ In FLExTrans &gt; Settings, under Synthesis Test settings, it is possible to lim
 a single POS or Citation Form, or to a specified number of stems (stems will be chosen
 randomly). This module also outputs a human readable version of the parses (with glosses of roots
 and affixes) to the Parses Output File specified in the settings.</source>
-      <translation type="unfinished">This module creates an Apertium file (that can be converted for input to a Synthesizer process) with
-all the parses that can be generated from the target FLEx project, based on its inflectional templates.
-(It doesn't generate based on derivation information in the project and it doesn't yet handle
-clitics or variants.)
-In FLExTrans &gt; Settings, under Synthesis Test settings, it is possible to limit output to
-a single POS or Citation Form, or to a specified number of stems (stems will be chosen
-randomly). This module also outputs a human readable version of the parses (with glosses of roots
-and affixes) to the Parses Output File specified in the settings.</translation>
+      <translation>Este módulo crea un archivo Apertium (que puede convertirse para la entrada a un proceso de sintetizador) con
+todos los análisis que se pueden generar a partir del proyecto FLEx objetivo, basándose en sus plantillas de flexión.
+(No genera basándose en información de derivación en el proyecto y aún no maneja
+clíticos o variantes.)
+En FLExTrans &gt; Configuración, en la configuración de Prueba de Síntesis, es posible limitar la salida a
+un solo POS o Forma de Citación, o a un número especificado de raíces (las raíces se elegirán
+aleatoriamente). Este módulo también genera una versión legible por humanos de los análisis (con glosas de raíces
+y afijos) en el archivo de salida de análisis especificado en la configuración.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="168"/>
       <source>No tags found for slot {slotName} of template {templateName}. Skipping.</source>
-      <translation type="unfinished">No tags found for slot {slotName} of template {templateName}. Skipping.</translation>
+      <translation>No se encontraron etiquetas para el slot {slotName} de la plantilla {templateName}. Omitiendo.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="242"/>
       <source>  Not adding Inactive template {templateName} for Category {categoryAbbrev}</source>
-      <translation type="unfinished">  Not adding Inactive template {templateName} for Category {categoryAbbrev}</translation>
+      <translation>  No se agrega la plantilla inactiva {templateName} para la categoría {categoryAbbrev}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="246"/>
       <source>  Adding template {templateName} for Category {categoryAbbrev}</source>
-      <translation type="unfinished">  Adding template {templateName} for Category {categoryAbbrev}</translation>
+      <translation>  Agregando plantilla {templateName} para la categoría {categoryAbbrev}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="314"/>
       <source>Logging to {logFile}</source>
-      <translation type="unfinished">Logging to {logFile}</translation>
+      <translation>Registrando en {logFile}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="316"/>
       <source>There was a problem creating the log file: {logFile}.</source>
-      <translation type="unfinished">There was a problem creating the log file: {logFile}.</translation>
+      <translation>Hubo un problema al crear el archivo de registro: {logFile}.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="321"/>
       <source>  No focus POS. Please select at least one POS with a template.</source>
-      <translation type="unfinished">  No focus POS. Please select at least one POS with a template.</translation>
+      <translation>  Sin POS de enfoque. Por favor, seleccione al menos un POS con una plantilla.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="327"/>
       <source>  Only collecting templates for these POS: {focusPOS}</source>
-      <translation type="unfinished">  Only collecting templates for these POS: {focusPOS}</translation>
+      <translation>  Solo recopilando plantillas para estos POS: {focusPOS}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="331"/>
       <source>Collecting templates from FLEx project...</source>
-      <translation type="unfinished">Collecting templates from FLEx project...</translation>
+      <translation>Recopilando plantillas del proyecto FLEx...</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="342"/>
       <source>  Not limiting number of stems</source>
-      <translation type="unfinished">  Not limiting number of stems</translation>
+      <translation>  No limitando el número de raíces</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="345"/>
       <source>  Only generating on the first {maxStems} stems</source>
-      <translation type="unfinished">  Only generating on the first {maxStems} stems</translation>
+      <translation>  Solo generando en las primeras {maxStems} raíces</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="350"/>
       <source>Processing entries</source>
-      <translation type="unfinished">Processing entries</translation>
+      <translation>Procesando entradas</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="380"/>
       <source>  Only generating on stem [{lex}]
 </source>
-      <translation type="unfinished">  Only generating on stem [{lex}]
+      <translation>  Solo generando en la raíz [{lex}]
 </translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="390"/>
       <source>  Skipping Variant with {count} Senses: {lex}</source>
-      <translation type="unfinished">  Skipping Variant with {count} Senses: {lex}</translation>
+      <translation>  Omitiendo variante con {count} sentidos: {lex}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="423"/>
       <source>  Adding [{thisGloss}]{lex}&lt;{pos}&gt; to roots list</source>
-      <translation type="unfinished">  Adding [{thisGloss}]{lex}&lt;{pos}&gt; to roots list</translation>
+      <translation>  Agregando [{thisGloss}]{lex}&lt;{pos}&gt; a la lista de raíces</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="427"/>
       <source>Using NoGloss as the gloss for {lex}.</source>
-      <translation type="unfinished">Using NoGloss as the gloss for {lex}.</translation>
+      <translation>Usando NoGloss como la glosa para {lex}.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="448"/>
       <source>Skipping deriv MSA for {lex}</source>
-      <translation type="unfinished">Skipping deriv MSA for {lex}</translation>
+      <translation>Omitiendo deriv MSA para {lex}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="502"/>
       <source>MSA missing POS in {lexForm} {lex}</source>
-      <translation type="unfinished">MSA missing POS in {lexForm} {lex}</translation>
+      <translation>MSA falta POS en {lexForm} {lex}</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="505"/>
       <source>POS msaPOS missing Abbreviation label</source>
-      <translation type="unfinished">POS msaPOS missing Abbreviation label</translation>
+      <translation>POS msaPOS falta etiqueta de abreviatura</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="510"/>
       <source>      Adding affix {lexForm} {lex} to slot [{slotName}]</source>
-      <translation type="unfinished">      Adding affix {lexForm} {lex} to slot [{slotName}]</translation>
+      <translation>      Agregando afijo {lexForm} {lex} al slot [{slotName}]</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="514"/>
       <source>Morph type {morphType} ignored.</source>
-      <translation type="unfinished">Morph type {morphType} ignored.</translation>
+      <translation>Tipo de morfema {morphType} ignorado.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="521"/>
       <source>Finished collecting templates. Now generating words.</source>
-      <translation type="unfinished">Finished collecting templates. Now generating words.</translation>
+      <translation>Finalizada la recopilación de plantillas. Ahora generando palabras.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="538"/>
       <source>There was a problem creating the Apertium file: {aperFile}.</source>
-      <translation type="unfinished">There was a problem creating the Apertium file: {aperFile}.</translation>
+      <translation>Hubo un problema al crear el archivo Apertium: {aperFile}.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="546"/>
       <source>There was a problem creating the words file: {outFile}.</source>
-      <translation type="unfinished">There was a problem creating the words file: {outFile}.</translation>
+      <translation>Hubo un problema al crear el archivo de palabras: {outFile}.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="574"/>
       <source>{wrdcnt} words generated.</source>
-      <translation type="unfinished">{wrdcnt} words generated.</translation>
+      <translation>{wrdcnt} palabras generadas.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="577"/>
       <source>Creation complete to the file: {outFile}.</source>
-      <translation type="unfinished">Creation complete to the file: {outFile}.</translation>
+      <translation>Creación completada en el archivo: {outFile}.</translation>
     </message>
     <message>
       <location filename="../GenerateParses.py" line="578"/>
       <source>{wrdCount} words generated.</source>
-      <translation type="unfinished">{wrdCount} words generated.</translation>
+      <translation>{wrdCount} palabras generadas.</translation>
     </message>
   </context>
 </TS>
