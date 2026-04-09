25ecf4c198384d70d4a67de4437897af47d26a16
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:33 +0200

    New translations genstc.ts (German)

--- Dev/Modules/translations/GenStc_de.ts
+++ Dev/Modules/translations/GenStc_de.ts
@@ -6,47 +6,47 @@
     <message>
       <location filename="../GenStc.py" line="73"/>
       <source>Generate Sentences from Model</source>
-      <translation type="unfinished">Generate Sentences from Model</translation>
+      <translation>Sätze aus Modell erzeugen</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="78"/>
       <source>Iterate over certain grammatical categories in a model sentence to produce variations.</source>
-      <translation type="unfinished">Iterate over certain grammatical categories in a model sentence to produce variations.</translation>
+      <translation>Iterieren Sie über bestimmte grammatikalische Kategorien in einem Mustelsatz, um Variationen zu erzeugen.</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="175"/>
       <source>Problem with output file path: {filePath}. Please check configuration.</source>
-      <translation type="unfinished">Problem with output file path: {filePath}. Please check configuration.</translation>
+      <translation>Problem mit Ausgabedateipfad: {filePath}. Bitte überprüfen Sie die Konfiguration.</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="191"/>
       <source>Text not found: {sourceTextName}</source>
-      <translation type="unfinished">Text not found: {sourceTextName}</translation>
+      <translation>Text nicht gefunden: {sourceTextName}</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="648"/>
       <source>Found {stcCount} sentences in the text</source>
-      <translation type="unfinished">Found {stcCount} sentences in the text</translation>
+      <translation>{stcCount} Sätze im Text gefunden</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="277"/>
       <source>valid pos: {valid_pos}</source>
-      <translation type="unfinished">valid pos: {valid_pos}</translation>
+      <translation>gültige POS: {valid_pos}</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="568"/>
       <source>custom field: {customField}</source>
-      <translation type="unfinished">custom field: {customField}</translation>
+      <translation>benutzerdefiniertes Feld: {customField}</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="575"/>
       <source>Translation file check: {translationFile}</source>
-      <translation type="unfinished">Translation file check: {translationFile}</translation>
+      <translation>Übersetzungsdateiprüfung: {translationFile}</translation>
     </message>
     <message>
       <location filename="../GenStc.py" line="734"/>
       <source>Export of {sourceName} complete.</source>
-      <translation type="unfinished">Export of {sourceName} complete.</translation>
+      <translation>Export von {sourceName} abgeschlossen.</translation>
     </message>
   </context>
 </TS>
