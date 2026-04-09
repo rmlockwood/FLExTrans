ee71cb63f94ad8855e522405476831b436df17ac
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:51 +0200

    New translations textoutrules.ts (German)

--- Dev/Modules/translations/TextOutRules_de.ts
+++ Dev/Modules/translations/TextOutRules_de.ts
@@ -6,19 +6,18 @@
     <message>
       <location filename="../TextOutRules.py" line="81"/>
       <source>Text Out Rules</source>
-      <translation type="unfinished">Text Out Rules</translation>
+      <translation>Text-Out-Regeln</translation>
     </message>
     <message>
       <location filename="../TextOutRules.py" line="84"/>
       <source>Define and test a set of post-synthesis search and replace operations.</source>
-      <translation type="unfinished">Define and test a set of post-synthesis search and replace operations.</translation>
+      <translation>Definieren und testen Sie eine Reihe von Such- und Ersetzungsoperationen nach der Synthese.</translation>
     </message>
     <message>
       <location filename="../TextOutRules.py" line="86"/>
       <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 synthesis. Regular expressions can be used if desired.</source>
-      <translation type="unfinished">This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
-synthesis. Regular expressions can be used if desired.</translation>
+      <translation>Dieses Modul wird verwendet, um eine Reihe von Such- und Ersetzungsoperationen zu definieren und zu testen, die verwendet werden, um den Text zu korrigieren, der aus der Synthese stammt. Reguläre Ausdrücke können bei Bedarf verwendet werden.</translation>
     </message>
   </context>
 </TS>
