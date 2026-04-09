736920faa4159630a5252eccdd418affc25b1cad
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:06:52 +0200

    New translations translatetext.ts (German)

--- Dev/Modules/translations/TranslateText_de.ts
+++ Dev/Modules/translations/TranslateText_de.ts
@@ -6,67 +6,67 @@
     <message>
       <location filename="../TranslateText.py" line="67"/>
       <source>Translate Text</source>
-      <translation type="unfinished">Translate Text</translation>
+      <translation>Text übersetzen</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="72"/>
       <source>Translate the current source text.</source>
-      <translation type="unfinished">Translate the current source text.</translation>
+      <translation>Den aktuellen Quelltext übersetzen.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="98"/>
       <source>Exporting source text...</source>
-      <translation type="unfinished">Exporting source text...</translation>
+      <translation>Quelltext wird exportiert...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="105"/>
       <source>Building the bilingual lexicon...</source>
-      <translation type="unfinished">Building the bilingual lexicon...</translation>
+      <translation>Zweisprachiges Lexikon wird erstellt...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="114"/>
       <source>Running the Apertium transfer engine...</source>
-      <translation type="unfinished">Running the Apertium transfer engine...</translation>
+      <translation>Apertium Transfer-Engine wird ausgeführt...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="128"/>
       <source>Cataloging target affixes...</source>
-      <translation type="unfinished">Cataloging target affixes...</translation>
+      <translation>Ziel-Affixe werden katalogisiert...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="137"/>
       <source>Converting target words to synthesizer format...</source>
-      <translation type="unfinished">Converting target words to synthesizer format...</translation>
+      <translation>Zielwörter werden in Synthesizer-Format konvertiert...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="146"/>
       <source>Synthesizing target text...</source>
-      <translation type="unfinished">Synthesizing target text...</translation>
+      <translation>Zieltext wird synthetisiert...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="150"/>
       <source>Using HermitCrab for synthesis.</source>
-      <translation type="unfinished">Using HermitCrab for synthesis.</translation>
+      <translation>HermitCrab wird für die Synthese verwendet.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="154"/>
       <source>Using STAMP for synthesis.</source>
-      <translation type="unfinished">Using STAMP for synthesis.</translation>
+      <translation>STAMP wird für die Synthese verwendet.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="164"/>
       <source>Exporting to Paratext...</source>
-      <translation type="unfinished">Exporting to Paratext...</translation>
+      <translation>Export nach Paratext...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="171"/>
       <source>Inserting text into the target project...</source>
-      <translation type="unfinished">Inserting text into the target project...</translation>
+      <translation>Text wird in das Zielprojekt eingefügt...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="177"/>
       <source>Translation complete.</source>
-      <translation type="unfinished">Translation complete.</translation>
+      <translation>Übersetzung abgeschlossen.</translation>
     </message>
   </context>
 </TS>
