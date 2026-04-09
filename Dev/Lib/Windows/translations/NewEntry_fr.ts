13705050dd459431ad624100a966791a2bfabd31
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:04:00 +0200

    New translations newentry.ts (French)

--- Dev/Lib/Windows/translations/NewEntry_fr.ts
+++ Dev/Lib/Windows/translations/NewEntry_fr.ts
@@ -6,27 +6,27 @@
     <message>
       <location filename="../NewEntry.py" line="82"/>
       <source>Add New Target Entry</source>
-      <translation type="unfinished">Add New Target Entry</translation>
+      <translation>Ajouter une nouvelle entrée cible</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="83"/>
       <source>Lexeme Form</source>
-      <translation type="unfinished">Lexeme Form</translation>
+      <translation>Forme de lexème</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="84"/>
       <source>Morpheme Type</source>
-      <translation type="unfinished">Morpheme Type</translation>
+      <translation>Type de morphème</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="85"/>
       <source>Grammatical Category</source>
-      <translation type="unfinished">Grammatical Category</translation>
+      <translation>Catégorie grammaticale</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="92"/>
       <source>Add an entry to the target lexicon.</source>
-      <translation type="unfinished">Add an entry to the target lexicon.</translation>
+      <translation>Ajouter une entrée au lexique cible.</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="88"/>
@@ -36,17 +36,17 @@
     <message>
       <location filename="../NewEntry.py" line="90"/>
       <source>Cancel</source>
-      <translation type="unfinished">Cancel</translation>
+      <translation>Annuler</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="91"/>
       <source>Gloss</source>
-      <translation type="unfinished">Gloss</translation>
+      <translation>Glose</translation>
     </message>
     <message>
       <location filename="../NewEntry.py" line="93"/>
       <source>Cluster Projects to Apply to</source>
-      <translation type="unfinished">Cluster Projects to Apply to</translation>
+      <translation>Projets de cluster auxquels appliquer</translation>
     </message>
   </context>
 </TS>
