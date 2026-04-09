e9fbaf2ee07b395646fc961eb77935732149fedf
R Lockwood <rlbooks@web.de>
2026-04-09 08:24:32 +0200

    Update CreateApertiumRules.ts

--- Dev/Lib/translations/CreateApertiumRules.ts
+++ Dev/Lib/translations/CreateApertiumRules.ts
@@ -4,6 +4,11 @@
 <context>
     <name>CreateApertiumRules</name>
     <message>
+        <location filename="../CreateApertiumRules.py" line="582"/>
+        <source>List values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../CreateApertiumRules.py" line="446"/>
         <source>Could not find any tags for feature {label} of part-of-speech {category}.</source>
         <translation>Impossible de trouver des balises pour la caractéristique {label} de la catégorie grammaticale {category}.</translation>
