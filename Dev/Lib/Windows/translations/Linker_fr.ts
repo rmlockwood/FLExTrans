67a1ebb4b8fffcdef87b883a7735166035d827e2
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:03:58 +0200

    New translations linker.ts (French)

--- Dev/Lib/Windows/translations/Linker_fr.ts
+++ Dev/Lib/Windows/translations/Linker_fr.ts
@@ -6,49 +6,49 @@
     <message>
       <location filename="../Linker.py" line="245"/>
       <source>FLExTrans Sense Linker Tool</source>
-      <translation type="unfinished">FLExTrans Sense Linker Tool</translation>
+      <translation>Outil de liaison de sens FLExTrans</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="246"/>
       <source>Source Text:</source>
-      <translation type="unfinished">Source Text:</translation>
+      <translation>Texte source :</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="247"/>
       <source>All Target Senses:</source>
-      <translation type="unfinished">All Target Senses:</translation>
+      <translation>Tous les sens cibles :</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="248"/>
       <source>After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</source>
-      <translation type="unfinished">After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</translation>
+      <translation>Après avoir sélectionné le sens souhaité ici, double-cliquez dans la colonne Mot-vedette cible pour la ligne souhaitée pour y créer un lien.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="249"/>
       <source>Type the beginning of an entry or any text (if 'Filter on all fields' is checked),
 then double-click in the Target Head Word column for the desired row to link to it.</source>
-      <translation type="unfinished">Type the beginning of an entry or any text (if 'Filter on all fields' is checked),
-then double-click in the Target Head Word column for the desired row to link to it.</translation>
+      <translation>Tapez le début d'une entrée ou n'importe quel texte (si 'Filtrer sur tous les champs' est coché),
+puis double-cliquez dans la colonne Mot-vedette cible pour la ligne souhaitée pour y créer un lien.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="251"/>
       <source>Search here</source>
-      <translation type="unfinished">Search here</translation>
+      <translation>Rechercher ici</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="252"/>
       <source>Filter on all fields</source>
-      <translation type="unfinished">Filter on all fields</translation>
+      <translation>Filtrer sur tous les champs</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="253"/>
       <source>Add a new entry to the target lexicon.</source>
-      <translation type="unfinished">Add a new entry to the target lexicon.</translation>
+      <translation>Ajouter une nouvelle entrée au lexique cible.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="254"/>
       <source> Add Entry </source>
-      <translation type="unfinished"> Add Entry </translation>
+      <translation> Ajouter une entrée </translation>
     </message>
     <message>
       <location filename="../Linker.py" line="255"/>
@@ -58,49 +58,49 @@ then double-click in the Target Head Word column for the desired row to link to 
     <message>
       <location filename="../Linker.py" line="256"/>
       <source>Cancel</source>
-      <translation type="unfinished">Cancel</translation>
+      <translation>Annuler</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="257"/>
       <source>Show Only Unlinked</source>
-      <translation type="unfinished">Show Only Unlinked</translation>
+      <translation>Afficher uniquement les non liés</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="258"/>
       <source>Hide Proper Nouns</source>
-      <translation type="unfinished">Hide Proper Nouns</translation>
+      <translation>Masquer les noms propres</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="259"/>
       <source>Zoom:</source>
-      <translation type="unfinished">Zoom:</translation>
+      <translation>Zoom :</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="260"/>
       <source>Font</source>
-      <translation type="unfinished">Font</translation>
+      <translation>Police</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="261"/>
       <source>Rebuild Bilingual Lexicon</source>
-      <translation type="unfinished">Rebuild Bilingual Lexicon</translation>
+      <translation>Reconstruire le lexique bilingue</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="262"/>
       <source>Export a list of unlinked senses to a file. If Hide Proper Nouns
 is checked, Proper Nouns will not be exported.</source>
-      <translation type="unfinished">Export a list of unlinked senses to a file. If Hide Proper Nouns
-is checked, Proper Nouns will not be exported.</translation>
+      <translation>Exporter une liste de sens non liés vers un fichier. Si Masquer les noms propres
+est coché, les noms propres ne seront pas exportés.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="264"/>
       <source>Export Unlinked Senses</source>
-      <translation type="unfinished">Export Unlinked Senses</translation>
+      <translation>Exporter les sens non liés</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="265"/>
       <source>Senses to link:</source>
-      <translation type="unfinished">Senses to link:</translation>
+      <translation>Sens à lier :</translation>
     </message>
   </context>
 </TS>
