f8c318a9f2f2054d0fdd44beeca144681a76b170
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-09 22:05:07 +0200

    New translations rulecatsandattribs.ts (Spanish)

--- Dev/Lib/Windows/translations/RuleCatsAndAttribs_es.ts
+++ Dev/Lib/Windows/translations/RuleCatsAndAttribs_es.ts
@@ -6,17 +6,17 @@
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="76"/>
       <source>Set Up Transfer Rule Categories &amp; Attributes</source>
-      <translation type="unfinished">Set Up Transfer Rule Categories &amp; Attributes</translation>
+      <translation>Configurar categorías y atributos de reglas de transferencia</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="77"/>
       <source>Overwrite existing inflection feature attributes</source>
-      <translation type="unfinished">Overwrite existing inflection feature attributes</translation>
+      <translation>Sobrescribir atributos de características de flexión existentes</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="78"/>
       <source>Populate inflection features as attributes</source>
-      <translation type="unfinished">Populate inflection features as attributes</translation>
+      <translation>Rellenar características de flexión como atributos</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="79"/>
@@ -26,32 +26,32 @@
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="80"/>
       <source>Cancel</source>
-      <translation type="unfinished">Cancel</translation>
+      <translation>Cancelar</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="81"/>
       <source>Overwrite existing inflection class attributes</source>
-      <translation type="unfinished">Overwrite existing inflection class attributes</translation>
+      <translation>Sobrescribir atributos de clases de flexión existentes</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="82"/>
       <source>Populate inflection classes as attributes</source>
-      <translation type="unfinished">Populate inflection classes as attributes</translation>
+      <translation>Rellenar clases de flexión como atributos</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="83"/>
       <source>Populate template slots as attributes</source>
-      <translation type="unfinished">Populate template slots as attributes</translation>
+      <translation>Rellenar ranuras de plantilla como atributos</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="84"/>
       <source>Overwrite existing template slot attributes</source>
-      <translation type="unfinished">Overwrite existing template slot attributes</translation>
+      <translation>Sobrescribir atributos de ranuras de plantilla existentes</translation>
     </message>
     <message>
       <location filename="../RuleCatsAndAttribs.py" line="85"/>
       <source>Source categories and the a_gram_cat attribute are populated automatically. Existing source categories are not overwritten.</source>
-      <translation type="unfinished">Source categories and the a_gram_cat attribute are populated automatically. Existing source categories are not overwritten.</translation>
+      <translation>Las categorías de origen y el atributo a_gram_cat se rellenan automáticamente. Las categorías de origen existentes no se sobrescriben.</translation>
     </message>
   </context>
 </TS>
