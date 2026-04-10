97cd3072c4fabd2c37f9deb25370cc49368e6f3b
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:58:45 +0200

    New translations replacementeditorwindow.ts (Spanish)

--- Dev/Lib/Windows/translations/ReplacementEditorWindow_es.ts
+++ Dev/Lib/Windows/translations/ReplacementEditorWindow_es.ts
@@ -1,67 +1,67 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="es-ES" sourcelanguage="en">
-<context>
+  <context>
     <name>ReplacementEditorWindow</name>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="106"/>
-        <source>Replacement Dictionary Editor</source>
-        <translation>Editor de diccionario de reemplazo</translation>
+      <location filename="../ReplacementEditorWindow.py" line="106"/>
+      <source>Replacement Dictionary Editor</source>
+      <translation>Replacement Dictionary Editor</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="108"/>
-        <source>Source Lemma</source>
-        <translation>Lema fuente</translation>
+      <location filename="../ReplacementEditorWindow.py" line="108"/>
+      <source>Source Lemma</source>
+      <translation>Source Lemma</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="118"/>
-        <source>Gramm. Cat.</source>
-        <translation>Cat. gram.</translation>
+      <location filename="../ReplacementEditorWindow.py" line="118"/>
+      <source>Gramm. Cat.</source>
+      <translation>Gramm. Cat.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="120"/>
-        <source>Features/Classes</source>
-        <translation>Características/Clases</translation>
+      <location filename="../ReplacementEditorWindow.py" line="120"/>
+      <source>Features/Classes</source>
+      <translation>Features/Classes</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="122"/>
-        <source>Affixes</source>
-        <translation>Afijos</translation>
+      <location filename="../ReplacementEditorWindow.py" line="122"/>
+      <source>Affixes</source>
+      <translation>Affixes</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="116"/>
-        <source>Target Lemma</source>
-        <translation>Lema objetivo</translation>
+      <location filename="../ReplacementEditorWindow.py" line="116"/>
+      <source>Target Lemma</source>
+      <translation>Target Lemma</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="124"/>
-        <source>Comment</source>
-        <translation>Comentario</translation>
+      <location filename="../ReplacementEditorWindow.py" line="124"/>
+      <source>Comment</source>
+      <translation>Comment</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="125"/>
-        <source>Separate multiple features/classes and/or affixes with a period. For example f.sg for femine singular.</source>
-        <translation>Separe múltiples características/clases y/o afijos con un punto. Por ejemplo, f.sg para femenino singular.</translation>
+      <location filename="../ReplacementEditorWindow.py" line="125"/>
+      <source>Separate multiple features/classes and/or affixes with a period. For example f.sg for femine singular.</source>
+      <translation>Separate multiple features/classes and/or affixes with a period. For example f.sg for femine singular.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="126"/>
-        <source>Add Row</source>
-        <translation>Agregar fila</translation>
+      <location filename="../ReplacementEditorWindow.py" line="126"/>
+      <source>Add Row</source>
+      <translation>Add Row</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="127"/>
-        <source>Delete Selected Rows</source>
-        <translation>Eliminar filas seleccionadas</translation>
+      <location filename="../ReplacementEditorWindow.py" line="127"/>
+      <source>Delete Selected Rows</source>
+      <translation>Delete Selected Rows</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="128"/>
-        <source>Save</source>
-        <translation>Guardar</translation>
+      <location filename="../ReplacementEditorWindow.py" line="128"/>
+      <source>Save</source>
+      <translation>Save</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditorWindow.py" line="129"/>
-        <source>Close</source>
-        <translation>Cerrar</translation>
+      <location filename="../ReplacementEditorWindow.py" line="129"/>
+      <source>Close</source>
+      <translation>Close</translation>
     </message>
-</context>
+  </context>
 </TS>
