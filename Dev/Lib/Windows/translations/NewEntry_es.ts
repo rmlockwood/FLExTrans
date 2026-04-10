33b81b36e7010549390cac1929b2e4c4ca947c15
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:58:35 +0200

    New translations newentry.ts (Spanish)

--- Dev/Lib/Windows/translations/NewEntry_es.ts
+++ Dev/Lib/Windows/translations/NewEntry_es.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="es-ES" sourcelanguage="en">
-<context>
+  <context>
     <name>Dialog</name>
     <message>
-        <location filename="../NewEntry.py" line="80"/>
-        <source>Add New Target Entry</source>
-        <translation>Agregar nueva entrada objetivo</translation>
+      <location filename="../NewEntry.py" line="80"/>
+      <source>Add New Target Entry</source>
+      <translation>Add New Target Entry</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="81"/>
-        <source>Lexeme Form</source>
-        <translation>Forma del lexema</translation>
+      <location filename="../NewEntry.py" line="81"/>
+      <source>Lexeme Form</source>
+      <translation>Lexeme Form</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="82"/>
-        <source>Morpheme Type</source>
-        <translation>Tipo de morfema</translation>
+      <location filename="../NewEntry.py" line="82"/>
+      <source>Morpheme Type</source>
+      <translation>Morpheme Type</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="83"/>
-        <source>Grammatical Category</source>
-        <translation>Categoría gramatical</translation>
+      <location filename="../NewEntry.py" line="83"/>
+      <source>Grammatical Category</source>
+      <translation>Grammatical Category</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="90"/>
-        <source>Add an entry to the target lexicon.</source>
-        <translation>Agregar una entrada al léxico objetivo.</translation>
+      <location filename="../NewEntry.py" line="90"/>
+      <source>Add an entry to the target lexicon.</source>
+      <translation>Add an entry to the target lexicon.</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="86"/>
-        <source>OK</source>
-        <translation type="unfinished">OK</translation>
+      <location filename="../NewEntry.py" line="86"/>
+      <source>OK</source>
+      <translation>OK</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="88"/>
-        <source>Cancel</source>
-        <translation>Cancelar</translation>
+      <location filename="../NewEntry.py" line="88"/>
+      <source>Cancel</source>
+      <translation>Cancel</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="89"/>
-        <source>Gloss</source>
-        <translation>Glosa</translation>
+      <location filename="../NewEntry.py" line="89"/>
+      <source>Gloss</source>
+      <translation>Gloss</translation>
     </message>
     <message>
-        <location filename="../NewEntry.py" line="91"/>
-        <source>Cluster Projects to Apply to</source>
-        <translation>Proyectos de clúster a los que aplicar</translation>
+      <location filename="../NewEntry.py" line="91"/>
+      <source>Cluster Projects to Apply to</source>
+      <translation>Cluster Projects to Apply to</translation>
     </message>
-</context>
+  </context>
 </TS>
