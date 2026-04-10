24c8ed2d4c9d60e9799e2be4449b670da5ba4560
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 14:23:27 +0200

    New translations flextransmenu.ts (Spanish)

--- Dev/TopLevel/translations/FLExTransMenu_es.ts
+++ Dev/TopLevel/translations/FLExTransMenu_es.ts
@@ -1,70 +1,54 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="es-ES" sourcelanguage="en">
-<context>
+  <context>
     <name>FLExTransMenu</name>
     <message>
-        <location filename="../FLExTransMenu.py" line="101"/>
-        <source>Transfer rule file: {xferRulesFile} does not exist.</source>
-        <translation>El archivo de reglas de transferencia: {xferRulesFile} no existe.</translation>
+      <location filename="../FLExTransMenu.py" line="95"/>
+      <source>Transfer rule file: {xferRulesFile} does not exist.</source>
+      <translation>El archivo de reglas de transferencia: {xferRulesFile} no existe.</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="111"/>
-        <source>Not Found Error</source>
-        <translation>Error de no encontrado</translation>
+      <location filename="../FLExTransMenu.py" line="95"/>
+      <source>Not Found Error</source>
+      <translation>Error de no encontrado</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="141"/>
-        <source>{name} version {version}
+      <location filename="../FLExTransMenu.py" line="120"/>
+      <source>{name} version {version}
 
 Build {build}, {build_date}
 
 software.sil.org/flextrans</source>
-        <translation type="unfinished">{name} version {version}
+      <translation>{name} version {version}
 
 Build {build}, {build_date}
 
 software.sil.org/flextrans</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="148"/>
-        <source>About FLExTrans</source>
-        <translation>Acerca de FLExTrans</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>About FLExTrans</source>
+      <translation>Acerca de FLExTrans</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="148"/>
-        <source>Help</source>
-        <translation>Ayuda</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Help</source>
+      <translation>Ayuda</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="148"/>
-        <source>Settings</source>
-        <translation>Configuraciones</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Settings</source>
+      <translation>Configuraciones</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="148"/>
-        <source>Edit Transfer Rules</source>
-        <translation>Editar reglas de transferencia</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>Edit Transfer Rules</source>
+      <translation>Editar reglas de transferencia</translation>
     </message>
     <message>
-        <location filename="../FLExTransMenu.py" line="148"/>
-        <source>About</source>
-        <translation>Acerca de</translation>
+      <location filename="../FLExTransMenu.py" line="127"/>
+      <source>About</source>
+      <translation>Acerca de</translation>
     </message>
-    <message>
-        <location filename="../FLExTransMenu.py" line="111"/>
-        <source>XMLmind XML Editor not found at expected location: {xxe}</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FLExTransMenu.py" line="121"/>
-        <source>Error occurred while trying to open transfer rules file: {e}</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FLExTransMenu.py" line="121"/>
-        <source>Error</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+  </context>
 </TS>
