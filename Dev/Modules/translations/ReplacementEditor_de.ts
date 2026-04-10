adcf38c87a74884616b5a9740309b598cb949617
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:00:16 +0200

    New translations replacementeditor.ts (German)

--- Dev/Modules/translations/ReplacementEditor_de.ts
+++ Dev/Modules/translations/ReplacementEditor_de.ts
@@ -1,72 +1,71 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>ReplacementEditor</name>
     <message>
-        <location filename="../ReplacementEditor.py" line="90"/>
-        <source>Replacement Dictionary Editor</source>
-        <translation>Editor für Ersetzungswörterbuch</translation>
+      <location filename="../ReplacementEditor.py" line="87"/>
+      <source>Replacement Dictionary Editor</source>
+      <translation>Editor für Ersetzungswörterbuch</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="93"/>
-        <source>Edit manual overrides for the bilingual dictionary.</source>
-        <translation>Manuelle Überschreibungen für das zweisprachige Wörterbuch bearbeiten.</translation>
+      <location filename="../ReplacementEditor.py" line="90"/>
+      <source>Edit manual overrides for the bilingual dictionary.</source>
+      <translation>Manuelle Überschreibungen für das zweisprachige Wörterbuch bearbeiten.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="95"/>
-        <source>This module provides an interface for editing the replacement dictionary
+      <location filename="../ReplacementEditor.py" line="92"/>
+      <source>This module provides an interface for editing the replacement dictionary
 which allows you to override the links created by the {linkSenseToolModule}
 in the presence of particular affixes. For example, if you have a noun that
 is generally translated one way, but has a different translation in the
 vocative singular, you can specify that here.</source>
-        <translation>Dieses Modul bietet eine Schnittstelle zum Bearbeiten des Ersetzungswörterbuchs,
+      <translation>Dieses Modul bietet eine Schnittstelle zum Bearbeiten des Ersetzungswörterbuchs,
 mit dem Sie die vom {linkSenseToolModule} erstellten Verknüpfungen
 bei bestimmten Affixen überschreiben können. Zum Beispiel, wenn Sie ein Substantiv haben,
 das normalerweise auf eine Weise übersetzt wird, aber im Vokativ Singular eine andere Übersetzung hat,
 können Sie dies hier angeben.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="559"/>
-        <source>There are unsaved changes.</source>
-        <translation>Es gibt ungespeicherte Änderungen.</translation>
+      <location filename="../ReplacementEditor.py" line="556"/>
+      <source>There are unsaved changes.</source>
+      <translation>Es gibt ungespeicherte Änderungen.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="574"/>
-        <source>The following sets of rows are identical on the source side and only the first one will have any effect:
+      <location filename="../ReplacementEditor.py" line="571"/>
+      <source>The following sets of rows are identical on the source side and only the first one will have any effect:
 </source>
-        <translation>Die folgenden Zeilensätze sind auf der Quellseite identisch, und nur der erste wird eine Wirkung haben:
+      <translation>Die folgenden Zeilensätze sind auf der Quellseite identisch, und nur der erste wird eine Wirkung haben:
 </translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="576"/>
-        <source>The following rows have no affixes and thus are redundant with the links created by Sense Linker Tool: </source>
-        <translation>Die folgenden Zeilen haben keine Affixe und sind daher redundant mit den vom Sense Linker Tool erstellten Verknüpfungen: </translation>
+      <location filename="../ReplacementEditor.py" line="573"/>
+      <source>The following rows have no affixes and thus are redundant with the links created by Sense Linker Tool: </source>
+      <translation>Die folgenden Zeilen haben keine Affixe und sind daher redundant mit den vom Sense Linker Tool erstellten Verknüpfungen: </translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="579"/>
-        <source>Useless Lines:</source>
-        <translation>Nutzlose Zeilen:</translation>
+      <location filename="../ReplacementEditor.py" line="576"/>
+      <source>Useless Lines:</source>
+      <translation>Nutzlose Zeilen:</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="591"/>
-        <source>Replacement dictionary file saved.</source>
-        <translation>Ersetzungswörterbuchdatei gespeichert.</translation>
+      <location filename="../ReplacementEditor.py" line="588"/>
+      <source>Replacement dictionary file saved.</source>
+      <translation>Ersetzungswörterbuchdatei gespeichert.</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="597"/>
-        <source>Unsaved Changes</source>
-        <translation>Ungespeicherte Änderungen</translation>
+      <location filename="../ReplacementEditor.py" line="594"/>
+      <source>Unsaved Changes</source>
+      <translation>Ungespeicherte Änderungen</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="597"/>
-        <source>Save changes before exiting?</source>
-        <translation>Änderungen vor dem Beenden speichern?</translation>
+      <location filename="../ReplacementEditor.py" line="594"/>
+      <source>Save changes before exiting?</source>
+      <translation>Änderungen vor dem Beenden speichern?</translation>
     </message>
     <message>
-        <location filename="../ReplacementEditor.py" line="627"/>
-        <source>A value for {configValue} not found in the configuration file.</source>
-        <translation>Ein Wert für {configValue} wurde in der Konfigurationsdatei nicht gefunden.</translation>
+      <location filename="../ReplacementEditor.py" line="624"/>
+      <source>A value for {configValue} not found in the configuration file.</source>
+      <translation>Ein Wert für {configValue} wurde in der Konfigurationsdatei nicht gefunden.</translation>
     </message>
-</context>
+  </context>
 </TS>
