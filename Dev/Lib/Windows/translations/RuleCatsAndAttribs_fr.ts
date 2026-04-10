00536d27350bdc6ea216f79e31671366f599c897
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:57:48 +0200

    New translations rulecatsandattribs.ts (French)

--- Dev/Lib/Windows/translations/RuleCatsAndAttribs_fr.ts
+++ Dev/Lib/Windows/translations/RuleCatsAndAttribs_fr.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr" sourcelanguage="en">
-<context>
+  <context>
     <name>CatsAndAttribsWindow</name>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="111"/>
-        <source>Set Up Transfer Rule Categories &amp; Attributes</source>
-        <translation>Configurer les catégories et attributs de règles de transfert</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="111"/>
+      <source>Set Up Transfer Rule Categories &amp; Attributes</source>
+      <translation>Set Up Transfer Rule Categories &amp; Attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="113"/>
-        <source>Overwrite existing inflection feature attributes</source>
-        <translation>Écraser les attributs de trait de flexion existants</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="113"/>
+      <source>Overwrite existing inflection feature attributes</source>
+      <translation>Overwrite existing inflection feature attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="112"/>
-        <source>Populate inflection features as attributes</source>
-        <translation>Remplir les traits de flexion en tant qu'attributs</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="112"/>
+      <source>Populate inflection features as attributes</source>
+      <translation>Populate inflection features as attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="119"/>
-        <source>OK</source>
-        <translation type="unfinished">OK</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="119"/>
+      <source>OK</source>
+      <translation>OK</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="120"/>
-        <source>Cancel</source>
-        <translation>Annuler</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="120"/>
+      <source>Cancel</source>
+      <translation>Cancel</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="115"/>
-        <source>Overwrite existing inflection class attributes</source>
-        <translation>Écraser les attributs de classe de flexion existants</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="115"/>
+      <source>Overwrite existing inflection class attributes</source>
+      <translation>Overwrite existing inflection class attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="114"/>
-        <source>Populate inflection classes as attributes</source>
-        <translation>Remplir les classes de flexion en tant qu'attributs</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="114"/>
+      <source>Populate inflection classes as attributes</source>
+      <translation>Populate inflection classes as attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="116"/>
-        <source>Populate template slots as attributes</source>
-        <translation>Remplir les emplacements de modèle en tant qu'attributs</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="116"/>
+      <source>Populate template slots as attributes</source>
+      <translation>Populate template slots as attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="117"/>
-        <source>Overwrite existing template slot attributes</source>
-        <translation>Écraser les attributs d'emplacement de modèle existants</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="117"/>
+      <source>Overwrite existing template slot attributes</source>
+      <translation>Overwrite existing template slot attributes</translation>
     </message>
     <message>
-        <location filename="../RuleCatsAndAttribs.py" line="118"/>
-        <source>Source categories and the a_gram_cat attribute are populated automatically. Existing source categories are not overwritten.</source>
-        <translation>Les catégories sources et l'attribut a_gram_cat sont remplis automatiquement. Les catégories sources existantes ne sont pas écrasées.</translation>
+      <location filename="../RuleCatsAndAttribs.py" line="118"/>
+      <source>Source categories and the a_gram_cat attribute are populated automatically. Existing source categories are not overwritten.</source>
+      <translation>Source categories and the a_gram_cat attribute are populated automatically. Existing source categories are not overwritten.</translation>
     </message>
-</context>
+  </context>
 </TS>
