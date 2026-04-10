0cae7cb2ab92798e5d2845fb88454603ff1924b4
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:55:20 +0200

    New translations newentrydlg.ts (German)

--- Dev/Lib/translations/NewEntryDlg_de.ts
+++ Dev/Lib/translations/NewEntryDlg_de.ts
@@ -4,52 +4,52 @@
   <context>
     <name>NewEntryDlg</name>
     <message>
-      <location filename="../NewEntryDlg.py" line="98"/>
+      <location filename="../NewEntryDlg.py" line="92"/>
       <source>Target FLEx project name</source>
       <translation>Target FLEx project name</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="99"/>
+      <location filename="../NewEntryDlg.py" line="93"/>
       <source>Lexeme Form</source>
       <translation>Lexeme Form</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="298"/>
+      <location filename="../NewEntryDlg.py" line="292"/>
       <source>Error Check</source>
       <translation>Error Check</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="223"/>
+      <location filename="../NewEntryDlg.py" line="217"/>
       <source>You must enter a Gloss.</source>
       <translation>You must enter a Gloss.</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="244"/>
+      <location filename="../NewEntryDlg.py" line="238"/>
       <source>Cluster Project Selection Error</source>
       <translation>Cluster Project Selection Error</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="244"/>
+      <location filename="../NewEntryDlg.py" line="238"/>
       <source>You must at least select the default target project, {proj}, among your cluster projects.</source>
       <translation>You must at least select the default target project, {proj}, among your cluster projects.</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="257"/>
+      <location filename="../NewEntryDlg.py" line="251"/>
       <source>You must enter all the Lexeme Forms.</source>
       <translation>You must enter all the Lexeme Forms.</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="277"/>
+      <location filename="../NewEntryDlg.py" line="271"/>
       <source>Not Found Error</source>
       <translation>Not Found Error</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="277"/>
+      <location filename="../NewEntryDlg.py" line="271"/>
       <source>Failed to open the project: {proj}</source>
       <translation>Failed to open the project: {proj}</translation>
     </message>
     <message>
-      <location filename="../NewEntryDlg.py" line="298"/>
+      <location filename="../NewEntryDlg.py" line="292"/>
       <source>You must enter a Lexeme Form.</source>
       <translation>You must enter a Lexeme Form.</translation>
     </message>
