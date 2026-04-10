ab6a6e6bb8adb87ff101a4d9c352ba76b99cea10
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:55:23 +0200

    New translations testbedvalidator.ts (German)

--- Dev/Lib/translations/TestbedValidator_de.ts
+++ Dev/Lib/translations/TestbedValidator_de.ts
@@ -4,22 +4,22 @@
   <context>
     <name>TestbedValidator</name>
     <message>
-      <location filename="../TestbedValidator.py" line="111"/>
+      <location filename="../TestbedValidator.py" line="108"/>
       <source>Word Sense: {wordSense} not found.</source>
       <translation>Word Sense: {wordSense} not found.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="117"/>
+      <location filename="../TestbedValidator.py" line="114"/>
       <source>Grammatical Category: {gramCat} not found.</source>
       <translation>Grammatical Category: {gramCat} not found.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="123"/>
+      <location filename="../TestbedValidator.py" line="120"/>
       <source>Tag: {tag} not found.</source>
       <translation>Tag: {tag} not found.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="235"/>
+      <location filename="../TestbedValidator.py" line="232"/>
       <source>Configuration File Problem. Morphnames not found.</source>
       <translation>Configuration File Problem. Morphnames not found.</translation>
     </message>
