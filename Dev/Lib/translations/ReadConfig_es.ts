d51d3fb3586b0c8191e2c0fa7c02d1cfbf5cb27a
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:54:52 +0200

    New translations readconfig.ts (Spanish)

--- Dev/Lib/translations/ReadConfig_es.ts
+++ Dev/Lib/translations/ReadConfig_es.ts
@@ -4,34 +4,34 @@
   <context>
     <name>ReadConfig</name>
     <message>
-      <location filename="../ReadConfig.py" line="196"/>
+      <location filename="../ReadConfig.py" line="193"/>
       <source>Error reading the file: &quot;{path}/{file}&quot;. Check that it exists.</source>
-      <translation>Error reading the file: &quot;{path}/{file}&quot;. Check that it exists.</translation>
+      <translation>Error al leer el archivo: &quot;{path}/{file}&quot;. Verifique que exista.</translation>
     </message>
     <message>
-      <location filename="../ReadConfig.py" line="233"/>
+      <location filename="../ReadConfig.py" line="230"/>
       <source>Setting: &quot;{setting}&quot; not found in the configuration file.</source>
-      <translation>Setting: &quot;{setting}&quot; not found in the configuration file.</translation>
+      <translation>Configuración: &quot;{setting}&quot; no encontrada en el archivo de configuración.</translation>
     </message>
     <message>
-      <location filename="../ReadConfig.py" line="272"/>
+      <location filename="../ReadConfig.py" line="269"/>
       <source>Problem reading the file: &quot;{file}&quot;. A line without &quot;=&quot; was found.</source>
-      <translation>Problem reading the file: &quot;{file}&quot;. A line without &quot;=&quot; was found.</translation>
+      <translation>Problema al leer el archivo: &quot;{file}&quot;. Se encontró una línea sin &quot;=&quot;.</translation>
     </message>
     <message>
-      <location filename="../ReadConfig.py" line="282"/>
+      <location filename="../ReadConfig.py" line="279"/>
       <source>Problem reading the file: &quot;{file}&quot;. A line with more than one &quot;=&quot; was found.</source>
-      <translation>Problem reading the file: &quot;{file}&quot;. A line with more than one &quot;=&quot; was found.</translation>
+      <translation>Problema al leer el archivo: &quot;{file}&quot;. Se encontró una línea sin &quot;=&quot;.</translation>
     </message>
     <message>
-      <location filename="../ReadConfig.py" line="306"/>
+      <location filename="../ReadConfig.py" line="303"/>
       <source>Error in the file: &quot;{file}&quot;. A value for &quot;{key}&quot; was not found.</source>
-      <translation>Error in the file: &quot;{file}&quot;. A value for &quot;{key}&quot; was not found.</translation>
+      <translation>Error en el archivo: &quot;{file}&quot;. No se encontró un valor para &quot;{key}&quot;.</translation>
     </message>
     <message>
-      <location filename="../ReadConfig.py" line="332"/>
+      <location filename="../ReadConfig.py" line="329"/>
       <source>Error in the file: &quot;{file}&quot;. The value for &quot;{key}&quot; is supposed to be a comma separated list. For a single value, end it with a comma.</source>
-      <translation>Error in the file: &quot;{file}&quot;. The value for &quot;{key}&quot; is supposed to be a comma separated list. For a single value, end it with a comma.</translation>
+      <translation>Error en el archivo: &quot;{file}&quot;. El valor para &quot;{key}&quot; debe ser una lista separada por comas. Para un solo valor, termínelo con una coma.</translation>
     </message>
   </context>
 </TS>
