1b04fa51996962aa03a911307f4e2e91fb2dd557
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 14:24:25 +0200

    New translations cleanfiles.ts (French)

--- Dev/Modules/translations/CleanFiles_fr.ts
+++ Dev/Modules/translations/CleanFiles_fr.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr" sourcelanguage="en">
-<context>
+  <context>
     <name>CleanFiles</name>
     <message>
-        <location filename="../CleanFiles.py" line="88"/>
-        <source>Clean Files</source>
-        <translation>Nettoyer les fichiers</translation>
+      <location filename="../CleanFiles.py" line="88"/>
+      <source>Clean Files</source>
+      <translation>Nettoyer les fichiers</translation>
     </message>
     <message>
-        <location filename="../CleanFiles.py" line="91"/>
-        <source>Remove generated files to force each FLExTrans module to regenerate everything</source>
-        <translation>Supprimer les fichiers générés pour forcer chaque module FLExTrans à tout regénérer</translation>
+      <location filename="../CleanFiles.py" line="91"/>
+      <source>Remove generated files to force each FLExTrans module to regenerate everything</source>
+      <translation>Supprimer les fichiers générés pour forcer chaque module FLExTrans à tout regénérer</translation>
     </message>
     <message>
-        <location filename="../CleanFiles.py" line="93"/>
-        <source>Remove generated files to force each FLExTrans module to regenerate everything. This typically removes most files in the Build and Output folders.</source>
-        <translation>Supprimer les fichiers générés pour forcer chaque module FLExTrans à tout regénérer. Cela supprime généralement la plupart des fichiers dans les dossiers Build et Output.</translation>
+      <location filename="../CleanFiles.py" line="93"/>
+      <source>Remove generated files to force each FLExTrans module to regenerate everything. This typically removes most files in the Build and Output folders.</source>
+      <translation>Supprimer les fichiers générés pour forcer chaque module FLExTrans à tout regénérer. Cela supprime généralement la plupart des fichiers dans les dossiers Build et Output.</translation>
     </message>
-</context>
+  </context>
 </TS>
