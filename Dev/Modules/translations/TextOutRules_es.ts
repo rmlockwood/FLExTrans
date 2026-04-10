e1da9533e0e7de2af6c9723f39118e4a301bd77b
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:59:36 +0200

    New translations textoutrules.ts (Spanish)

--- Dev/Modules/translations/TextOutRules_es.ts
+++ Dev/Modules/translations/TextOutRules_es.ts
@@ -1,23 +1,24 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="es-ES" sourcelanguage="en">
-<context>
+  <context>
     <name>TextOutRules</name>
     <message>
-        <location filename="../TextOutRules.py" line="84"/>
-        <source>Text Out Rules</source>
-        <translation>Reglas de salida de texto</translation>
+      <location filename="../TextOutRules.py" line="81"/>
+      <source>Text Out Rules</source>
+      <translation>Text Out Rules</translation>
     </message>
     <message>
-        <location filename="../TextOutRules.py" line="87"/>
-        <source>Define and test a set of post-synthesis search and replace operations.</source>
-        <translation>Definir y probar un conjunto de operaciones de búsqueda y reemplazo posteriores a la síntesis.</translation>
+      <location filename="../TextOutRules.py" line="84"/>
+      <source>Define and test a set of post-synthesis search and replace operations.</source>
+      <translation>Define and test a set of post-synthesis search and replace operations.</translation>
     </message>
     <message>
-        <location filename="../TextOutRules.py" line="89"/>
-        <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+      <location filename="../TextOutRules.py" line="86"/>
+      <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 synthesis. Regular expressions can be used if desired.</source>
-        <translation>Este módulo se utiliza para definir y probar un conjunto de operaciones de búsqueda y reemplazo que se utilizarán para corregir el texto que proviene de la síntesis. Se pueden usar expresiones regulares si se desea.</translation>
+      <translation>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+synthesis. Regular expressions can be used if desired.</translation>
     </message>
-</context>
+  </context>
 </TS>
