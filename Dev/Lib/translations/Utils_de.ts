5ac4b2cbbf2d7c6bcb27c4e8e1ddbf0b1a0023a1
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:55:27 +0200

    New translations utils.ts (German)

--- Dev/Lib/translations/Utils_de.ts
+++ Dev/Lib/translations/Utils_de.ts
@@ -4,87 +4,87 @@
   <context>
     <name>Utils</name>
     <message>
-      <location filename="../Utils.py" line="342"/>
+      <location filename="../Utils.py" line="331"/>
       <source>space</source>
       <translation>space</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="342"/>
+      <location filename="../Utils.py" line="331"/>
       <source>converted to an underscore</source>
       <translation>converted to an underscore</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="343"/>
+      <location filename="../Utils.py" line="332"/>
       <source>period</source>
       <translation>period</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="343"/>
+      <location filename="../Utils.py" line="332"/>
       <source>removed</source>
       <translation>removed</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="409"/>
+      <location filename="../Utils.py" line="398"/>
       <source> - Copy</source>
       <translation> - Copy</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="438"/>
+      <location filename="../Utils.py" line="427"/>
       <source>Could not open the file {myFile} when converting to NFD.</source>
       <translation>Could not open the file {myFile} when converting to NFD.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="617"/>
+      <location filename="../Utils.py" line="606"/>
       <source>There was an error opening project: {DBname}. Perhaps the project is open and the sharing option under FieldWorks Project Properties has not been clicked.</source>
       <translation>There was an error opening project: {DBname}. Perhaps the project is open and the sharing option under FieldWorks Project Properties has not been clicked.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="634"/>
+      <location filename="../Utils.py" line="623"/>
       <source>The target project does not exist. Please check the configuration file.</source>
       <translation>The target project does not exist. Please check the configuration file.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="641"/>
+      <location filename="../Utils.py" line="630"/>
       <source>There was an error opening target project: {targetProj}. Perhaps the project is open and the sharing option under FieldWorks Project Properties has not been clicked.</source>
       <translation>There was an error opening target project: {targetProj}. Perhaps the project is open and the sharing option under FieldWorks Project Properties has not been clicked.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="668"/>
+      <location filename="../Utils.py" line="657"/>
       <source>source</source>
       <translation>source</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="673"/>
+      <location filename="../Utils.py" line="662"/>
       <source>target</source>
       <translation>target</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="738"/>
+      <location filename="../Utils.py" line="727"/>
       <source>category</source>
       <translation>category</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="757"/>
-      <source>The abbreviation/name: &apos;{posAbbrStr}&apos; for {myType}: &apos;{posFullNameStr}&apos; can&apos;t have a {charName} in it. Could not complete, &apos;+                                            &apos;please correct this {myType} in the {dbType} project.</source>
-      <translation>The abbreviation/name: '{posAbbrStr}' for {myType}: '{posFullNameStr}' can't have a {charName} in it. Could not complete, '+                                            'please correct this {myType} in the {dbType} project.</translation>
+      <location filename="../Utils.py" line="746"/>
+      <source>The abbreviation/name: '{posAbbrStr}' for {myType}: '{posFullNameStr}' can't have a {charName} in it. Could not complete, '+                                            'please correct this {myType} in the {dbType} project.</source>
+      <translation type="unfinished">The abbreviation/name: '{posAbbrStr}' for {myType}: '{posFullNameStr}' can't have a {charName} in it. Could not complete, '+                                            'please correct this {myType} in the {dbType} project.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="773"/>
-      <source>The abbreviation/name: &apos;{oldAbbrStr}&apos; for {myType}: &apos;{posFullNameStr}&apos; in the {dbType} project can&apos;t have a {charName} in it. The {charName} &apos;+                                              &apos;has been {message}, forming {posAbbrStr}. Keep this in mind when referring to this {myType} in transfer rules.</source>
-      <translation>The abbreviation/name: '{oldAbbrStr}' for {myType}: '{posFullNameStr}' in the {dbType} project can't have a {charName} in it. The {charName} '+                                              'has been {message}, forming {posAbbrStr}. Keep this in mind when referring to this {myType} in transfer rules.</translation>
+      <location filename="../Utils.py" line="762"/>
+      <source>The abbreviation/name: '{oldAbbrStr}' for {myType}: '{posFullNameStr}' in the {dbType} project can't have a {charName} in it. The {charName} '+                                              'has been {message}, forming {posAbbrStr}. Keep this in mind when referring to this {myType} in transfer rules.</source>
+      <translation type="unfinished">The abbreviation/name: '{oldAbbrStr}' for {myType}: '{posFullNameStr}' in the {dbType} project can't have a {charName} in it. The {charName} '+                                              'has been {message}, forming {posAbbrStr}. Keep this in mind when referring to this {myType} in transfer rules.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="781"/>
+      <location filename="../Utils.py" line="770"/>
       <source>Suppressing further warnings of this type.</source>
       <translation>Suppressing further warnings of this type.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="931"/>
+      <location filename="../Utils.py" line="920"/>
       <source>Invalid url link or url not found in the target project while processing source headword: {headWord}.</source>
       <translation>Invalid url link or url not found in the target project while processing source headword: {headWord}.</translation>
     </message>
     <message>
-      <location filename="../Utils.py" line="1045"/>
+      <location filename="../Utils.py" line="1034"/>
       <source>linked to entry: {headWordStr}, sense: {glossStr} in the {projectName} project.</source>
       <translation>linked to entry: {headWordStr}, sense: {glossStr} in the {projectName} project.</translation>
     </message>
