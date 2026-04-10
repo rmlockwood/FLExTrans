126a14160ac811de1aa2cc7c616abb4a84ed4286
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 12:00:25 +0200

    New translations textinrules.ts (German)

--- Dev/Modules/translations/TextInRules_de.ts
+++ Dev/Modules/translations/TextInRules_de.ts
@@ -1,23 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>TextInRules</name>
     <message>
-        <location filename="../TextInRules.py" line="70"/>
-        <source>Text In Rules</source>
-        <translation>Text-In-Regeln</translation>
+      <location filename="../TextInRules.py" line="67"/>
+      <source>Text In Rules</source>
+      <translation>Text-In-Regeln</translation>
     </message>
     <message>
-        <location filename="../TextInRules.py" line="73"/>
-        <source>Define and test a set of Paratext-import search and replace operations.</source>
-        <translation>Definieren und testen Sie eine Reihe von Such- und Ersetzungsoperationen für den Paratext-Import.</translation>
+      <location filename="../TextInRules.py" line="70"/>
+      <source>Define and test a set of Paratext-import search and replace operations.</source>
+      <translation>Definieren und testen Sie eine Reihe von Such- und Ersetzungsoperationen für den Paratext-Import.</translation>
     </message>
     <message>
-        <location filename="../TextInRules.py" line="75"/>
-        <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+      <location filename="../TextInRules.py" line="72"/>
+      <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 Paratext. Regular expressions and Wildebeest normalization can be used if desired.</source>
-        <translation>Dieses Modul wird verwendet, um eine Reihe von Such- und Ersetzungsoperationen zu definieren und zu testen, die verwendet werden, um den Text zu korrigieren, der aus Paratext stammt. Reguläre Ausdrücke und Wildebeest-Normalisierung können bei Bedarf verwendet werden.</translation>
+      <translation>Dieses Modul wird verwendet, um eine Reihe von Such- und Ersetzungsoperationen zu definieren und zu testen, die verwendet werden, um den Text zu korrigieren, der aus Paratext stammt. Reguläre Ausdrücke und Wildebeest-Normalisierung können bei Bedarf verwendet werden.</translation>
     </message>
-</context>
+  </context>
 </TS>
