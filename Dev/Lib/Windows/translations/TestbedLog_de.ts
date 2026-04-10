4e17eea3de4b73c63f80a0ed0706e3466cf089f0
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 14:24:15 +0200

    New translations testbedlog.ts (German)

--- Dev/Lib/Windows/translations/TestbedLog_de.ts
+++ Dev/Lib/Windows/translations/TestbedLog_de.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>TestbedLogWindow</name>
     <message>
-        <location filename="../TestbedLog.py" line="74"/>
-        <source>Testbed Log</source>
-        <translation>Testbed-Protokoll</translation>
+      <location filename="../TestbedLog.py" line="74"/>
+      <source>Testbed Log</source>
+      <translation type="unfinished">Testbed Log</translation>
     </message>
     <message>
-        <location filename="../TestbedLog.py" line="75"/>
-        <source>OK</source>
-        <translation>OK</translation>
+      <location filename="../TestbedLog.py" line="75"/>
+      <source>OK</source>
+      <translation type="unfinished">OK</translation>
     </message>
     <message>
-        <location filename="../TestbedLog.py" line="76"/>
-        <source>Edit Testbed</source>
-        <translation>Testbed bearbeiten</translation>
+      <location filename="../TestbedLog.py" line="76"/>
+      <source>Edit Testbed</source>
+      <translation type="unfinished">Edit Testbed</translation>
     </message>
     <message>
-        <location filename="../TestbedLog.py" line="77"/>
-        <source>Font Size:</source>
-        <translation>Schriftgröße:</translation>
+      <location filename="../TestbedLog.py" line="77"/>
+      <source>Font Size:</source>
+      <translation type="unfinished">Font Size:</translation>
     </message>
-</context>
+  </context>
 </TS>
