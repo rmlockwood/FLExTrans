9cc615bee4717ba0b2f00f946f1b5810d44521d3
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:57:41 +0200

    New translations clusteradhoc.ts (French)

--- Dev/Lib/Windows/translations/ClusterAdHoc_fr.ts
+++ Dev/Lib/Windows/translations/ClusterAdHoc_fr.ts
@@ -1,62 +1,62 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr" sourcelanguage="en">
-<context>
+  <context>
     <name>AdHocMainWindow</name>
     <message>
-        <location filename="../ClusterAdHoc.py" line="120"/>
-        <source>Add Cluster Ad Hoc Rules</source>
-        <translation>Ajouter des règles ad hoc de cluster</translation>
+      <location filename="../ClusterAdHoc.py" line="120"/>
+      <source>Add Cluster Ad Hoc Rules</source>
+      <translation>Add Cluster Ad Hoc Rules</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="121"/>
-        <source>Source Project</source>
-        <translation>Projet source</translation>
+      <location filename="../ClusterAdHoc.py" line="121"/>
+      <source>Source Project</source>
+      <translation>Source Project</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="122"/>
-        <source>Add</source>
-        <translation>Ajouter</translation>
+      <location filename="../ClusterAdHoc.py" line="122"/>
+      <source>Add</source>
+      <translation>Add</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="123"/>
-        <source>Close</source>
-        <translation>Fermer</translation>
+      <location filename="../ClusterAdHoc.py" line="123"/>
+      <source>Close</source>
+      <translation>Close</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="124"/>
-        <source>Ad Hoc Group</source>
-        <translation>Groupe ad hoc</translation>
+      <location filename="../ClusterAdHoc.py" line="124"/>
+      <source>Ad Hoc Group</source>
+      <translation>Ad Hoc Group</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="125"/>
-        <source>Ad Hoc Type</source>
-        <translation>Type ad hoc</translation>
+      <location filename="../ClusterAdHoc.py" line="125"/>
+      <source>Ad Hoc Type</source>
+      <translation>Ad Hoc Type</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="126"/>
-        <source>Key Morpheme/Allomorph</source>
-        <translation>Morphème/allomorphe clé</translation>
+      <location filename="../ClusterAdHoc.py" line="126"/>
+      <source>Key Morpheme/Allomorph</source>
+      <translation>Key Morpheme/Allomorph</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="127"/>
-        <source>Other Morphemes/Allomorphs</source>
-        <translation>Autres morphèmes/allomorphes</translation>
+      <location filename="../ClusterAdHoc.py" line="127"/>
+      <source>Other Morphemes/Allomorphs</source>
+      <translation>Other Morphemes/Allomorphs</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="128"/>
-        <source>Cannot Occur</source>
-        <translation>Ne peut pas se produire</translation>
+      <location filename="../ClusterAdHoc.py" line="128"/>
+      <source>Cannot Occur</source>
+      <translation>Cannot Occur</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="129"/>
-        <source>Cluster Projects to Apply to</source>
-        <translation>Projets de cluster auxquels appliquer</translation>
+      <location filename="../ClusterAdHoc.py" line="129"/>
+      <source>Cluster Projects to Apply to</source>
+      <translation>Cluster Projects to Apply to</translation>
     </message>
     <message>
-        <location filename="../ClusterAdHoc.py" line="130"/>
-        <source>New Group</source>
-        <translation>Nouveau groupe</translation>
+      <location filename="../ClusterAdHoc.py" line="130"/>
+      <source>New Group</source>
+      <translation>New Group</translation>
     </message>
-</context>
+  </context>
 </TS>
