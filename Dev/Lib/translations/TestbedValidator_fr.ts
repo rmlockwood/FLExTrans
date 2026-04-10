64a3e6ec6cf762c123d4c9f248660f22cc835674
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 11:57:37 +0200

    New translations testbedvalidator.ts (French)

--- Dev/Lib/translations/TestbedValidator_fr.ts
+++ Dev/Lib/translations/TestbedValidator_fr.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr" sourcelanguage="en">
-<context>
+  <context>
     <name>TestbedValidator</name>
     <message>
-        <location filename="../TestbedValidator.py" line="111"/>
-        <source>Word Sense: {wordSense} not found.</source>
-        <translation>Word Sense: {wordSense} not found.</translation>
+      <location filename="../TestbedValidator.py" line="111"/>
+      <source>Word Sense: {wordSense} not found.</source>
+      <translation>Word Sense: {wordSense} not found.</translation>
     </message>
     <message>
-        <location filename="../TestbedValidator.py" line="117"/>
-        <source>Grammatical Category: {gramCat} not found.</source>
-        <translation>Grammatical Category: {gramCat} not found.</translation>
+      <location filename="../TestbedValidator.py" line="117"/>
+      <source>Grammatical Category: {gramCat} not found.</source>
+      <translation>Grammatical Category: {gramCat} not found.</translation>
     </message>
     <message>
-        <location filename="../TestbedValidator.py" line="123"/>
-        <source>Tag: {tag} not found.</source>
-        <translation>Tag: {tag} not found.</translation>
+      <location filename="../TestbedValidator.py" line="123"/>
+      <source>Tag: {tag} not found.</source>
+      <translation>Tag: {tag} not found.</translation>
     </message>
     <message>
-        <location filename="../TestbedValidator.py" line="235"/>
-        <source>Configuration File Problem. Morphnames not found.</source>
-        <translation>Configuration File Problem. Morphnames not found.</translation>
+      <location filename="../TestbedValidator.py" line="235"/>
+      <source>Configuration File Problem. Morphnames not found.</source>
+      <translation>Configuration File Problem. Morphnames not found.</translation>
     </message>
-</context>
+  </context>
 </TS>
