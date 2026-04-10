5424f8b75c19b2f70e02b803cc821d716427e492
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-10 14:24:12 +0200

    New translations srctgtviewer.ts (German)

--- Dev/Lib/Windows/translations/SrcTgtViewer_de.ts
+++ Dev/Lib/Windows/translations/SrcTgtViewer_de.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>SrcTgtWindow</name>
     <message>
-        <location filename="../SrcTgtViewer.py" line="145"/>
-        <source>Source/Target Viewer</source>
-        <translation>Quell-/Zielviewer</translation>
+      <location filename="../SrcTgtViewer.py" line="145"/>
+      <source>Source/Target Viewer</source>
+      <translation type="unfinished">Source/Target Viewer</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="146"/>
-        <source>Font</source>
-        <translation>Schriftart</translation>
+      <location filename="../SrcTgtViewer.py" line="146"/>
+      <source>Font</source>
+      <translation type="unfinished">Font</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="147"/>
-        <source>Source</source>
-        <translation>Quelle</translation>
+      <location filename="../SrcTgtViewer.py" line="147"/>
+      <source>Source</source>
+      <translation type="unfinished">Source</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="148"/>
-        <source>Target</source>
-        <translation>Ziel</translation>
+      <location filename="../SrcTgtViewer.py" line="148"/>
+      <source>Target</source>
+      <translation type="unfinished">Target</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="149"/>
-        <source>Right to Left</source>
-        <translation>Rechts nach Links</translation>
+      <location filename="../SrcTgtViewer.py" line="149"/>
+      <source>Right to Left</source>
+      <translation type="unfinished">Right to Left</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="150"/>
-        <source>Zoom:</source>
-        <translation>Zoom:</translation>
+      <location filename="../SrcTgtViewer.py" line="150"/>
+      <source>Zoom:</source>
+      <translation type="unfinished">Zoom:</translation>
     </message>
     <message>
-        <location filename="../SrcTgtViewer.py" line="151"/>
-        <source>OK</source>
-        <translation>OK</translation>
+      <location filename="../SrcTgtViewer.py" line="151"/>
+      <source>OK</source>
+      <translation type="unfinished">OK</translation>
     </message>
-</context>
+  </context>
 </TS>
