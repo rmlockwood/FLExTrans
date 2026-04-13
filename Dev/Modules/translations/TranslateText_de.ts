4321bed172dc01747c9d301664e668d233804b88
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:44:30 +0200

    New translations translatetext.ts (German)

--- Dev/Modules/translations/TranslateText_de.ts
+++ Dev/Modules/translations/TranslateText_de.ts
@@ -6,67 +6,67 @@
     <message>
       <location filename="../TranslateText.py" line="70"/>
       <source>Translate Text</source>
-      <translation type="unfinished">Translate Text</translation>
+      <translation>Translate Text</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="75"/>
       <source>Translate the current source text.</source>
-      <translation type="unfinished">Translate the current source text.</translation>
+      <translation>Translate the current source text.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="101"/>
       <source>Exporting source text...</source>
-      <translation type="unfinished">Exporting source text...</translation>
+      <translation>Exporting source text...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="108"/>
       <source>Building the bilingual lexicon...</source>
-      <translation type="unfinished">Building the bilingual lexicon...</translation>
+      <translation>Building the bilingual lexicon...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="117"/>
       <source>Running the Apertium transfer engine...</source>
-      <translation type="unfinished">Running the Apertium transfer engine...</translation>
+      <translation>Running the Apertium transfer engine...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="131"/>
       <source>Cataloging target affixes...</source>
-      <translation type="unfinished">Cataloging target affixes...</translation>
+      <translation>Cataloging target affixes...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="140"/>
       <source>Converting target words to synthesizer format...</source>
-      <translation type="unfinished">Converting target words to synthesizer format...</translation>
+      <translation>Converting target words to synthesizer format...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="149"/>
       <source>Synthesizing target text...</source>
-      <translation type="unfinished">Synthesizing target text...</translation>
+      <translation>Synthesizing target text...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="153"/>
       <source>Using HermitCrab for synthesis.</source>
-      <translation type="unfinished">Using HermitCrab for synthesis.</translation>
+      <translation>Using HermitCrab for synthesis.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="157"/>
       <source>Using STAMP for synthesis.</source>
-      <translation type="unfinished">Using STAMP for synthesis.</translation>
+      <translation>Using STAMP for synthesis.</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="167"/>
       <source>Exporting to Paratext...</source>
-      <translation type="unfinished">Exporting to Paratext...</translation>
+      <translation>Exporting to Paratext...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="174"/>
       <source>Inserting text into the target project...</source>
-      <translation type="unfinished">Inserting text into the target project...</translation>
+      <translation>Inserting text into the target project...</translation>
     </message>
     <message>
       <location filename="../TranslateText.py" line="180"/>
       <source>Translation complete.</source>
-      <translation type="unfinished">Translation complete.</translation>
+      <translation>Translation complete.</translation>
     </message>
   </context>
 </TS>
