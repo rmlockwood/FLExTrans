968b66de04d6cf8895d2ba2cce1175af5ae6a69d
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:44:31 +0200

    New translations viewsrctgt.ts (Spanish)

--- Dev/Modules/translations/ViewSrcTgt_es.ts
+++ Dev/Modules/translations/ViewSrcTgt_es.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../ViewSrcTgt.py" line="98"/>
       <source>View Source/Target Apertium Text Tool</source>
-      <translation type="unfinished">View Source/Target Apertium Text Tool</translation>
+      <translation>View Source/Target Apertium Text Tool</translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="101"/>
       <source>View an easy-to-read source or target text file.</source>
-      <translation type="unfinished">View an easy-to-read source or target text file.</translation>
+      <translation>View an easy-to-read source or target text file.</translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="103"/>
@@ -20,7 +20,7 @@ file. The lexical units are color coded as follows: black-lemma, blue-grammatica
 category, green-affix or feature or class, yellow-non-sentence punctuation, 
 dark pink-unknown lemma, pink-unknown category, red-lemma not found. Important! You
 must run the modules up to and including {runApert} before running this module.</source>
-      <translation type="unfinished">This module will display a more readable view of the Apertium source or target 
+      <translation>This module will display a more readable view of the Apertium source or target 
 file. The lexical units are color coded as follows: black-lemma, blue-grammatical 
 category, green-affix or feature or class, yellow-non-sentence punctuation, 
 dark pink-unknown lemma, pink-unknown category, red-lemma not found. Important! You
@@ -29,32 +29,32 @@ must run the modules up to and including {runApert} before running this module.<
     <message>
       <location filename="../ViewSrcTgt.py" line="235"/>
       <source>File Error</source>
-      <translation type="unfinished">File Error</translation>
+      <translation>File Error</translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="232"/>
       <source>There was a problem opening the Source Apertium Text file: {fileName}. </source>
-      <translation type="unfinished">There was a problem opening the Source Apertium Text file: {fileName}. </translation>
+      <translation>There was a problem opening the Source Apertium Text file: {fileName}. </translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="232"/>
       <source>Make sure you have run the {moduleName} module first.</source>
-      <translation type="unfinished">Make sure you have run the {moduleName} module first.</translation>
+      <translation>Make sure you have run the {moduleName} module first.</translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="235"/>
       <source>There was a problem opening a Target Apertium Text file: {fileName}. </source>
-      <translation type="unfinished">There was a problem opening a Target Apertium Text file: {fileName}. </translation>
+      <translation>There was a problem opening a Target Apertium Text file: {fileName}. </translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="235"/>
       <source>Make sure you have run the modules up through {moduleName} first.</source>
-      <translation type="unfinished">Make sure you have run the modules up through {moduleName} first.</translation>
+      <translation>Make sure you have run the modules up through {moduleName} first.</translation>
     </message>
     <message>
       <location filename="../ViewSrcTgt.py" line="327"/>
       <source>Open in Browser</source>
-      <translation type="unfinished">Open in Browser</translation>
+      <translation>Open in Browser</translation>
     </message>
   </context>
 </TS>
