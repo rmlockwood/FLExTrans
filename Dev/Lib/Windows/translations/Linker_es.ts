9aef90dffc3e68112035eb02a198695c429d38f1
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:42:24 +0200

    New translations linker.ts (Spanish)

--- Dev/Lib/Windows/translations/Linker_es.ts
+++ Dev/Lib/Windows/translations/Linker_es.ts
@@ -6,100 +6,100 @@
     <message>
       <location filename="../Linker.py" line="243"/>
       <source>FLExTrans Sense Linker Tool</source>
-      <translation type="unfinished">FLExTrans Sense Linker Tool</translation>
+      <translation>FLExTrans Sense Linker Tool</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="244"/>
       <source>Source Text:</source>
-      <translation type="unfinished">Source Text:</translation>
+      <translation>Source Text:</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="245"/>
       <source>All Target Senses:</source>
-      <translation type="unfinished">All Target Senses:</translation>
+      <translation>All Target Senses:</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="246"/>
       <source>After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</source>
-      <translation type="unfinished">After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</translation>
+      <translation>After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="249"/>
       <source>Search here</source>
-      <translation type="unfinished">Search here</translation>
+      <translation>Search here</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="250"/>
       <source>Filter on all fields</source>
-      <translation type="unfinished">Filter on all fields</translation>
+      <translation>Filter on all fields</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="251"/>
       <source>Add a new entry to the target lexicon.</source>
-      <translation type="unfinished">Add a new entry to the target lexicon.</translation>
+      <translation>Add a new entry to the target lexicon.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="252"/>
       <source> Add Entry </source>
-      <translation type="unfinished"> Add Entry </translation>
+      <translation> Add Entry </translation>
     </message>
     <message>
       <location filename="../Linker.py" line="253"/>
       <source>OK</source>
-      <translation type="unfinished">OK</translation>
+      <translation>OK</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="254"/>
       <source>Cancel</source>
-      <translation type="unfinished">Cancel</translation>
+      <translation>Cancel</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="255"/>
       <source>Show Only Unlinked</source>
-      <translation type="unfinished">Show Only Unlinked</translation>
+      <translation>Show Only Unlinked</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="256"/>
       <source>Hide Proper Nouns</source>
-      <translation type="unfinished">Hide Proper Nouns</translation>
+      <translation>Hide Proper Nouns</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="257"/>
       <source>Zoom:</source>
-      <translation type="unfinished">Zoom:</translation>
+      <translation>Zoom:</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="258"/>
       <source>Font</source>
-      <translation type="unfinished">Font</translation>
+      <translation>Font</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="259"/>
       <source>Rebuild Bilingual Lexicon</source>
-      <translation type="unfinished">Rebuild Bilingual Lexicon</translation>
+      <translation>Rebuild Bilingual Lexicon</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="262"/>
       <source>Export Unlinked Senses</source>
-      <translation type="unfinished">Export Unlinked Senses</translation>
+      <translation>Export Unlinked Senses</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="263"/>
       <source>Senses to link:</source>
-      <translation type="unfinished">Senses to link:</translation>
+      <translation>Senses to link:</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="247"/>
       <source>Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
 then double-click in the Target Head Word column for the desired row to link to it.</source>
-      <translation type="unfinished">Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
+      <translation>Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
 then double-click in the Target Head Word column for the desired row to link to it.</translation>
     </message>
     <message>
       <location filename="../Linker.py" line="260"/>
       <source>Export a list of unlinked senses to a file. If Hide Proper Nouns 
 is checked, Proper Nouns will not be exported.</source>
-      <translation type="unfinished">Export a list of unlinked senses to a file. If Hide Proper Nouns 
+      <translation>Export a list of unlinked senses to a file. If Hide Proper Nouns 
 is checked, Proper Nouns will not be exported.</translation>
     </message>
   </context>
