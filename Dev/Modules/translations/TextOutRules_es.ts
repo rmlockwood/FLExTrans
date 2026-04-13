3a090035a4c886558b857829c5ce8949f98aa868
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:44:26 +0200

    New translations textoutrules.ts (Spanish)

--- Dev/Modules/translations/TextOutRules_es.ts
+++ Dev/Modules/translations/TextOutRules_es.ts
@@ -6,18 +6,18 @@
     <message>
       <location filename="../TextOutRules.py" line="84"/>
       <source>Text Out Rules</source>
-      <translation type="unfinished">Text Out Rules</translation>
+      <translation>Text Out Rules</translation>
     </message>
     <message>
       <location filename="../TextOutRules.py" line="87"/>
       <source>Define and test a set of post-synthesis search and replace operations.</source>
-      <translation type="unfinished">Define and test a set of post-synthesis search and replace operations.</translation>
+      <translation>Define and test a set of post-synthesis search and replace operations.</translation>
     </message>
     <message>
       <location filename="../TextOutRules.py" line="89"/>
       <source>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 synthesis. Regular expressions can be used if desired.</source>
-      <translation type="unfinished">This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
+      <translation>This module is used to define and test a set of search and replace operations to be used to fix up the text that comes out of 
 synthesis. Regular expressions can be used if desired.</translation>
     </message>
   </context>
