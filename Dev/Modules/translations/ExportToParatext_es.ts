9abb105abf0cde218040068a323dce7ea841b8a4
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:43:20 +0200

    New translations exporttoparatext.ts (Spanish)

--- Dev/Modules/translations/ExportToParatext_es.ts
+++ Dev/Modules/translations/ExportToParatext_es.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../ExportToParatext.py" line="119"/>
       <source>Export FLExTrans Draft to Paratext</source>
-      <translation type="unfinished">Export FLExTrans Draft to Paratext</translation>
+      <translation>Export FLExTrans Draft to Paratext</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="122"/>
       <source>Export the draft that has been translated with FLExTrans to Paratext.</source>
-      <translation type="unfinished">Export the draft that has been translated with FLExTrans to Paratext.</translation>
+      <translation>Export the draft that has been translated with FLExTrans to Paratext.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="124"/>
@@ -19,7 +19,7 @@
 After chapters have been synthesized with the {synthText} module, the draft resides in the file specified
 by the setting &apos;Target Output Synthesis File&apos; (typically called &apos;target_text-syn.txt&apos;). This module
 takes the draft in this file and copies the chapters into Paratext to the project specified.</source>
-      <translation type="unfinished">
+      <translation>
 After chapters have been synthesized with the {synthText} module, the draft resides in the file specified
 by the setting &apos;Target Output Synthesis File&apos; (typically called &apos;target_text-syn.txt&apos;). This module
 takes the draft in this file and copies the chapters into Paratext to the project specified.</translation>
@@ -27,27 +27,27 @@ takes the draft in this file and copies the chapters into Paratext to the projec
     <message>
       <location filename="../ExportToParatext.py" line="147"/>
       <source>Export Chapters to Paratext</source>
-      <translation type="unfinished">Export Chapters to Paratext</translation>
+      <translation>Export Chapters to Paratext</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="176"/>
       <source>The text name &quot;{sourceText}&quot; is invalid it should be of the form GEN 01 or Genesis 23-38</source>
-      <translation type="unfinished">The text name &quot;{sourceText}&quot; is invalid it should be of the form GEN 01 or Genesis 23-38</translation>
+      <translation>The text name &quot;{sourceText}&quot; is invalid it should be of the form GEN 01 or Genesis 23-38</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="199"/>
       <source>The book name or abbreviation {book} is invalid. It should match a Paratext book.</source>
-      <translation type="unfinished">The book name or abbreviation {book} is invalid. It should match a Paratext book.</translation>
+      <translation>The book name or abbreviation {book} is invalid. It should match a Paratext book.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="278"/>
       <source>Could not find the synthesis file. Have you run the Synthesize Text module? Missing file: {synFile}.</source>
-      <translation type="unfinished">Could not find the synthesis file. Have you run the Synthesize Text module? Missing file: {synFile}.</translation>
+      <translation>Could not find the synthesis file. Have you run the Synthesize Text module? Missing file: {synFile}.</translation>
     </message>
     <message>
       <location filename="../ExportToParatext.py" line="294"/>
       <source>Export cancelled.</source>
-      <translation type="unfinished">Export cancelled.</translation>
+      <translation>Export cancelled.</translation>
     </message>
   </context>
 </TS>
