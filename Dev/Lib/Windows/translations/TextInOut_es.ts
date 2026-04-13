470484f8cef4864644f984574102f30c8023aa50
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:42:48 +0200

    New translations textinout.ts (Spanish)

--- Dev/Lib/Windows/translations/TextInOut_es.ts
+++ Dev/Lib/Windows/translations/TextInOut_es.ts
@@ -6,181 +6,181 @@
     <message>
       <location filename="../TextInOut.py" line="426"/>
       <source>Text Out Rules</source>
-      <translation type="unfinished">Text Out Rules</translation>
+      <translation>Text Out Rules</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="427"/>
       <source>Search for:</source>
-      <translation type="unfinished">Search for:</translation>
+      <translation>Search for:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="428"/>
       <source>Replace with:</source>
-      <translation type="unfinished">Replace with:</translation>
+      <translation>Replace with:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="429"/>
       <source>Comment:</source>
-      <translation type="unfinished">Comment:</translation>
+      <translation>Comment:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="430"/>
       <source>Treat the search and replace text as regular expressions.</source>
-      <translation type="unfinished">Treat the search and replace text as regular expressions.</translation>
+      <translation>Treat the search and replace text as regular expressions.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="431"/>
       <source>Regular Expression</source>
-      <translation type="unfinished">Regular Expression</translation>
+      <translation>Regular Expression</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="432"/>
       <source>Inactive</source>
-      <translation type="unfinished">Inactive</translation>
+      <translation>Inactive</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="433"/>
       <source>Add search and replace text as a new rule.</source>
-      <translation type="unfinished">Add search and replace text as a new rule.</translation>
+      <translation>Add search and replace text as a new rule.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="434"/>
       <source>Add</source>
-      <translation type="unfinished">Add</translation>
+      <translation>Add</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="435"/>
       <source>Update the rule selected below with the above search and replace text.</source>
-      <translation type="unfinished">Update the rule selected below with the above search and replace text.</translation>
+      <translation>Update the rule selected below with the above search and replace text.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="436"/>
       <source>Update</source>
-      <translation type="unfinished">Update</translation>
+      <translation>Update</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="437"/>
       <source>Delete the below selected rule.</source>
-      <translation type="unfinished">Delete the below selected rule.</translation>
+      <translation>Delete the below selected rule.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="438"/>
       <source>Delete</source>
-      <translation type="unfinished">Delete</translation>
+      <translation>Delete</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="439"/>
       <source>Current project automatically included</source>
-      <translation type="unfinished">Current project automatically included</translation>
+      <translation>Current project automatically included</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="440"/>
       <source>Cluster Projects to Apply to</source>
-      <translation type="unfinished">Cluster Projects to Apply to</translation>
+      <translation>Cluster Projects to Apply to</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="441"/>
       <source>Rules</source>
-      <translation type="unfinished">Rules</translation>
+      <translation>Rules</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="442"/>
       <source>Move the selected rule up.</source>
-      <translation type="unfinished">Move the selected rule up.</translation>
+      <translation>Move the selected rule up.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="443"/>
       <source>Move the selected rule down.</source>
-      <translation type="unfinished">Move the selected rule down.</translation>
+      <translation>Move the selected rule down.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="444"/>
       <source>This tool will run before other rules.</source>
-      <translation type="unfinished">This tool will run before other rules.</translation>
+      <translation>This tool will run before other rules.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="445"/>
       <source>Steps:</source>
-      <translation type="unfinished">Steps:</translation>
+      <translation>Steps:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="446"/>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>Default</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="447"/>
       <source>All</source>
-      <translation type="unfinished">All</translation>
+      <translation>All</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="448"/>
       <source>None</source>
-      <translation type="unfinished">None</translation>
+      <translation>None</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="449"/>
       <source>Lang:</source>
-      <translation type="unfinished">Lang:</translation>
+      <translation>Lang:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="450"/>
       <source>3-letter ISO 639-3 code</source>
-      <translation type="unfinished">3-letter ISO 639-3 code</translation>
+      <translation>3-letter ISO 639-3 code</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="451"/>
       <source>code</source>
-      <translation type="unfinished">code</translation>
+      <translation>code</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="452"/>
       <source>Add:</source>
-      <translation type="unfinished">Add:</translation>
+      <translation>Add:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="453"/>
       <source>Steps to add to the base number of steps selected above.
 Separate steps with spaces or commas.</source>
-      <translation type="unfinished">Steps to add to the base number of steps selected above.
+      <translation>Steps to add to the base number of steps selected above.
 Separate steps with spaces or commas.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="455"/>
       <source>Skip:</source>
-      <translation type="unfinished">Skip:</translation>
+      <translation>Skip:</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="456"/>
       <source>Steps to skip from the base number of steps selected above.
 Separate steps with spaces or commas.</source>
-      <translation type="unfinished">Steps to skip from the base number of steps selected above.
+      <translation>Steps to skip from the base number of steps selected above.
 Separate steps with spaces or commas.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="458"/>
       <source>Test input</source>
-      <translation type="unfinished">Test input</translation>
+      <translation>Test input</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="459"/>
       <source>Test the checked rules above with the input text.</source>
-      <translation type="unfinished">Test the checked rules above with the input text.</translation>
+      <translation>Test the checked rules above with the input text.</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="460"/>
       <source>Test</source>
-      <translation type="unfinished">Test</translation>
+      <translation>Test</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="461"/>
       <source>Test output</source>
-      <translation type="unfinished">Test output</translation>
+      <translation>Test output</translation>
     </message>
     <message>
       <location filename="../TextInOut.py" line="462"/>
       <source>Close</source>
-      <translation type="unfinished">Close</translation>
+      <translation>Close</translation>
     </message>
   </context>
 </TS>
