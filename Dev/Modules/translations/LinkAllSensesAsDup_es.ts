aa39d39dbbe68fe99fc10a720081be584ab291f1
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:43:48 +0200

    New translations linkallsensesasdup.ts (Spanish)

--- Dev/Modules/translations/LinkAllSensesAsDup_es.ts
+++ Dev/Modules/translations/LinkAllSensesAsDup_es.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="80"/>
       <source>Link All Senses As Duplicate</source>
-      <translation type="unfinished">Link All Senses As Duplicate</translation>
+      <translation>Link All Senses As Duplicate</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="83"/>
       <source>Link all senses to the same ID in the target.</source>
-      <translation type="unfinished">Link all senses to the same ID in the target.</translation>
+      <translation>Link all senses to the same ID in the target.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="85"/>
@@ -19,7 +19,7 @@
 overwrite all senses in the source project!
 This assumes the target project was copied from the source and all the senses have the same
 unique identifier (guid).</source>
-      <translation type="unfinished">This module will link all senses to the same ID in the target. CAUTION: This will 
+      <translation>This module will link all senses to the same ID in the target. CAUTION: This will 
 overwrite all senses in the source project!
 This assumes the target project was copied from the source and all the senses have the same
 unique identifier (guid).</translation>
@@ -27,32 +27,32 @@ unique identifier (guid).</translation>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="106"/>
       <source>You need to run this module in &quot;modify mode.&quot;</source>
-      <translation type="unfinished">You need to run this module in &quot;modify mode.&quot;</translation>
+      <translation>You need to run this module in &quot;modify mode.&quot;</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="126"/>
       <source>No Source Text Name has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Text Name has been set. Please go to Settings and fix this.</translation>
+      <translation>No Source Text Name has been set. Please go to Settings and fix this.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="131"/>
       <source>No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</translation>
+      <translation>No Source Custom Field for Entry Link has been set. Please go to Settings and fix this.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="139"/>
       <source>No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</source>
-      <translation type="unfinished">No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
+      <translation>No Source Morpheme Types Counted As Roots have been selected. Please go to Settings and fix this.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="177"/>
       <source>Skipped this guid that was not found: {guidSubStr}.</source>
-      <translation type="unfinished">Skipped this guid that was not found: {guidSubStr}.</translation>
+      <translation>Skipped this guid that was not found: {guidSubStr}.</translation>
     </message>
     <message>
       <location filename="../LinkAllSensesAsDup.py" line="183"/>
       <source>{entryIndex} entries processed.</source>
-      <translation type="unfinished">{entryIndex} entries processed.</translation>
+      <translation>{entryIndex} entries processed.</translation>
     </message>
   </context>
 </TS>
