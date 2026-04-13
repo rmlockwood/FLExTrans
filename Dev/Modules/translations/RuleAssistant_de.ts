7349119b201f1b41003a0cfb3c16bbedcbb31075
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:44:07 +0200

    New translations ruleassistant.ts (German)

--- Dev/Modules/translations/RuleAssistant_de.ts
+++ Dev/Modules/translations/RuleAssistant_de.ts
@@ -6,54 +6,54 @@
     <message>
       <location filename="../RuleAssistant.py" line="105"/>
       <source>This module runs a tool which let&apos;s you create transfer rules.</source>
-      <translation type="unfinished">This module runs a tool which let&apos;s you create transfer rules.</translation>
+      <translation>This module runs a tool which let&apos;s you create transfer rules.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="106"/>
       <source>Rule Assistant</source>
-      <translation type="unfinished">Rule Assistant</translation>
+      <translation>Rule Assistant</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="109"/>
       <source>Runs a tool for creating transfer rules.</source>
-      <translation type="unfinished">Runs a tool for creating transfer rules.</translation>
+      <translation>Runs a tool for creating transfer rules.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="350"/>
       <source>Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</source>
-      <translation type="unfinished">Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</translation>
+      <translation>Bilingual dictionary not found. Build the bilingual dictionary to see test data in the {ruleAssistant}.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="362"/>
       <source>The text named &apos;%s&apos; was not found.</source>
-      <translation type="unfinished">The text named &apos;%s&apos; was not found.</translation>
+      <translation>The text named &apos;%s&apos; was not found.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="382"/>
       <source>Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</source>
-      <translation type="unfinished">Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</translation>
+      <translation>Compiled bilingual dictionary not found. There was an error compiling the bilingual dictionary.</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="398"/>
       <source>&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </source>
-      <translation type="unfinished">&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </translation>
+      <translation>&lt;p&gt;&lt;b&gt;Source Text:&lt;/b&gt; </translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="437"/>
       <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
 </source>
-      <translation type="unfinished">&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
+      <translation>&lt;html&gt;&lt;body&gt;&lt;p&gt;No test data available.&lt;/body&gt;&lt;/html&gt;
 </translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="472"/>
       <source>An error happened when running the {ruleAssistant} tool: {error}</source>
-      <translation type="unfinished">An error happened when running the {ruleAssistant} tool: {error}</translation>
+      <translation>An error happened when running the {ruleAssistant} tool: {error}</translation>
     </message>
     <message>
       <location filename="../RuleAssistant.py" line="530"/>
       <source>No rules created.</source>
-      <translation type="unfinished">No rules created.</translation>
+      <translation>No rules created.</translation>
     </message>
   </context>
 </TS>
