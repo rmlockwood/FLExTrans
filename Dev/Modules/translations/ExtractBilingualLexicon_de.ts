660dce25b5d72df2a2eadb694beff096b49ad9d6
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:43:25 +0200

    New translations extractbilinguallexicon.ts (German)

--- Dev/Modules/translations/ExtractBilingualLexicon_de.ts
+++ Dev/Modules/translations/ExtractBilingualLexicon_de.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="141"/>
       <source>Build Bilingual Lexicon</source>
-      <translation type="unfinished">Build Bilingual Lexicon</translation>
+      <translation>Build Bilingual Lexicon</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="144"/>
       <source>Builds an Apertium-style bilingual lexicon.</source>
-      <translation type="unfinished">Builds an Apertium-style bilingual lexicon.</translation>
+      <translation>Builds an Apertium-style bilingual lexicon.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="146"/>
@@ -25,7 +25,7 @@ This is typically called bilingual.dix and is usually in the Output folder.
 
 You can make custom changes to the bilingual lexicon by using the {replEditorModule}. See the help
 document for more details.</source>
-      <translation type="unfinished">This module will build a bilingual lexicon for two projects. The
+      <translation>This module will build a bilingual lexicon for two projects. The
 project that FlexTools is set to is your source project. Set the Target Project
 in Settings to the name of your target project.
 This module builds the bilingual lexicon based on the links from source senses to target senses
@@ -39,117 +39,117 @@ document for more details.</translation>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="273"/>
       <source>Custom field for linking doesn&apos;t exist. Please read the instructions.</source>
-      <translation type="unfinished">Custom field for linking doesn&apos;t exist. Please read the instructions.</translation>
+      <translation>Custom field for linking doesn&apos;t exist. Please read the instructions.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="276"/>
       <source>No Source Morphnames to count as root found. Review your Settings.</source>
-      <translation type="unfinished">No Source Morphnames to count as root found. Review your Settings.</translation>
+      <translation>No Source Morphnames to count as root found. Review your Settings.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="279"/>
       <source>No Sentence Punctuation found. Review your Settings.</source>
-      <translation type="unfinished">No Sentence Punctuation found. Review your Settings.</translation>
+      <translation>No Sentence Punctuation found. Review your Settings.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="289"/>
       <source>Ill-formed property: &quot;CategoryAbbrevSubstitutionList&quot;. Expected pairs of categories.</source>
-      <translation type="unfinished">Ill-formed property: &quot;CategoryAbbrevSubstitutionList&quot;. Expected pairs of categories.</translation>
+      <translation>Ill-formed property: &quot;CategoryAbbrevSubstitutionList&quot;. Expected pairs of categories.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="299"/>
       <source>Custom field: {linkField} doesn&apos;t exist. Please read the instructions.</source>
-      <translation type="unfinished">Custom field: {linkField} doesn&apos;t exist. Please read the instructions.</translation>
+      <translation>Custom field: {linkField} doesn&apos;t exist. Please read the instructions.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="321"/>
       <source>A value for {key} not found in the configuration file.</source>
-      <translation type="unfinished">A value for {key} not found in the configuration file.</translation>
+      <translation>A value for {key} not found in the configuration file.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="333"/>
       <source>The bilingual dictionary is up to date.</source>
-      <translation type="unfinished">The bilingual dictionary is up to date.</translation>
+      <translation>The bilingual dictionary is up to date.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="352"/>
       <source>Error retrieving categories.</source>
-      <translation type="unfinished">Error retrieving categories.</translation>
+      <translation>Error retrieving categories.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="388"/>
       <source>Found a headword with preceding or trailing spaces while processing source headword: {rawHeadWord}. The spaces were removed, but please correct this in the lexicon.</source>
-      <translation type="unfinished">Found a headword with preceding or trailing spaces while processing source headword: {rawHeadWord}. The spaces were removed, but please correct this in the lexicon.</translation>
+      <translation>Found a headword with preceding or trailing spaces while processing source headword: {rawHeadWord}. The spaces were removed, but please correct this in the lexicon.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="391"/>
       <source>Found a headword with one of the following invalid characters: {chars} in {rawHeadWord}. Please correct this in the lexicon before continuing.</source>
-      <translation type="unfinished">Found a headword with one of the following invalid characters: {chars} in {rawHeadWord}. Please correct this in the lexicon before continuing.</translation>
+      <translation>Found a headword with one of the following invalid characters: {chars} in {rawHeadWord}. Please correct this in the lexicon before continuing.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="418"/>
       <source>Encountered a sense that has unknown POS while processing source headword: {rawHeadWord}</source>
-      <translation type="unfinished">Encountered a sense that has unknown POS while processing source headword: {rawHeadWord}</translation>
+      <translation>Encountered a sense that has unknown POS while processing source headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="425"/>
       <source>Encountered a headword that only differs in case from another headword with the same POS ({sourcePOSabbrev}). Skipping this sense. Source headword: {rawHeadWord}</source>
-      <translation type="unfinished">Encountered a headword that only differs in case from another headword with the same POS ({sourcePOSabbrev}). Skipping this sense. Source headword: {rawHeadWord}</translation>
+      <translation>Encountered a headword that only differs in case from another headword with the same POS ({sourcePOSabbrev}). Skipping this sense. Source headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="476"/>
       <source>Skipping sense because the target POS is undefined for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</source>
-      <translation type="unfinished">Skipping sense because the target POS is undefined for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</translation>
+      <translation>Skipping sense because the target POS is undefined for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="478"/>
       <source>Skipping sense because it is of this class: {className} for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</source>
-      <translation type="unfinished">Skipping sense because it is of this class: {className} for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</translation>
+      <translation>Skipping sense because it is of this class: {className} for target headword: {targetHeadWord} while processing source headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="486"/>
       <source>Skipping sense that is of class: {className} for headword: {rawHeadWord}</source>
-      <translation type="unfinished">Skipping sense that is of class: {className} for headword: {rawHeadWord}</translation>
+      <translation>Skipping sense that is of class: {className} for headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="488"/>
       <source>Skipping sense, no analysis object for headword: {rawHeadWord}</source>
-      <translation type="unfinished">Skipping sense, no analysis object for headword: {rawHeadWord}</translation>
+      <translation>Skipping sense, no analysis object for headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="511"/>
       <source>No lexeme form. Skipping. Headword: {rawHeadWord}</source>
-      <translation type="unfinished">No lexeme form. Skipping. Headword: {rawHeadWord}</translation>
+      <translation>No lexeme form. Skipping. Headword: {rawHeadWord}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="520"/>
       <source>No Morph Type. Skipping. {rawHeadWord} Best Vern: {vernString}</source>
-      <translation type="unfinished">No Morph Type. Skipping. {rawHeadWord} Best Vern: {vernString}</translation>
+      <translation>No Morph Type. Skipping. {rawHeadWord} Best Vern: {vernString}</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="546"/>
       <source>There is a problem with the Bilingual Dictionary Replacement File: {replFile}. Please check the configuration file setting.</source>
-      <translation type="unfinished">There is a problem with the Bilingual Dictionary Replacement File: {replFile}. Please check the configuration file setting.</translation>
+      <translation>There is a problem with the Bilingual Dictionary Replacement File: {replFile}. Please check the configuration file setting.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="573"/>
       <source>There was a problem creating the Bilingual Dictionary Output File: {fullPathBilingFile}. Please check the configuration file setting.</source>
-      <translation type="unfinished">There was a problem creating the Bilingual Dictionary Output File: {fullPathBilingFile}. Please check the configuration file setting.</translation>
+      <translation>There was a problem creating the Bilingual Dictionary Output File: {fullPathBilingFile}. Please check the configuration file setting.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="577"/>
       <source>Creation complete to the file: {filePath}.</source>
-      <translation type="unfinished">Creation complete to the file: {filePath}.</translation>
+      <translation>Creation complete to the file: {filePath}.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="578"/>
       <source>{recordsDumpedCount} records created.</source>
-      <translation type="unfinished">{recordsDumpedCount} records created.</translation>
+      <translation>{recordsDumpedCount} records created.</translation>
     </message>
     <message>
       <location filename="../ExtractBilingualLexicon.py" line="247"/>
       <source>Encountered a sense that has an invalid feature while processing source headword: {rawHeadWord}</source>
-      <translation type="unfinished">Encountered a sense that has an invalid feature while processing source headword: {rawHeadWord}</translation>
+      <translation>Encountered a sense that has an invalid feature while processing source headword: {rawHeadWord}</translation>
     </message>
   </context>
 </TS>
