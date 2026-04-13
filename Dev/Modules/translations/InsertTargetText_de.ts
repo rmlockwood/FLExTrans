ef3a4d35d54ca4f182d622e861b18575808cb95c
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-13 15:43:46 +0200

    New translations inserttargettext.ts (German)

--- Dev/Modules/translations/InsertTargetText_de.ts
+++ Dev/Modules/translations/InsertTargetText_de.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../InsertTargetText.py" line="110"/>
       <source>Insert Target Text</source>
-      <translation type="unfinished">Insert Target Text</translation>
+      <translation>Insert Target Text</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="113"/>
       <source>Insert a translated text into the target FLEx project.</source>
-      <translation type="unfinished">Insert a translated text into the target FLEx project.</translation>
+      <translation>Insert a translated text into the target FLEx project.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="115"/>
@@ -20,7 +20,7 @@ the results of the {doSynthModule} module
 and insert the text into the target FLEx project. The Source Text Name setting
 will be used for the text name in the target project. An existing text of the 
 same name will not be overwritten. A copy will be created.</source>
-      <translation type="unfinished">The target project specified in the settings will be used. This module will take
+      <translation>The target project specified in the settings will be used. This module will take
 the results of the {doSynthModule} module
 and insert the text into the target FLEx project. The Source Text Name setting
 will be used for the text name in the target project. An existing text of the 
@@ -29,27 +29,27 @@ same name will not be overwritten. A copy will be created.</translation>
     <message>
       <location filename="../InsertTargetText.py" line="140"/>
       <source>Failed to open the target project.</source>
-      <translation type="unfinished">Failed to open the target project.</translation>
+      <translation>Failed to open the target project.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="143"/>
       <source>Using: {targetProj} as the target project.</source>
-      <translation type="unfinished">Using: {targetProj} as the target project.</translation>
+      <translation>Using: {targetProj} as the target project.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="161"/>
       <source>The Synthesize Text module must be run before this one. Could not open the synthesis file: &quot;</source>
-      <translation type="unfinished">The Synthesize Text module must be run before this one. Could not open the synthesis file: &quot;</translation>
+      <translation>The Synthesize Text module must be run before this one. Could not open the synthesis file: &quot;</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="196"/>
       <source>Text: &quot;{sourceTextName}&quot; created in the {targetProj} project.</source>
-      <translation type="unfinished">Text: &quot;{sourceTextName}&quot; created in the {targetProj} project.</translation>
+      <translation>Text: &quot;{sourceTextName}&quot; created in the {targetProj} project.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="205"/>
       <source>You need to run this module in &quot;modify mode.&quot;</source>
-      <translation type="unfinished">You need to run this module in &quot;modify mode.&quot;</translation>
+      <translation>You need to run this module in &quot;modify mode.&quot;</translation>
     </message>
   </context>
 </TS>
