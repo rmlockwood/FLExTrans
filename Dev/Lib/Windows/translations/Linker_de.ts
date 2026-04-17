8861dbe100a3b9664a141f8aaf92b45269eec930
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-17 10:38:14 +0200

    New translations linker.ts (German)

--- Dev/Lib/Windows/translations/Linker_de.ts
+++ Dev/Lib/Windows/translations/Linker_de.ts
@@ -1,104 +1,106 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>SenseLinkerWindow</name>
     <message>
-        <location filename="../Linker.py" line="243"/>
-        <source>FLExTrans Sense Linker Tool</source>
-        <translation>FLExTrans Sense-Linker-Werkzeug</translation>
+      <location filename="../Linker.py" line="243"/>
+      <source>FLExTrans Sense Linker Tool</source>
+      <translation>FLExTrans Sense-Linker-Werkzeug</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="244"/>
-        <source>Source Text:</source>
-        <translation>Quelltext:</translation>
+      <location filename="../Linker.py" line="244"/>
+      <source>Source Text:</source>
+      <translation>Quelltext:</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="245"/>
-        <source>All Target Senses:</source>
-        <translation>Alle Zielbedeutungen:</translation>
+      <location filename="../Linker.py" line="245"/>
+      <source>All Target Senses:</source>
+      <translation>Alle Zielbedeutungen:</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="246"/>
-        <source>After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</source>
-        <translation>Wählen Sie hier die gewünschte Bedeutung aus und doppelklicken Sie in der Spalte &quot;Ziel-Stichwort&quot; auf die gewünschte Zeile, um sie zu verknüpfen.</translation>
+      <location filename="../Linker.py" line="246"/>
+      <source>After selecting the desired sense here, double-click in the Target Head Word column for the desired row to link to it.</source>
+      <translation>Wählen Sie hier die gewünschte Bedeutung aus und doppelklicken Sie in der Spalte &quot;Ziel-Stichwort&quot; auf die gewünschte Zeile, um sie zu verknüpfen.</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="249"/>
-        <source>Search here</source>
-        <translation>Hier suchen</translation>
+      <location filename="../Linker.py" line="249"/>
+      <source>Search here</source>
+      <translation>Hier suchen</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="250"/>
-        <source>Filter on all fields</source>
-        <translation>Nach allen Feldern filtern</translation>
+      <location filename="../Linker.py" line="250"/>
+      <source>Filter on all fields</source>
+      <translation>Nach allen Feldern filtern</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="251"/>
-        <source>Add a new entry to the target lexicon.</source>
-        <translation>Einen neuen Eintrag zum Ziellexikon hinzufügen.</translation>
+      <location filename="../Linker.py" line="251"/>
+      <source>Add a new entry to the target lexicon.</source>
+      <translation>Einen neuen Eintrag zum Ziellexikon hinzufügen.</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="252"/>
-        <source> Add Entry </source>
-        <translation> Eintrag hinzufügen </translation>
+      <location filename="../Linker.py" line="252"/>
+      <source> Add Entry </source>
+      <translation> Eintrag hinzufügen </translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="253"/>
-        <source>OK</source>
-        <translation>OK</translation>
+      <location filename="../Linker.py" line="253"/>
+      <source>OK</source>
+      <translation>OK</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="254"/>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
+      <location filename="../Linker.py" line="254"/>
+      <source>Cancel</source>
+      <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="255"/>
-        <source>Show Only Unlinked</source>
-        <translation>Nur nicht verknüpfte anzeigen</translation>
+      <location filename="../Linker.py" line="255"/>
+      <source>Show Only Unlinked</source>
+      <translation>Nur nicht verknüpfte anzeigen</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="256"/>
-        <source>Hide Proper Nouns</source>
-        <translation>Eigennamen ausblenden</translation>
+      <location filename="../Linker.py" line="256"/>
+      <source>Hide Proper Nouns</source>
+      <translation>Eigennamen ausblenden</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="257"/>
-        <source>Zoom:</source>
-        <translation>Zoom:</translation>
+      <location filename="../Linker.py" line="257"/>
+      <source>Zoom:</source>
+      <translation>Zoom:</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="258"/>
-        <source>Font</source>
-        <translation>Schriftart</translation>
+      <location filename="../Linker.py" line="258"/>
+      <source>Font</source>
+      <translation>Schriftart</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="259"/>
-        <source>Rebuild Bilingual Lexicon</source>
-        <translation>Zweisprachiges Lexikon neu erstellen</translation>
+      <location filename="../Linker.py" line="259"/>
+      <source>Rebuild Bilingual Lexicon</source>
+      <translation>Zweisprachiges Lexikon neu erstellen</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="262"/>
-        <source>Export Unlinked Senses</source>
-        <translation>Nicht verknüpfte Bedeutungen exportieren</translation>
+      <location filename="../Linker.py" line="262"/>
+      <source>Export Unlinked Senses</source>
+      <translation>Nicht verknüpfte Bedeutungen exportieren</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="263"/>
-        <source>Senses to link:</source>
-        <translation>Zu verknüpfende Bedeutungen:</translation>
+      <location filename="../Linker.py" line="263"/>
+      <source>Senses to link:</source>
+      <translation>Zu verknüpfende Bedeutungen:</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="247"/>
-        <source>Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
+      <location filename="../Linker.py" line="247"/>
+      <source>Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
 then double-click in the Target Head Word column for the desired row to link to it.</source>
-        <translation type="unfinished"></translation>
+      <translation>Type the beginning of an entry or any text (if &apos;Filter on all fields&apos; is checked), 
+then double-click in the Target Head Word column for the desired row to link to it.</translation>
     </message>
     <message>
-        <location filename="../Linker.py" line="260"/>
-        <source>Export a list of unlinked senses to a file. If Hide Proper Nouns 
+      <location filename="../Linker.py" line="260"/>
+      <source>Export a list of unlinked senses to a file. If Hide Proper Nouns 
 is checked, Proper Nouns will not be exported.</source>
-        <translation type="unfinished"></translation>
+      <translation>Export a list of unlinked senses to a file. If Hide Proper Nouns 
+is checked, Proper Nouns will not be exported.</translation>
     </message>
-</context>
+  </context>
 </TS>
