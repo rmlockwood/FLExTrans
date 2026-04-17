5ea4c31ba61a5d27e9d52d2da18791a324fa29ca
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-17 10:38:40 +0200

    New translations catalogtargetaffixes.ts (French)

--- Dev/Modules/translations/CatalogTargetAffixes_fr.ts
+++ Dev/Modules/translations/CatalogTargetAffixes_fr.ts
@@ -71,12 +71,12 @@ NOTE: messages in the output window will show the SOURCE project
 as being used. Actually the target project is being used.
 The catalog will be created in the file specified by the Target Affix Gloss List File setting.
 This is typically called target_affix_glosses.txt and is usually in the Build folder.</source>
-      <translation type="unfinished">This module creates a file which has a list of all the affix glosses and morpheme types in the target project. 
-This list is used in subsequent FLExTrans modules to do conversions and synthesize the target text.
-NOTE: messages in the output window will show the SOURCE project
-as being used. Actually the target project is being used.
-The catalog will be created in the file specified by the Target Affix Gloss List File setting.
-This is typically called target_affix_glosses.txt and is usually in the Build folder.</translation>
+      <translation>Ce module crée un fichier qui contient une liste de toutes les gloses d'affixes et types de morphèmes dans le projet cible.
+Cette liste est utilisée dans les modules FLExTrans ultérieurs pour effectuer des conversions et synthétiser le texte cible.
+REMARQUE : les messages dans la fenêtre de sortie afficheront le projet SOURCE
+comme étant utilisé. En réalité, c'est le projet cible qui est utilisé.
+Le catalogue sera créé dans le fichier spécifié par le paramètre Fichier liste de glosses d'affixe cible.
+Il s'appelle généralement target_affix_glosses.txt et se trouve habituellement dans le dossier Build.</translation>
     </message>
   </context>
 </TS>
