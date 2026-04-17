7253121abbfa25bbcb98809e0a1d345d36e51971
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-17 15:57:22 +0200

    New translations paratextchapselectiondlg.ts (Spanish)

--- Dev/Lib/Windows/translations/ParatextChapSelectionDlg_es.ts
+++ Dev/Lib/Windows/translations/ParatextChapSelectionDlg_es.ts
@@ -102,8 +102,8 @@
       <location filename="../ParatextChapSelectionDlg.py" line="138"/>
       <source>Overwrite a text that has the same name. For example, 
 if JHN 01 already exists, it will delete it and create JHN 01 again.</source>
-      <translation>Overwrite a text that has the same name. For example, 
-if JHN 01 already exists, it will delete it and create JHN 01 again.</translation>
+      <translation>Sobrescribir un texto que tenga el mismo nombre. Por ejemplo, 
+si JHN 01 ya existe, se eliminará y se creará JHN 01 nuevamente.</translation>
     </message>
   </context>
 </TS>
