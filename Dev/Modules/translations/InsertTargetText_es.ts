5f294caa74ad76a101575d6fc9f6c308de6ba838
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-17 15:58:06 +0200

    New translations inserttargettext.ts (Spanish)

--- Dev/Modules/translations/InsertTargetText_es.ts
+++ Dev/Modules/translations/InsertTargetText_es.ts
@@ -20,11 +20,7 @@ the results of the {doSynthModule} module
 and insert the text into the target FLEx project. The Source Text Name setting
 will be used for the text name in the target project. An existing text of the 
 same name will not be overwritten. A copy will be created.</source>
-      <translation>Se utilizará el proyecto de destino especificado en la configuración. Este módulo tomará
-los resultados del módulo {doSynthModule}
-y insertará el texto en el proyecto FLEx de destino. La configuración de Nombre de Texto de Origen
-se utilizará para el nombre del texto en el proyecto de destino. Un texto existente con el mismo nombre
-no será sobrescrito. Se creará una copia.</translation>
+      <translation>Se utilizará el proyecto de destino establecido en el archivo de configuración. Este módulo tomará los resultados del proceso de síntesis (módulo Crear diccionarios de destino y Sintetizar) e insertará el texto en el proyecto FLEx de destino. La propiedad SourceTextName en el archivo FlexTrans.config se usará como nombre del texto en el proyecto de destino. NOTA: Se mostrará una ventana de mensaje preguntando si desea hacer cambios en el proyecto FUENTE. Esto no es cierto. Este módulo solo cambiará el proyecto de destino según lo especificado en el archivo de configuración.</translation>
     </message>
     <message>
       <location filename="../InsertTargetText.py" line="140"/>
