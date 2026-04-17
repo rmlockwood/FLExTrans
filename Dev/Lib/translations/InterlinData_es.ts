9f173d53d6932730f8873ff45aeb9eaee1ada232
R Lockwood <rmlockwood@users.noreply.github.com>
2026-04-17 15:56:53 +0200

    New translations interlindata.ts (Spanish)

--- Dev/Lib/translations/InterlinData_es.ts
+++ Dev/Lib/translations/InterlinData_es.ts
@@ -11,7 +11,7 @@
     <message>
       <location filename="../InterlinData.py" line="174"/>
       <source>The Tree Tran Words to Insert File has invalid XML content.</source>
-      <translation>El archivo de palabras para insertar de Tree Tran tiene contenido XML no válido.</translation>
+      <translation>Las palabras a insertar en The Tree Tran tienen contenido XML inválido.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="220"/>
@@ -21,7 +21,7 @@
     <message>
       <location filename="../InterlinData.py" line="265"/>
       <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</source>
-      <translation>No se pudo encontrar un GUID en el archivo de resultados de TreeTran. Quizás TreeTran no está generando todo lo que espera. anaRec id=</translation>
+      <translation>No se pudo encontrar un GUID en el archivo de resultados de TreeTran. Quizás TreeTran no está generando todo lo que se espera. anaRec id=</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="265"/>
@@ -36,12 +36,12 @@
     <message>
       <location filename="../InterlinData.py" line="624"/>
       <source>No sense found for some part of the source word: </source>
-      <translation>No hay sentido para alguna parte de la palabra de origen: </translation>
+      <translation>No se encontró sentido en parte de la palabra fuente </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="629"/>
       <source>No morphosyntactic analysis found for some part of the source word: </source>
-      <translation>No se ha encontrado un análisis morfosinntáctico para alguna parte de la palabra fuente: </translation>
+      <translation>No se ha encontrado un análisis morfosintáctico para parte de la palabra fuente: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="661"/>
@@ -51,12 +51,12 @@
     <message>
       <location filename="../InterlinData.py" line="678"/>
       <source>Couldn't find the sense for source headword: </source>
-      <translation>No se pudo encontrar el sentido para la palabra clave fuente: </translation>
+      <translation>No se pudo encontrar el sentido de la palabra principal de entrada: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="691"/>
       <source>No root or stem found for source word: </source>
-      <translation>No se encontró raíz ni base para la palabra fuente: </translation>
+      <translation>No se encontró raíz ni tema para la palabra fuente: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="701"/>
