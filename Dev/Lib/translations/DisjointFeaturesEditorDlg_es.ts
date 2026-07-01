29dd9335716976ff9a086104115e57b445160549
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-01 22:09:39 +0200

    New translations disjointfeatureseditordlg.ts (Spanish)
    [ci skip]

--- Dev/Lib/translations/DisjointFeaturesEditorDlg_es.ts
+++ Dev/Lib/translations/DisjointFeaturesEditorDlg_es.ts
@@ -1,15 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es" sourcelanguage="en">
-<context>
+<TS version="2.1" language="es-ES" sourcelanguage="en">
+  <context>
     <name>DisjointFeaturesEditorDialog</name>
     <message>
-        <source>Source</source>
-        <translation>Origen</translation>
+      <source>Source</source>
+      <translation>Origen</translation>
     </message>
     <message>
-        <source>Target</source>
-        <translation>Destino</translation>
+      <source>Target</source>
+      <translation>Destino</translation>
     </message>
-</context>
+  </context>
 </TS>
