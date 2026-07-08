7df02ea416b836e1f44a9ae38b322fef77028bad
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-08 10:22:06 +0200

    New translations pastedatawindow.ts (French)
    [ci skip]

---
+++ Dev/Lib/Windows/translations/PasteDataWindow_fr.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
+    <name>PasteDataDialog</name>
+    <message>
+      <location filename="../PasteDataWindow.py" line="51"/>
+      <source>Paste Data</source>
+      <translation type="unfinished">Paste Data</translation>
+    </message>
+    <message>
+      <location filename="../PasteDataWindow.py" line="53"/>
+      <source>Paste from Clipboard</source>
+      <translation type="unfinished">Paste from Clipboard</translation>
+    </message>
+    <message>
+      <location filename="../PasteDataWindow.py" line="54"/>
+      <source>Clear</source>
+      <translation type="unfinished">Clear</translation>
+    </message>
+    <message>
+      <location filename="../PasteDataWindow.py" line="52"/>
+      <source>Paste interlinear data (e.g. copied from FLEx).</source>
+      <translation type="unfinished">Paste interlinear data (e.g. copied from FLEx).</translation>
+    </message>
+  </context>
+</TS>
