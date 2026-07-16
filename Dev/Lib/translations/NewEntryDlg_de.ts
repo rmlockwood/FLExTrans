6129aca1d7751dbb441d0afe81abcb4979964efb
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-16 12:37:27 +0200

    New translations newentrydlg.ts (German)
    [ci skip]

--- Dev/Lib/translations/NewEntryDlg_de.ts
+++ Dev/Lib/translations/NewEntryDlg_de.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../NewEntryDlg.py" line="92"/>
       <source>Target FLEx project name</source>
-      <translation>Name des Ziel-FLEx-Projekts</translation>
+      <translation>Target FLEx project name</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="93"/>
       <source>Lexeme Form</source>
-      <translation>Lexemform</translation>
+      <translation>Lexeme Form</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="292"/>
       <source>Error Check</source>
-      <translation>Fehlerprüfung</translation>
+      <translation>Error Check</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="217"/>
       <source>You must enter a Gloss.</source>
-      <translation>Sie müssen eine Glosse eingeben.</translation>
+      <translation>You must enter a Gloss.</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="238"/>
       <source>Cluster Project Selection Error</source>
-      <translation>Fehler bei der Cluster-Projektauswahl</translation>
+      <translation>Cluster Project Selection Error</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="238"/>
       <source>You must at least select the default target project, {proj}, among your cluster projects.</source>
-      <translation>Sie müssen mindestens das Standard-Zielprojekt {proj} unter Ihren Cluster-Projekten auswählen.</translation>
+      <translation>You must at least select the default target project, {proj}, among your cluster projects.</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="251"/>
       <source>You must enter all the Lexeme Forms.</source>
-      <translation>Sie müssen alle Lexemformen eingeben.</translation>
+      <translation>You must enter all the Lexeme Forms.</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="271"/>
       <source>Not Found Error</source>
-      <translation>Nicht gefundener Fehler</translation>
+      <translation>Not Found Error</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="271"/>
       <source>Failed to open the project: {proj}</source>
-      <translation>Das Projekt konnte nicht geöffnet werden: {proj}</translation>
+      <translation>Failed to open the project: {proj}</translation>
     </message>
     <message>
       <location filename="../NewEntryDlg.py" line="292"/>
       <source>You must enter a Lexeme Form.</source>
-      <translation>Sie müssen eine Lexemform eingeben.</translation>
+      <translation>You must enter a Lexeme Form.</translation>
     </message>
   </context>
 </TS>
