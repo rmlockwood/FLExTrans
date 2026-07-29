8eff979d7a8615d1ecff6d2f8941ee6fe707957e
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 14:14:06 +0200

    New translations liveruletestertool.ts (Spanish)
    [ci skip]

--- Dev/Modules/translations/LiveRuleTesterTool_es.ts
+++ Dev/Modules/translations/LiveRuleTesterTool_es.ts
@@ -359,12 +359,12 @@ Ejecute el módulo {moduleName} por separado para obtener más detalles.</transl
       <translation>Ejecutando {name} (versión {version})...</translation>
     </message>
     <message>
-      <location filename="../LiveRuleTesterTool.py" line="1704"/>
+      <location filename="../LiveRuleTesterTool.py" line="1705"/>
       <source>Fieldworks directory is not set.</source>
       <translation>Fieldworks directory is not set.</translation>
     </message>
     <message>
-      <location filename="../LiveRuleTesterTool.py" line="2881"/>
+      <location filename="../LiveRuleTesterTool.py" line="2882"/>
       <source>Problem opening file: {tgt_file}.</source>
       <translation>Problem opening file: {tgt_file}.</translation>
     </message>
@@ -372,7 +372,7 @@ Ejecute el módulo {moduleName} por separado para obtener más detalles.</transl
   <context>
     <name>RunApertium</name>
     <message>
-      <location filename="../LiveRuleTesterTool.py" line="2842"/>
+      <location filename="../LiveRuleTesterTool.py" line="2843"/>
       <source>An error happened when running the Apertium tools. The contents of apertium_error.txt is:</source>
       <translation>An error happened when running the Apertium tools. The contents of apertium_error.txt is:</translation>
     </message>
