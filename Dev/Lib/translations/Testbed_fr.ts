4a9eee4702b89b7ec928efe02d6f7d35ceae7a31
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 14:37:19 +0200

    New translations testbed.ts (French)
    [ci skip]

--- Dev/Lib/translations/Testbed_fr.ts
+++ Dev/Lib/translations/Testbed_fr.ts
@@ -4,24 +4,24 @@
   <context>
     <name>Testbed</name>
     <message>
-      <location filename="../Testbed.py" line="652"/>
+      <location filename="../Testbed.py" line="653"/>
       <source>No more lines to read in the synthesis file.</source>
-      <translation>Aucune autre ligne à lire dans le fichier de synthèse.</translation>
+      <translation type="unfinished">No more lines to read in the synthesis file.</translation>
     </message>
     <message>
-      <location filename="../Testbed.py" line="870"/>
+      <location filename="../Testbed.py" line="871"/>
       <source>The testbed file: {filePath} could not be read or written.</source>
-      <translation>Le fichier test : {filePath} n'a pas pu être lu ou écrit.</translation>
+      <translation type="unfinished">The testbed file: {filePath} could not be read or written.</translation>
     </message>
     <message>
-      <location filename="../Testbed.py" line="875"/>
+      <location filename="../Testbed.py" line="876"/>
       <source>The testbed file: {filePath} is invalid.</source>
-      <translation>Le fichier test : {filePath} est invalide.</translation>
+      <translation type="unfinished">The testbed file: {filePath} is invalid.</translation>
     </message>
     <message>
-      <location filename="../Testbed.py" line="1107"/>
+      <location filename="../Testbed.py" line="1108"/>
       <source>The testbed results file: {resultsPath} is invalid.</source>
-      <translation>Le fichier de résultats test : {resultsPath} est invalide.</translation>
+      <translation type="unfinished">The testbed results file: {resultsPath} is invalid.</translation>
     </message>
   </context>
 </TS>
