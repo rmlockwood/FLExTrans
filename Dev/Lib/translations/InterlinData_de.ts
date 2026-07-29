367bc569184c11cdf36af5794fed214314b369bf
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 16:06:19 +0200

    New translations interlindata.ts (German)
    [ci skip]

--- Dev/Lib/translations/InterlinData_de.ts
+++ Dev/Lib/translations/InterlinData_de.ts
@@ -5,47 +5,47 @@
     <name>InterlinData</name>
     <message>
       <location filename="../InterlinData.py" line="164"/>
-      <source>No analyses found.</source>
+      <source>No analyses found. </source>
       <translation>Keine Analysen gefunden.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="175"/>
-      <source>The Tree Tran Words to Insert File has invalid XML content.</source>
+      <source>The Tree Tran Words to Insert File has invalid XML content. </source>
       <translation>Die Tree-Tran-Datei „Words to Insert“ enthält ungültigen XML-Inhalt.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="221"/>
-      <source>The Tree Tran Result File has invalid XML content.</source>
+      <source>The Tree Tran Result File has invalid XML content. </source>
       <translation>Die Tree-Tran-Ergebnisdatei enthält ungültigen XML-Inhalt.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="266"/>
-      <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</source>
+      <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id= </source>
       <translation>In der TreeTran-Ergebnisdatei konnte keine GUID gefunden werden. Möglicherweise gibt TreeTran nicht alles aus, was Sie erwarten. anaRec id=</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="266"/>
-      <source>. Exiting.</source>
+      <source>. Exiting. </source>
       <translation>. Wird beendet.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="379"/>
-      <source>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</source>
+      <source>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars} </source>
       <translation>Ungültige Zeichen im Affix: {myStr}. Die folgenden Zeichen sind nicht erlaubt: {chars}</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="625"/>
-      <source>No sense found for some part of the source word: </source>
+      <source>No sense found for some part of the source word:  </source>
       <translation>Für einen Teil des Quellworts wurde keine Bedeutung gefunden:</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="630"/>
-      <source>No morphosyntactic analysis found for some part of the source word: </source>
+      <source>No morphosyntactic analysis found for some part of the source word:  </source>
       <translation>Für einen Teil des Quellworts wurde keine morphosyntaktische Analyse gefunden:</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="662"/>
-      <source>No grammatical category found for the source word: </source>
+      <source>No grammatical category found for the source word:  </source>
       <translation>Keine grammatische Kategorie für das Quellwort gefunden:</translation>
     </message>
     <message>
@@ -55,12 +55,12 @@
     </message>
     <message>
       <location filename="../InterlinData.py" line="692"/>
-      <source>No root or stem found for source word: </source>
+      <source>No root or stem found for source word:  </source>
       <translation>Keine Wurzel oder kein Stamm für das Quellwort gefunden:</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="702"/>
-      <source>One or more unknown words occurred multiple times.</source>
+      <source>One or more unknown words occurred multiple times. </source>
       <translation>Ein oder mehrere unbekannte Wörter sind mehrfach aufgetreten.</translation>
     </message>
   </context>
