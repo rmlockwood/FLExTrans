e6f6d79f4a872e6cf7d08c33dcd1c7d42a06fcae
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 14:38:02 +0200

    New translations paratextchapselectiondlg.ts (French)
    [ci skip]

--- Dev/Lib/Windows/translations/ParatextChapSelectionDlg_fr.ts
+++ Dev/Lib/Windows/translations/ParatextChapSelectionDlg_fr.ts
@@ -99,10 +99,11 @@
       <translation>Textes bibliques</translation>
     </message>
     <message>
-      <location filename="../ParatextChapSelectionDlg.py" line="138"/>
+      <location filename="../ParatextChapSelectionDlg.py" line="139"/>
       <source>Overwrite a text that has the same name. For example, 
 if JHN 01 already exists, it will delete it and create JHN 01 again.</source>
-      <translation>Écraser un texte portant le même nom. Par exemple, si JHN 01 existe déjà, il sera supprimé et recréé.</translation>
+      <translation type="unfinished">Overwrite a text that has the same name. For example, 
+if JHN 01 already exists, it will delete it and create JHN 01 again.</translation>
     </message>
   </context>
 </TS>
