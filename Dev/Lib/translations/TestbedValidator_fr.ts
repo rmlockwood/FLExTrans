7b56970e0a7230ea411d42b5ca63acfc8d354487
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 14:12:38 +0200

    New translations testbedvalidator.ts (French)
    [ci skip]

--- Dev/Lib/translations/TestbedValidator_fr.ts
+++ Dev/Lib/translations/TestbedValidator_fr.ts
@@ -4,22 +4,22 @@
   <context>
     <name>TestbedValidator</name>
     <message>
-      <location filename="../TestbedValidator.py" line="108"/>
+      <location filename="../TestbedValidator.py" line="109"/>
       <source>Word Sense: {wordSense} not found.</source>
       <translation>Sens du mot : {wordSense} introuvable.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="114"/>
+      <location filename="../TestbedValidator.py" line="115"/>
       <source>Grammatical Category: {gramCat} not found.</source>
       <translation>Catégorie grammaticale : {gramCat} introuvable.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="120"/>
+      <location filename="../TestbedValidator.py" line="121"/>
       <source>Tag: {tag} not found.</source>
       <translation>Étiquette : {tag} introuvable.</translation>
     </message>
     <message>
-      <location filename="../TestbedValidator.py" line="232"/>
+      <location filename="../TestbedValidator.py" line="233"/>
       <source>Configuration File Problem. Morphnames not found.</source>
       <translation>Problème de fichier de configuration. Noms de morphèmes introuvables.</translation>
     </message>
