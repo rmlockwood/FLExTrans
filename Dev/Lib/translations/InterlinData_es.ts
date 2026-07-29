01f269aa8e77e48a7ec587bb2b55688a8434f773
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 16:06:17 +0200

    New translations interlindata.ts (Spanish)
    [ci skip]

--- Dev/Lib/translations/InterlinData_es.ts
+++ Dev/Lib/translations/InterlinData_es.ts
@@ -5,48 +5,48 @@
     <name>InterlinData</name>
     <message>
       <location filename="../InterlinData.py" line="164"/>
-      <source>No analyses found.</source>
-      <translation type="unfinished">No analyses found.</translation>
+      <source>No analyses found. </source>
+      <translation>No se encontraron análisis.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="175"/>
-      <source>The Tree Tran Words to Insert File has invalid XML content.</source>
-      <translation type="unfinished">The Tree Tran Words to Insert File has invalid XML content.</translation>
+      <source>The Tree Tran Words to Insert File has invalid XML content. </source>
+      <translation>Las palabras a insertar en The Tree Tran tienen contenido XML inválido.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="221"/>
-      <source>The Tree Tran Result File has invalid XML content.</source>
-      <translation type="unfinished">The Tree Tran Result File has invalid XML content.</translation>
+      <source>The Tree Tran Result File has invalid XML content. </source>
+      <translation>El Archivo de Resultado TreeTran tiene contenido XML inválido.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="266"/>
-      <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</source>
-      <translation type="unfinished">Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id=</translation>
+      <source>Could not find a GUID in the TreeTran results file. Perhaps TreeTran is not putting out all that you expect. anaRec id= </source>
+      <translation>No se pudo encontrar un GUID en el archivo de resultados de TreeTran. Quizás TreeTran no está generando todo lo que se espera. anaRec id=</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="266"/>
-      <source>. Exiting.</source>
-      <translation type="unfinished">. Exiting.</translation>
+      <source>. Exiting. </source>
+      <translation>. Saliendo.</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="379"/>
-      <source>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</source>
-      <translation type="unfinished">Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars}</translation>
+      <source>Invalid characters in the affix: {myStr}. The following characters are not allowed: {chars} </source>
+      <translation>Caracteres no válidos en el afijo: {myStr}. Los siguientes caracteres no están permitidos: {chars}</translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="625"/>
-      <source>No sense found for some part of the source word: </source>
-      <translation type="unfinished">No sense found for some part of the source word: </translation>
+      <source>No sense found for some part of the source word:  </source>
+      <translation>No se encontró sentido en parte de la palabra fuente </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="630"/>
-      <source>No morphosyntactic analysis found for some part of the source word: </source>
-      <translation type="unfinished">No morphosyntactic analysis found for some part of the source word: </translation>
+      <source>No morphosyntactic analysis found for some part of the source word:  </source>
+      <translation>No se ha encontrado un análisis morfosintáctico para parte de la palabra fuente: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="662"/>
-      <source>No grammatical category found for the source word: </source>
-      <translation type="unfinished">No grammatical category found for the source word: </translation>
+      <source>No grammatical category found for the source word:  </source>
+      <translation>No se encontró una categoría gramatical para la palabra fuente: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="678"/>
@@ -55,13 +55,13 @@
     </message>
     <message>
       <location filename="../InterlinData.py" line="692"/>
-      <source>No root or stem found for source word: </source>
-      <translation type="unfinished">No root or stem found for source word: </translation>
+      <source>No root or stem found for source word:  </source>
+      <translation>No se encontró raíz ni tema para la palabra fuente: </translation>
     </message>
     <message>
       <location filename="../InterlinData.py" line="702"/>
-      <source>One or more unknown words occurred multiple times.</source>
-      <translation type="unfinished">One or more unknown words occurred multiple times.</translation>
+      <source>One or more unknown words occurred multiple times. </source>
+      <translation>Una o más palabras desconocidas ocurrieron varias veces.</translation>
     </message>
   </context>
 </TS>
