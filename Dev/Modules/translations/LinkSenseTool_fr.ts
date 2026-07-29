0f59fadd4639540c4248db4dc056e2be53ddae88
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 14:39:02 +0200

    New translations linksensetool.ts (French)
    [ci skip]

--- Dev/Modules/translations/LinkSenseTool_fr.ts
+++ Dev/Modules/translations/LinkSenseTool_fr.ts
@@ -78,9 +78,9 @@ Définissez quel champ personnalisé est utilisé pour la liaison dans les param
       <translation>Plus de {num_warnings} gloses vides trouvées. Suppression des avertissements supplémentaires pour les gloses cibles vides.</translation>
     </message>
     <message>
-      <location filename="../LinkSenseTool.py" line="1526"/>
+      <location filename="../LinkSenseTool.py" line="1527"/>
       <source>No words with a valid root morph type were found. Please check the your settings, specifically Source Morpheme Types Counted As Roots.</source>
-      <translation>Aucun mot avec un type de morphème racine valide n'a été trouvé. Veuillez vérifier vos paramètres, en particulier les types de morphèmes sources comptés comme racines.</translation>
+      <translation type="unfinished">No words with a valid root morph type were found. Please check the your settings, specifically Source Morpheme Types Counted As Roots.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1633"/>
@@ -98,9 +98,9 @@ Définissez quel champ personnalisé est utilisé pour la liaison dans les param
       <translation>1 lien supprimé</translation>
     </message>
     <message>
-      <location filename="../LinkSenseTool.py" line="1643"/>
+      <location filename="../LinkSenseTool.py" line="1644"/>
       <source>{num} links removed</source>
-      <translation>{num} liens supprimés</translation>
+      <translation type="unfinished">{num} links removed</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1711"/>
@@ -193,9 +193,9 @@ Définissez quel champ personnalisé est utilisé pour la liaison dans les param
       <translation>Aucun sens n'a été trouvé pour la liaison. Veuillez vérifier votre texte et approuver certains mots.</translation>
     </message>
     <message>
-      <location filename="../LinkSenseTool.py" line="1958"/>
+      <location filename="../LinkSenseTool.py" line="1959"/>
       <source>There was an error finding senses to link.</source>
-      <translation>Il y a eu une erreur lors de la recherche des sens à lier.</translation>
+      <translation type="unfinished">There was an error finding senses to link.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1960"/>
