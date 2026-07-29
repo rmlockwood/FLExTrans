12162071b837df6caad4dae908521861d7fd8099
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 16:06:45 +0200

    New translations textinoututils.ts (French)
    [ci skip]

--- Dev/Lib/translations/TextInOutUtils_fr.ts
+++ Dev/Lib/translations/TextInOutUtils_fr.ts
@@ -10,78 +10,78 @@
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="343"/>
-      <source>The rules file: {textOutRulesFile} has invalid XML data.</source>
-      <translation type="unfinished">The rules file: {textOutRulesFile} has invalid XML data.</translation>
+      <source>The rules file: {textOutRulesFile} has invalid XML data. </source>
+      <translation>Le fichier de règles : {textOutRulesFile} contient des données XML invalides.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="355"/>
-      <source>{numRules} {moduleName} rules applied.</source>
-      <translation type="unfinished">{numRules} {moduleName} rules applied.</translation>
+      <source>{numRules} {moduleName} rules applied. </source>
+      <translation>{numRules} règles {moduleName} appliquées.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="419"/>
-      <source>Current Project not in Cluster Projects list, exiting.</source>
-      <translation type="unfinished">Current Project not in Cluster Projects list, exiting.</translation>
+      <source>Current Project not in Cluster Projects list, exiting. </source>
+      <translation>Le projet actuel ne figure pas dans la liste des projets groupés, fermeture en cours.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="445"/>
-      <source>Error accessing work project folders.</source>
-      <translation type="unfinished">Error accessing work project folders.</translation>
+      <source>Error accessing work project folders. </source>
+      <translation>Erreur lors de l'accès aux dossiers du projet de travail.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="448"/>
-      <source>FLEx project name</source>
-      <translation type="unfinished">FLEx project name</translation>
+      <source>FLEx project name </source>
+      <translation>Nom du projet FLEx</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="449"/>
-      <source>WorkProject folder</source>
-      <translation type="unfinished">WorkProject folder</translation>
+      <source>WorkProject folder </source>
+      <translation>Dossier WorkProject</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="513"/>
-      <source>Run the {wildebeest} cleanup tool</source>
-      <translation type="unfinished">Run the {wildebeest} cleanup tool</translation>
+      <source>Run the {wildebeest} cleanup tool </source>
+      <translation>Exécuter l'outil de nettoyage {wildebeest}</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="514"/>
-      <source>help</source>
-      <translation type="unfinished">help</translation>
+      <source>help </source>
+      <translation>aide</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="559"/>
-      <source>There was a problem reading the configuration file for folder {folderName}. Check your configuration.</source>
-      <translation type="unfinished">There was a problem reading the configuration file for folder {folderName}. Check your configuration.</translation>
+      <source>There was a problem reading the configuration file for folder {folderName}. Check your configuration. </source>
+      <translation>Un problème est survenu lors de la lecture du fichier de configuration pour le dossier {folderName}. Vérifiez votre configuration.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="593"/>
-      <source>There was a problem creating or backing up the rules file. Check your configuration.</source>
-      <translation type="unfinished">There was a problem creating or backing up the rules file. Check your configuration.</translation>
+      <source>There was a problem creating or backing up the rules file. Check your configuration. </source>
+      <translation>Un problème est survenu lors de la création ou de la sauvegarde du fichier de règles. Vérifiez votre configuration.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="761"/>
-      <source>For folder {foldName}, the rule: {ruleID} already exists.</source>
-      <translation type="unfinished">For folder {foldName}, the rule: {ruleID} already exists.</translation>
+      <source>For folder {foldName}, the rule: {ruleID} already exists. </source>
+      <translation>Pour le dossier {foldName}, la règle : {ruleID} existe déjà.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="854"/>
-      <source>For folder {foldName}, the rule: {ruleID} was not found.</source>
-      <translation type="unfinished">For folder {foldName}, the rule: {ruleID} was not found.</translation>
+      <source>For folder {foldName}, the rule: {ruleID} was not found. </source>
+      <translation>Pour le dossier {foldName}, la règle : {ruleID} introuvable.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="1019"/>
-      <source>For folder {foldName}, the rule: {ruleID} not found.</source>
-      <translation type="unfinished">For folder {foldName}, the rule: {ruleID} not found.</translation>
+      <source>For folder {foldName}, the rule: {ruleID} not found. </source>
+      <translation>Pour le dossier {foldName}, la règle : {ruleID} introuvable.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="1368"/>
-      <source>Error loading XML file.</source>
-      <translation type="unfinished">Error loading XML file.</translation>
+      <source>Error loading XML file. </source>
+      <translation>Erreur lors du chargement du fichier XML.</translation>
     </message>
     <message>
       <location filename="../TextInOutUtils.py" line="1472"/>
-      <source>Error saving settings.</source>
-      <translation type="unfinished">Error saving settings.</translation>
+      <source>Error saving settings. </source>
+      <translation>Erreur lors de la sauvegarde des paramètres.</translation>
     </message>
   </context>
 </TS>
