abc9c88d9b0ad18af1b6a22ab1bc124c08108b4c
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 16:06:35 +0200

    New translations testbed.ts (German)
    [ci skip]

--- Dev/Lib/translations/Testbed_de.ts
+++ Dev/Lib/translations/Testbed_de.ts
@@ -5,22 +5,22 @@
     <name>Testbed</name>
     <message>
       <location filename="../Testbed.py" line="653"/>
-      <source>No more lines to read in the synthesis file.</source>
+      <source>No more lines to read in the synthesis file. </source>
       <translation>Keine weiteren Zeilen in der Synthesedatei zu lesen.</translation>
     </message>
     <message>
       <location filename="../Testbed.py" line="871"/>
-      <source>The testbed file: {filePath} could not be read or written.</source>
+      <source>The testbed file: {filePath} could not be read or written. </source>
       <translation>Die Testbed-Datei: {filePath} konnte nicht gelesen oder geschrieben werden.</translation>
     </message>
     <message>
       <location filename="../Testbed.py" line="876"/>
-      <source>The testbed file: {filePath} is invalid.</source>
+      <source>The testbed file: {filePath} is invalid. </source>
       <translation>Die Testbed-Datei: {filePath} ist ungültig.</translation>
     </message>
     <message>
       <location filename="../Testbed.py" line="1108"/>
-      <source>The testbed results file: {resultsPath} is invalid.</source>
+      <source>The testbed results file: {resultsPath} is invalid. </source>
       <translation>Die Testbed-Ergebnisdatei: {resultsPath} ist ungültig.</translation>
     </message>
   </context>
