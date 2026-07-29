1fd64581d2b597b5439e7ea67919f207da87ef25
R Lockwood <rmlockwood@users.noreply.github.com>
2026-07-29 16:06:21 +0200

    New translations mixpanel.ts (Spanish)
    [ci skip]

--- Dev/Lib/translations/Mixpanel_es.ts
+++ Dev/Lib/translations/Mixpanel_es.ts
@@ -5,13 +5,13 @@
     <name>Mixpanel</name>
     <message>
       <location filename="../Mixpanel.py" line="52"/>
-      <source>FLExTrans would like to send usage statistics to FLExTrans developers. No personally identifiable information is sent. These anonymous statistics will help with future development. Do you want to opt out of sending usage statistics?</source>
-      <translation type="unfinished">FLExTrans would like to send usage statistics to FLExTrans developers. No personally identifiable information is sent. These anonymous statistics will help with future development. Do you want to opt out of sending usage statistics?</translation>
+      <source>FLExTrans would like to send usage statistics to FLExTrans developers. No personally identifiable information is sent. These anonymous statistics will help with future development. Do you want to opt out of sending usage statistics? </source>
+      <translation>FLExTrans desea enviar estadísticas de uso a los desarrolladores de FLExTrans. No se envía información personal identificable. Estas estadísticas anónimas ayudarán con futuro desarrollo del programa. ¿Desea optar por no enviar estadísticas de uso?</translation>
     </message>
     <message>
       <location filename="../Mixpanel.py" line="55"/>
-      <source>FLExTrans Usage</source>
-      <translation type="unfinished">FLExTrans Usage</translation>
+      <source>FLExTrans Usage </source>
+      <translation>Uso de FLExTrans</translation>
     </message>
   </context>
 </TS>
