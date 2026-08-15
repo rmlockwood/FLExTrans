ae8529d9eaa9de630ac053558c695472db2cccd4
R Lockwood <rmlockwood@users.noreply.github.com>
2026-08-15 20:32:01 +0200

    New translations rautils.ts (Spanish)
    [ci skip]

--- Dev/Lib/translations/RAutils_es.ts
+++ Dev/Lib/translations/RAutils_es.ts
@@ -17,11 +17,11 @@
     </message>
     <message>
       <source>No word or affix in the target has a feature.  Please insert at least one feature.</source>
-      <translation>Ninguna palabra o afijo en el destino tiene un rásgo. Por favor, inserte al menos uno rásgo.</translation>
+      <translation>Ninguna palabra o afijo en el destino tiene una característica. Por favor, inserta al menos una característica.</translation>
     </message>
     <message>
       <source>One or more source words do not have a category.  Please insert a category for every source word.</source>
-      <translation>Una o más palabras de origen no tienen una categoría. Por favor, inserte una categoría para cada palabra de origen.</translation>
+      <translation>Una o más palabras de origen no tienen una categoría. Por favor, inserta una categoría para cada palabra de origen.</translation>
     </message>
     <message>
       <source>phrase</source>
@@ -33,7 +33,7 @@
     </message>
     <message>
       <source>Target phrase has {0} head words; only one allowed</source>
-      <translation>Target phrase has {0} head words; only one allowed</translation>
+      <translation>La frase de destino tiene {0} palabras principales; solo se permite una</translation>
     </message>
     <message>
       <source>tgt</source>
