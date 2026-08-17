12b5500568ad2bf9c9b1e72c979dbc21b3131cdb
R Lockwood <rmlockwood@users.noreply.github.com>
2026-08-17 14:40:01 +0200

    New translations converttexttostampformat.ts (Spanish)
    [ci skip]

--- Dev/Modules/translations/ConvertTextToSTAMPformat_es.ts
+++ Dev/Modules/translations/ConvertTextToSTAMPformat_es.ts
@@ -92,7 +92,7 @@ NOTA: Los mensajes y la barra de tareas mostrarán que se está utilizando el pr
     <message>
       <location filename="../ConvertTextToSTAMPformat.py" line="1421"/>
       <source>The {modname} module must be run before this module. The file: ...\{filePath} does not exist.</source>
-      <translation type="unfinished">The {modname} module must be run before this module. The file: ...\{filePath} does not exist.</translation>
+      <translation>The {modname} module must be run before this module. The file: ...\{filePath} does not exist.</translation>
     </message>
   </context>
 </TS>
