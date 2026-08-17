842fad1c565f93fdee0f3c8d3b1488da06c66854
R Lockwood <rmlockwood@users.noreply.github.com>
2026-08-17 14:40:14 +0200

    New translations linksensetool.ts (Spanish)
    [ci skip]

--- Dev/Modules/translations/LinkSenseTool_es.ts
+++ Dev/Modules/translations/LinkSenseTool_es.ts
@@ -80,7 +80,7 @@ Configure qué campo personalizado se utiliza para vincular en la configuración
     <message>
       <location filename="../LinkSenseTool.py" line="1527"/>
       <source>No words with a valid root morph type were found. Please check the your settings, specifically Source Morpheme Types Counted As Roots.</source>
-      <translation type="unfinished">No words with a valid root morph type were found. Please check the your settings, specifically Source Morpheme Types Counted As Roots.</translation>
+      <translation>No words with a valid root morph type were found. Please check the your settings, specifically Source Morpheme Types Counted As Roots.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1633"/>
@@ -100,7 +100,7 @@ Configure qué campo personalizado se utiliza para vincular en la configuración
     <message>
       <location filename="../LinkSenseTool.py" line="1644"/>
       <source>{num} links removed</source>
-      <translation type="unfinished">{num} links removed</translation>
+      <translation>{num} links removed</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1711"/>
@@ -195,7 +195,7 @@ Configure qué campo personalizado se utiliza para vincular en la configuración
     <message>
       <location filename="../LinkSenseTool.py" line="1959"/>
       <source>There was an error finding senses to link.</source>
-      <translation type="unfinished">There was an error finding senses to link.</translation>
+      <translation>There was an error finding senses to link.</translation>
     </message>
     <message>
       <location filename="../LinkSenseTool.py" line="1960"/>
