c5775cc8bbc62f647dfa966ca24663173fc3fd93
R Lockwood <rlbooks@web.de>
2026-09-09 15:53:04 +0200

    Initial version of Delete Texts module

---
+++ Dev/Modules/translations/DeleteTexts_es.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en">
+<context>
+    <name>DeleteTexts</name>
+    <message>
+        <location filename="../DeleteTexts.py" line="125" />
+        <source>Delete Texts</source>
+        <translation>Eliminar textos</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="230" />
+        <source>Texts in the {projectName} project (multi-select):</source>
+        <translation>Textos del proyecto {projectName} (selección múltiple):</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="244" />
+        <source>{textName}  [current FLExTrans source text]</source>
+        <translation>{textName}  [texto fuente actual de FLExTrans]</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="156" />
+        <source>Hold Ctrl or Shift to select more than one text. Deleting a text cannot be undone.</source>
+        <translation>Mantenga pulsada la tecla Ctrl o Mayús para seleccionar más de un texto. La eliminación de un texto no se puede deshacer.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="162" />
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="163" />
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="292" />
+        <source>Deleting FLEx Texts</source>
+        <translation>Eliminando textos de FLEx</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="375" />
+        <source>You need to run this module in "modify mode."</source>
+        <translation>Debe ejecutar este módulo en "modo de modificación".</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="433" />
+        <source>No texts were deleted.</source>
+        <translation>No se eliminó ningún texto.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="447" />
+        <source>The text "{textName}" no longer exists in the project. Skipping.</source>
+        <translation>El texto "{textName}" ya no existe en el proyecto. Se omite.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="452" />
+        <source>The text "{textName}" cannot be deleted. Skipping.</source>
+        <translation>El texto "{textName}" no se puede eliminar. Se omite.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="459" />
+        <source>Deleted the text "{textName}".</source>
+        <translation>Se eliminó el texto "{textName}".</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="471" />
+        <source>The source text setting was cleared because the text it named was deleted. Choose a new source text in the FLExTrans Settings.</source>
+        <translation>Se borró la configuración del texto fuente porque se eliminó el texto que indicaba. Elija un nuevo texto fuente en la Configuración de FLExTrans.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="473" />
+        <source>{count} text(s) deleted from the {projectName} project.</source>
+        <translation>{count} texto(s) eliminado(s) del proyecto {projectName}.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="90" />
+        <source>Delete one or more texts from a FLEx project.</source>
+        <translation>Eliminar uno o más textos de un proyecto FLEx.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="136" />
+        <source>FLEx project:</source>
+        <translation>Proyecto FLEx:</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="218" />
+        <source>The {projectName} project could not be opened. See the report pane.</source>
+        <translation>No se pudo abrir el proyecto {projectName}. Consulte el panel de informes.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="227" />
+        <source>The {projectName} project has no texts.</source>
+        <translation>El proyecto {projectName} no tiene textos.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="315" />
+        <source>Delete these {count} text(s) from the FLEx project?</source>
+        <translation>¿Eliminar estos {count} texto(s) del proyecto FLEx?</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="328" />
+        <source>This CANNOT be undone. If FLEx is open, make sure you are NOT in the Texts &amp; Words section of FLEx.</source>
+        <translation>Esto NO se puede deshacer. Si FLEx está abierto, asegúrese de NO estar en la sección Textos y palabras de FLEx.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="395" />
+        <source>No FLEx projects were found.</source>
+        <translation>No se encontraron proyectos FLEx.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="92" />
+        <source>Choose a FLEx project, then select one or more of its texts and delete them. Hold Ctrl or Shift to select more than one text.
+The project list starts on your target project, since the texts you usually want to clear out are the ones FLExTrans inserted
+there, but you can pick any FLEx project, including your source project. 
+Deleting a text this way CANNOT be undone, in FLExTrans or in FLEx, so the module asks you to confirm and
+lists what it is about to delete. Before running it, make sure you are not in the Texts &amp; Words section of FLEx, otherwise FLEx
+may be left holding on to a text that no longer exists. If you delete the text that FLExTrans is currently set up to translate, 
+the source text setting is cleared and you will need
+to choose a new source text in the FLExTrans Settings.</source>
+        <translation>Elija un proyecto FLEx, luego seleccione uno o más de sus textos y elimínelos. Mantenga pulsada la tecla Ctrl o Mayús para
+seleccionar más de un texto. La lista de proyectos comienza con su proyecto de destino, ya que los textos que normalmente desea
+eliminar son los que FLExTrans insertó allí, pero puede elegir cualquier proyecto FLEx, incluido su proyecto de origen.
+La eliminación de un texto de esta manera NO se puede deshacer, ni en FLExTrans ni en FLEx, por lo que el módulo le pide
+confirmación y enumera lo que va a eliminar. Antes de ejecutarlo, asegúrese de no estar en la sección Textos y palabras de FLEx;
+de lo contrario, FLEx podría quedarse apuntando a un texto que ya no existe. Si elimina el texto que FLExTrans está configurado
+para traducir, se borra la configuración del texto fuente y tendrá que elegir un nuevo texto fuente en la Configuración de
+FLExTrans.</translation>
+    </message>
+</context>
+</TS>
---
+++ Dev/Modules/translations/DeleteTexts_fr.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>DeleteTexts</name>
+    <message>
+        <location filename="../DeleteTexts.py" line="125" />
+        <source>Delete Texts</source>
+        <translation>Supprimer des textes</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="230" />
+        <source>Texts in the {projectName} project (multi-select):</source>
+        <translation>Textes du projet {projectName} (sélection multiple) :</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="244" />
+        <source>{textName}  [current FLExTrans source text]</source>
+        <translation>{textName}  [texte source FLExTrans actuel]</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="156" />
+        <source>Hold Ctrl or Shift to select more than one text. Deleting a text cannot be undone.</source>
+        <translation>Maintenez la touche Ctrl ou Maj enfoncée pour sélectionner plusieurs textes. La suppression d'un texte est irréversible.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="162" />
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="163" />
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="292" />
+        <source>Deleting FLEx Texts</source>
+        <translation>Suppression de textes FLEx</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="375" />
+        <source>You need to run this module in "modify mode."</source>
+        <translation>Vous devez exécuter ce module en "mode modification".</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="433" />
+        <source>No texts were deleted.</source>
+        <translation>Aucun texte n'a été supprimé.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="447" />
+        <source>The text "{textName}" no longer exists in the project. Skipping.</source>
+        <translation>Le texte « {textName} » n'existe plus dans le projet. Ignoré.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="452" />
+        <source>The text "{textName}" cannot be deleted. Skipping.</source>
+        <translation>Le texte « {textName} » ne peut pas être supprimé. Ignoré.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="459" />
+        <source>Deleted the text "{textName}".</source>
+        <translation>Le texte « {textName} » a été supprimé.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="471" />
+        <source>The source text setting was cleared because the text it named was deleted. Choose a new source text in the FLExTrans Settings.</source>
+        <translation>Le paramètre de texte source a été effacé car le texte qu'il désignait a été supprimé. Choisissez un nouveau texte source dans les Paramètres FLExTrans.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="473" />
+        <source>{count} text(s) deleted from the {projectName} project.</source>
+        <translation>{count} texte(s) supprimé(s) du projet {projectName}.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="90" />
+        <source>Delete one or more texts from a FLEx project.</source>
+        <translation>Supprimer un ou plusieurs textes d'un projet FLEx.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="136" />
+        <source>FLEx project:</source>
+        <translation>Projet FLEx :</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="218" />
+        <source>The {projectName} project could not be opened. See the report pane.</source>
+        <translation>Le projet {projectName} n'a pas pu être ouvert. Consultez le volet de rapport.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="227" />
+        <source>The {projectName} project has no texts.</source>
+        <translation>Le projet {projectName} ne contient aucun texte.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="315" />
+        <source>Delete these {count} text(s) from the FLEx project?</source>
+        <translation>Supprimer ces {count} texte(s) du projet FLEx ?</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="328" />
+        <source>This CANNOT be undone. If FLEx is open, make sure you are NOT in the Texts &amp; Words section of FLEx.</source>
+        <translation>Cette action est IRRÉVERSIBLE. Si FLEx est ouvert, assurez-vous de ne PAS être dans la section Textes et mots de FLEx.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="395" />
+        <source>No FLEx projects were found.</source>
+        <translation>Aucun projet FLEx n'a été trouvé.</translation>
+    </message>
+    <message>
+        <location filename="../DeleteTexts.py" line="92" />
+        <source>Choose a FLEx project, then select one or more of its texts and delete them. Hold Ctrl or Shift to select more than one text.
+The project list starts on your target project, since the texts you usually want to clear out are the ones FLExTrans inserted
+there, but you can pick any FLEx project, including your source project. 
+Deleting a text this way CANNOT be undone, in FLExTrans or in FLEx, so the module asks you to confirm and
+lists what it is about to delete. Before running it, make sure you are not in the Texts &amp; Words section of FLEx, otherwise FLEx
+may be left holding on to a text that no longer exists. If you delete the text that FLExTrans is currently set up to translate, 
+the source text setting is cleared and you will need
+to choose a new source text in the FLExTrans Settings.</source>
+        <translation>Choisissez un projet FLEx, puis sélectionnez un ou plusieurs de ses textes et supprimez-les. Maintenez la touche Ctrl ou Maj
+enfoncée pour sélectionner plusieurs textes. La liste des projets commence par votre projet cible, car les textes que vous
+souhaitez habituellement supprimer sont ceux que FLExTrans y a insérés, mais vous pouvez choisir n'importe quel projet FLEx, y
+compris votre projet source.
+La suppression d'un texte de cette manière est IRRÉVERSIBLE, dans FLExTrans comme dans FLEx : le module demande donc une
+confirmation et énumère ce qu'il va supprimer. Avant de l'exécuter, assurez-vous de ne pas être dans la section Textes et mots de
+FLEx, sinon FLEx risque de conserver une référence à un texte qui n'existe plus. Si vous supprimez le texte que FLExTrans est
+actuellement configuré pour traduire, le paramètre de texte source est effacé et vous devrez choisir un nouveau texte source dans
+les Paramètres FLExTrans.</translation>
+    </message>
+</context>
+</TS>
